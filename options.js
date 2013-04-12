9b09a4354138888218109b504730c734ede7d3cc
jshackles <jshackles@gmail.com>
2013-04-12 12:03:35 -0700

    Options Rewrite
    Now all are with checkboxes, includes the ability to disable
    highlighting games

--- options.js
+++ options.js
@@ -6,16 +6,19 @@ function save_options() {
 	bgcolor = $("#bgcolor").val();
 	wlcolor = $("#wlcolor").val();
 	ccolor = $("#ccolor").val();
-	hideinstallsteambutton = $("#hideinstallsteambutton").val();
-	showdrm = $("#showdrm").val();
-	showlowestprice = $("#showlowestprice").val();	
-	showmcus = $("#showmcus").val();
-	showwsgf = $("#showwsgf").val();
+	showowned = $("#showowned").prop('checked');
+	showwishlist = $("#showwishlist").prop('checked');
+	showcoupon = $("#showcoupon").prop('checked');	
+	hideinstallsteambutton = $("#hideinstallsteambutton").prop('checked');
+	showdrm = $("#showdrm").prop('checked');
+	showlowestprice = $("#showlowestprice").prop('checked');
+	showmcus = $("#showmcus").prop('checked');
+	showwsgf = $("#showwsgf").prop('checked');
 	
 	// Community Options
-	showtotal = $("#showtotal").val();
-	showgroupevents = $("#showgroupevents").val();
-	showgreenlightbanner = $("#showgreenlightbanner").val();
+	showtotal = $("#showtotal").prop('checked');
+	showgroupevents = $("#showgroupevents").prop('checked');
+	showgreenlightbanner = $("#showgreenlightbanner").prop('checked');
 	
 	// Profile Link Options
 	profile_steamgifts = $("#profile_steamgifts").prop('checked');
@@ -29,6 +32,9 @@ function save_options() {
 		'showtotal': showtotal,
 		'showmcus': showmcus,
 		'showwsgf': showwsgf,
+		'showowned': showowned,
+		'showwishlist': showwishlist,
+		'showcoupon': showcoupon,
 		'hideinstallsteambutton': hideinstallsteambutton,
 		'showdrm': showdrm,
 		'showlowestprice': showlowestprice,
@@ -147,14 +153,17 @@ function restore_options() {
 		if (settings.bgcolor === undefined) { settings.bgcolor = "#5c7836";	chrome.storage.sync.set({'bgcolor': settings.bgcolor});	}
 		if (settings.wlcolor === undefined) { settings.wlcolor = "#496e93";	chrome.storage.sync.set({'wlcolor': settings.wlcolor}); }
 		if (settings.ccolor === undefined) { settings.ccolor = "#6b2269";	chrome.storage.sync.set({'ccolor': settings.ccolor}); }
-		if (settings.showtotal === undefined) {	settings.showtotal = "Yes";	chrome.storage.sync.set({'showtotal': settings.showtotal}); }		
-		if (settings.showmcus === undefined) { settings.showmcus = "Yes"; chrome.storage.sync.set({'showmcus': settings.showmcus}); }		
-		if (settings.showwsgf === undefined) { settings.showwsgf = "Yes"; chrome.storage.sync.set({'showwsgf': settings.showwsgf}); }
-		if (settings.hideinstallsteambutton === undefined) { settings.hideinstallsteambutton = "No"; chrome.storage.sync.set({'hideinstallsteambutton': settings.hideinstallsteambutton}); }
-		if (settings.showdrm === undefined) { settings.showdrm = "Yes";	chrome.storage.sync.set({'showdrm': settings.showdrm}); }
-		if (settings.showlowestprice === undefined) { settings.showlowestprice = "Yes";	chrome.storage.sync.set({'showlowestprice': settings.showlowestprice}); }		
-		if (settings.showgroupevents === undefined) { settings.showgroupevents = "Yes";	chrome.storage.sync.set({'showgroupevents': settings.showgroupevents});	}		
-		if (settings.showgreenlightbanner === undefined) { settings.showgreenlightbanner = "No"; chrome.storage.sync.set({'showgreenlightbanner': settings.showgreenlightbanner}); }	
+		if (settings.showowned === undefined) { settings.showowned = true; chrome.storage.sync.set({'showowned': settings.showowned}); }
+		if (settings.showwishlist === undefined) { settings.showwishlist = true; chrome.storage.sync.set({'showwishlist': settings.showwishlist}); }
+		if (settings.showcoupon === undefined) { settings.showcoupon = true; chrome.storage.sync.set({'showcoupon': settings.showcoupon}); }
+		if (settings.showtotal === undefined) { settings.showtotal = true; chrome.storage.sync.set({'showtotal': settings.showtotal}); }
+		if (settings.showmcus === undefined) { settings.showmcus = true; chrome.storage.sync.set({'showmcus': settings.showmcus}); }		
+		if (settings.showwsgf === undefined) { settings.showwsgf = true; chrome.storage.sync.set({'showwsgf': settings.showwsgf}); }
+		if (settings.hideinstallsteambutton === undefined) { settings.hideinstallsteambutton = false; chrome.storage.sync.set({'hideinstallsteambutton': settings.hideinstallsteambutton}); }
+		if (settings.showdrm === undefined) { settings.showdrm = true; chrome.storage.sync.set({'showdrm': settings.showdrm}); }
+		if (settings.showlowestprice === undefined) { settings.showlowestprice = true;	chrome.storage.sync.set({'showlowestprice': settings.showlowestprice}); }		
+		if (settings.showgroupevents === undefined) { settings.showgroupevents = true;	chrome.storage.sync.set({'showgroupevents': settings.showgroupevents});	}		
+		if (settings.showgreenlightbanner === undefined) { settings.showgreenlightbanner = true; chrome.storage.sync.set({'showgreenlightbanner': settings.showgreenlightbanner}); }	
 		if (settings.profile_steamgifts === undefined) { settings.profile_steamgifts = true; chrome.storage.sync.set({'profile_steamgifts': settings.profile_steamgifts}); }
 		if (settings.profile_steamtrades === undefined) { settings.profile_steamtrades = true; chrome.storage.sync.set({'profile_steamtrades': settings.profile_steamtrades}); }
 		if (settings.profile_steamrep === undefined) { settings.profile_steamrep = true; chrome.storage.sync.set({'profile_steamrep': settings.profile_steamrep}); }
@@ -166,16 +175,19 @@ function restore_options() {
 		$("#bgcolor").attr('value', settings.bgcolor);
 		$("#wlcolor").attr('value', settings.wlcolor);
 		$("#ccolor").attr('value', settings.ccolor);
-		$("#hideinstallsteambutton").attr('value', settings.hideinstallsteambutton);
-		$("#showdrm").attr('value', settings.showdrm);
-		$("#showmcus").attr('value', settings.showmcus);
-		$("#showwsgf").attr('value', settings.showwsgf);
-		$("#showlowestprice").attr('value', settings.showlowestprice);
+		$("#showowned").attr('checked', settings.showowned);
+		$("#showwishlist").attr('checked', settings.showwishlist);
+		$("#showcoupon").attr('checked', settings.showcoupon);
+		$("#hideinstallsteambutton").attr('checked', settings.hideinstallsteambutton);
+		$("#showdrm").attr('checked', settings.showdrm);
+		$("#showmcus").attr('checked', settings.showmcus);
+		$("#showwsgf").attr('checked', settings.showwsgf);
+		$("#showlowestprice").attr('checked', settings.showlowestprice);
 				
 		// Load Community Options
-		$("#showtotal").attr('value', settings.showtotal);
-		$("#showgroupevents").attr('value', settings.showgroupevents);
-		$("#showgreenlightbanner").attr('value', settings.showgreenlightbanner);
+		$("#showtotal").attr('checked', settings.showtotal);
+		$("#showgroupevents").attr('checked', settings.showgroupevents);
+		$("#showgreenlightbanner").attr('checked', settings.showgreenlightbanner);
 		
 		// Load Profile Link Options
 		$("#profile_steamgifts").attr('checked', settings.profile_steamgifts);
