09bce856edb7af9ddc88eccb4bb2bc700f50364d
jshackles <jshackles@gmail.com>
2013-09-06 12:17:39 -0700

    adds button to homepage for wallet balance

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -2583,6 +2583,33 @@ function add_carousel_descriptions() {
 	});
 }
 
+function add_affordable_button() {
+	var balance_text = $("#header_wallet_ctn").text().trim();
+	var currency_symbol = balance_text.match(/(?:R\$|\$|€|£|pуб)/)[0];
+	var balance = balance_text.replace(currency_symbol, "");
+	balance = balance.replace(",", ".");
+	if (balance > 0) {
+		var link = "http://store.steampowered.com/search/?sort_by=Price&sort_order=DESC&price=0%2C" + balance;
+		$(".btn_browse").each(function(index) {
+			if (index == 1) {
+				switch (currency_symbol) {
+					case "€":
+						$(this).after("<a class='btn_browse' style='width: 308px; background-image: url(" + chrome.extension.getURL("img/es_btn_browse.png") +");' href='" + link + "'><h3 style='width: 120px;'>" + balance + "<span class='currency'>" + currency_symbol + "</span></h3><h5><span id='es_results'></span> games under " + balance_text + "</h5></a>");
+					case "pуб":
+						$(this).after("<a class='btn_browse' style='width: 308px; background-image: url(" + chrome.extension.getURL("img/es_btn_browse.png") +");' href='" + link + "'><h3 style='width: 120px;'>" + balance + "</h3><h5><span id='es_results'></span> games under " + balance_text + "</h5></a>");
+					default:
+						$(this).after("<a class='btn_browse' style='width: 308px; background-image: url(" + chrome.extension.getURL("img/es_btn_browse.png") +");' href='" + link + "'><h3 style='width: 120px;'><span class='currency'>" + currency_symbol + "</span>" + balance + "</h3><h5><span id='es_results'></span> games under " + balance_text + "</h5></a>");
+				}		
+				get_http(link, function(txt) {
+					var results = txt.match(/search_pagination_left(.+)\r\n(.+)/)[2];
+					results = results.match(/(\d+)(?!.*\d)/)[0];
+					$("#es_results").text(results);
+				});	
+			}	
+		});
+	}
+}
+
 function add_small_cap_height() {
 	// Add height for another line for tags;
 	var height_to_add = 20,
@@ -3110,6 +3137,7 @@ $(document).ready(function(){
 					// Storefront-front only
 					case /^\/$/.test(window.location.pathname):
 						add_carousel_descriptions();
+						add_affordable_button();
 						break;
 				}
 
