dc2b9aec1372f89a79b0e8a015563ddb1307bc41
Jason Shackles <jshackles@gmail.com>
2013-09-24 12:56:03 -0700

    fixes a bug in comparison links

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -2781,10 +2781,10 @@ function start_friend_activity_highlights() {
 			if (appid && !node.classList.contains("blotter_userstats_game")) {
 				if (selector == ".blotter_author_block a") { $(node).addClass("inline_tags"); }
 				if (selector == ".blotter_daily_rollup_line a") {
-					if ($(node).parent().parent().html().match(/<img/)) {
+					if ($(node).parent().parent().html().match(/<img src="(.+apps.+)"/)) {						
 						add_achievement_comparison_link($(node).parent().parent()); 
 					}
-				}	
+				}
 				
 				on_app_info(appid, function(){
 					highlight_app(appid, node);					
