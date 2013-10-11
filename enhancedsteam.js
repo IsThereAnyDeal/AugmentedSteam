a2abd7dd8d563708d85c223d0b2aebc5d7276f2b
Jason Shackles <jshackles@gmail.com>
2013-10-11 14:16:20 -0700

    Adds platform icon to wishlist page games

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -1684,14 +1684,14 @@ function add_community_profile_links() {
 	});
 }
 
-// Places an "Add to Cart" button on wishlist items
-function add_cart_on_wishlist() {
+function appdata_on_wishlist() {
 	xpath_each("//a[contains(@class,'btn_visit_store')]", function (node) {
 		var app = get_appid(node.href);
 		get_http('//store.steampowered.com/api/appdetails/?appids=' + app, function (data) {
 			var storefront_data = JSON.parse(data);
 			$.each(storefront_data, function(appid, app_data) {
 				if (app_data.success) {
+					// Add "Add to Cart" button
 					if (app_data.data.packages && app_data.data.packages[0]) {
 						var htmlstring = '<form name="add_to_cart_' + app_data.data.packages[0] + '" action="http://store.steampowered.com/cart/" method="POST">';
 						htmlstring += '<input type="hidden" name="snr" value="1_5_9__403">';
@@ -1700,11 +1700,26 @@ function add_cart_on_wishlist() {
 						htmlstring += '</form>';
 						$(node).before('</form>' + htmlstring + '<a href="#" onclick="document.forms[\'add_to_cart_' + app_data.data.packages[0] + '\'].submit();" class="btn_visit_store">' + localized_strings[language].add_to_cart + '</a>  ');
 					}
+					
+					// Adds platform information
+					if (app_data.data.platforms) {
+						var htmlstring = "";
+						var platforms = 0;
+						if (app_data.data.platforms.windows) { htmlstring += "<span class='platform_img win'></span>"; platforms += 1; }
+						if (app_data.data.platforms.mac) { htmlstring += "<span class='platform_img mac'></span>"; platforms += 1; }
+						if (app_data.data.platforms.linux) { htmlstring += "<span class='platform_img linux'></span>"; platforms += 1; }
+						
+						if (platforms > 1) { htmlstring = "<span class='platform_img steamplay'></span>" + htmlstring; }
+						
+						$(node).parent().parent().parent().find(".bottom_controls").append(htmlstring);
+					}
 				}
 			});
 		});
 	});
 }
+
+
 
 function add_cart_to_search() {
 	$("#search_header").prepend('<div class="col search_released">Actions</div>');
@@ -3557,7 +3572,7 @@ $(document).ready(function(){
 				
 				switch (true) {
 					case /^\/(?:id|profiles)\/.+\/wishlist/.test(window.location.pathname):
-						add_cart_on_wishlist();
+						appdata_on_wishlist();
 						fix_wishlist_image_not_found();
 						add_empty_wishlist_button();
 						add_empty_owned_wishlist_button();
