bfa63dba372ae657d40ff049505c4427c22aa759
Jason Shackles <jshackles@gmail.com>
2014-02-14 15:43:45 -0800

    fix errors for games with forbidden achievements

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -3581,38 +3581,40 @@ function add_achievement_section(appid) {
 			var store_language = cookie.match(/language=([a-z]+)/i)[1];
 
 			get_http("http://api.enhancedsteam.com/steamapi/GetSchemaForGame/?appid=" + appid + "&language=" + store_language, function(txt) {
-				var data = JSON.parse(txt);
-				if (data["game"]["availableGameStats"] && data["game"]["availableGameStats"]["achievements"]) {
-					total_achievements = data["game"]["availableGameStats"]["achievements"].length;
-					if (data["game"]["availableGameStats"]["achievements"][0]) { 
-						icon1 = data["game"]["availableGameStats"]["achievements"][0]["icon"]; 
-						titl1 = data["game"]["availableGameStats"]["achievements"][0]["displayName"];
-						if (data["game"]["availableGameStats"]["achievements"][0]["description"]) desc1 = data["game"]["availableGameStats"]["achievements"][0]["description"];
-					}
-					if (data["game"]["availableGameStats"]["achievements"][1]) {
-						icon2 = data["game"]["availableGameStats"]["achievements"][1]["icon"];
-						titl2 = data["game"]["availableGameStats"]["achievements"][1]["displayName"];
-						if (data["game"]["availableGameStats"]["achievements"][1]["description"]) desc2 = data["game"]["availableGameStats"]["achievements"][1]["description"];
-					}
-					if (data["game"]["availableGameStats"]["achievements"][2]) {
-						icon3 = data["game"]["availableGameStats"]["achievements"][2]["icon"];
-						titl3 = data["game"]["availableGameStats"]["achievements"][2]["displayName"];
-						if (data["game"]["availableGameStats"]["achievements"][2]["description"]) desc3 = data["game"]["availableGameStats"]["achievements"][2]["description"];
-					}
-					if (data["game"]["availableGameStats"]["achievements"][3]) {
-						icon4 = data["game"]["availableGameStats"]["achievements"][3]["icon"];
-						titl4 = data["game"]["availableGameStats"]["achievements"][3]["displayName"];
-						if (data["game"]["availableGameStats"]["achievements"][3]["description"]) desc4 = data["game"]["availableGameStats"]["achievements"][3]["description"];
-					}
-					html = "</div><div class='rule'></div><div class='block_content_inner'>" + localized_strings[language].achievements.includes.replace("__num__", total_achievements) + "<div class='es_communitylink_achievement_images' style='margin-bottom: 4px; margin-top: 4px;'>";
-					if (icon1) html += "<img src='" + icon1 + "' class='es_communitylink_achievement' title='" + titl1 + "&#13;" + desc1 + "'>";
-					if (icon2) html += "<img src='" + icon2 + "' class='es_communitylink_achievement' title='" + titl2 + "&#13;" + desc2 + "'>";
-					if (icon3) html += "<img src='" + icon3 + "' class='es_communitylink_achievement' title='" + titl3 + "&#13;" + desc3 + "'>";
-					if (icon4) html += "<img src='" + icon4 + "' class='es_communitylink_achievement' title='" + titl4 + "&#13;" + desc4 + "'>";
+				if (txt) {
+					var data = JSON.parse(txt);
+					if (data["game"]["availableGameStats"] && data["game"]["availableGameStats"]["achievements"]) {
+						total_achievements = data["game"]["availableGameStats"]["achievements"].length;
+						if (data["game"]["availableGameStats"]["achievements"][0]) { 
+							icon1 = data["game"]["availableGameStats"]["achievements"][0]["icon"]; 
+							titl1 = data["game"]["availableGameStats"]["achievements"][0]["displayName"];
+							if (data["game"]["availableGameStats"]["achievements"][0]["description"]) desc1 = data["game"]["availableGameStats"]["achievements"][0]["description"];
+						}
+						if (data["game"]["availableGameStats"]["achievements"][1]) {
+							icon2 = data["game"]["availableGameStats"]["achievements"][1]["icon"];
+							titl2 = data["game"]["availableGameStats"]["achievements"][1]["displayName"];
+							if (data["game"]["availableGameStats"]["achievements"][1]["description"]) desc2 = data["game"]["availableGameStats"]["achievements"][1]["description"];
+						}
+						if (data["game"]["availableGameStats"]["achievements"][2]) {
+							icon3 = data["game"]["availableGameStats"]["achievements"][2]["icon"];
+							titl3 = data["game"]["availableGameStats"]["achievements"][2]["displayName"];
+							if (data["game"]["availableGameStats"]["achievements"][2]["description"]) desc3 = data["game"]["availableGameStats"]["achievements"][2]["description"];
+						}
+						if (data["game"]["availableGameStats"]["achievements"][3]) {
+							icon4 = data["game"]["availableGameStats"]["achievements"][3]["icon"];
+							titl4 = data["game"]["availableGameStats"]["achievements"][3]["displayName"];
+							if (data["game"]["availableGameStats"]["achievements"][3]["description"]) desc4 = data["game"]["availableGameStats"]["achievements"][3]["description"];
+						}
+						html = "</div><div class='rule'></div><div class='block_content_inner'>" + localized_strings[language].achievements.includes.replace("__num__", total_achievements) + "<div class='es_communitylink_achievement_images' style='margin-bottom: 4px; margin-top: 4px;'>";
+						if (icon1) html += "<img src='" + icon1 + "' class='es_communitylink_achievement' title='" + titl1 + "&#13;" + desc1 + "'>";
+						if (icon2) html += "<img src='" + icon2 + "' class='es_communitylink_achievement' title='" + titl2 + "&#13;" + desc2 + "'>";
+						if (icon3) html += "<img src='" + icon3 + "' class='es_communitylink_achievement' title='" + titl3 + "&#13;" + desc3 + "'>";
+						if (icon4) html += "<img src='" + icon4 + "' class='es_communitylink_achievement' title='" + titl4 + "&#13;" + desc4 + "'>";
 
-					html += "</div><a class='linkbar' href='http://steamcommunity.com/my/stats/" + appid + "'><div class='rightblock'><img src='http://cdn4.store.steampowered.com/public/images/ico/ico_achievements.png' width='24' height='16' border='0' align='top'></div>" + localized_strings[language].achievements.view_all + "</a></div>";
+						html += "</div><a class='linkbar' href='http://steamcommunity.com/my/stats/" + appid + "'><div class='rightblock'><img src='http://cdn4.store.steampowered.com/public/images/ico/ico_achievements.png' width='24' height='16' border='0' align='top'></div>" + localized_strings[language].achievements.view_all + "</a></div>";
 
-					$(".friend_blocks_twoxtwo:last").parent().after(html);
+						$(".friend_blocks_twoxtwo:last").parent().after(html);
+					}
 				}
 			});
 		}
