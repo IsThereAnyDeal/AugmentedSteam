d499b5c9b728a91810e13aecfea3ec5ac8f0a0d6
jshackles <jshackles@gmail.com>
2014-02-16 12:51:36 -0800

    Prep 6.0

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -1,5 +1,5 @@
-// version 5.9
-var version = "5.9"
+// version 6.0
+var version = "6.0"
 
 var console_info=["%c Enhanced %cSteam v"+version+" by jshackles %c http://www.enhancedsteam.com ","background: #000000;color: #7EBE45", "background: #000000;color: #ffffff",""];
 console.log.apply(console,console_info);
