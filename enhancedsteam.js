babd4d0998a0d07a842a0c0943c5bff90e08e872
Jason Shackles <jshackles@gmail.com>
2014-05-02 13:00:00 -0700

    Addresses #471, adds loading dialog to bg select
    Will implement a better fix later, this will help for now.

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -5429,12 +5429,16 @@ function add_es_background_selection() {
 				steam64 = steam64.match(/g_steamID = \"(.+)\";/)[1];
 				var html = "<form id='es_profile_bg' method='POST' action='http://www.enhancedsteam.com/gamedata/profile_bg_save.php'><div class='group_content group_summary'>";
 				html += "<input type='hidden' name='steam64' value='" + steam64 + "'>";
-				html += "<div class='formRow'><div class='formRowFields'><div class='profile_background_current'><div class='profile_background_current_img_ctn'>";
+				html += "<div class='formRow'><div class='formRowFields'><div class='profile_background_current'><div class='profile_background_current_img_ctn'><div class='es_loading'><img src='http://cdn.steamcommunity.com/public/images/login/throbber.gif'><span>"+ localized_strings[language].loading +"</div>";
 				html += "<img id='es_profile_background_current_image' src=''>";
-				html += "</div><div class='profile_background_current_description'><div id='profile_background_current_name'><select name='es_background' id='es_background' class='gray_bevel dynInput' onchange=\"function image(obj){index=obj.selectedIndex; document.getElementById('es_profile_background_current_image').src=obj.options[index].id; } image(this);\"><option value='0' id='http://www.enhancedsteam.com/gamedata/icons/smallblacksquare.jpg'>None Selected / No Change</option>";
+				html += "</div><div class='profile_background_current_description'><div id='es_profile_background_current_name'>";
+				html += "</div></div><div style='clear: left;'></div><div class='background_selector_launch_area'></div></div><div class='background_selector_launch_area'>&nbsp;<div style='float: right;'><span id='es_background_save_btn' class='btn_grey_white_innerfade btn_small btn_disabled'><span>" + localized_strings[language].save + "</span></span></div></div><div class='formRowTitle'>" + localized_strings[language].custom_background + ":<span class='formRowHint' title='" + localized_strings[language].custom_background_help + "'>(?)</span></div></div></div>";
+				html += "</form>";
+				$(".group_content_bodytext").before(html);
 
 				get_http("http://api.enhancedsteam.com/profile-select/?steam64=" + steam64, function (txt) {
 					var data = JSON.parse(txt);
+					var select_html = "<select name='es_background' id='es_background' class='gray_bevel dynInput' onchange=\"function image(obj){index=obj.selectedIndex; document.getElementById('es_profile_background_current_image').src=obj.options[index].id; } image(this);\"><option value='0' id='http://www.enhancedsteam.com/gamedata/icons/smallblacksquare.jpg'>None Selected / No Change</option>";
 
 					var array = [];
 					for (var key in data["backgrounds"]) {
@@ -5450,16 +5454,19 @@ function add_es_background_selection() {
 
 					$.each(array, function(index, value) {
 						if (value["selected"]) {
-							html += "<option id='" + escapeHTML(value['id'].toString()) + "' value='" + escapeHTML(value['index'].toString()) + "' SELECTED>" + escapeHTML(value['text'].toString()) + "</option>";
+							select_html += "<option id='" + escapeHTML(value['id'].toString()) + "' value='" + escapeHTML(value['index'].toString()) + "' SELECTED>" + escapeHTML(value['text'].toString()) + "</option>";
 						} else {
-							html += "<option id='" + escapeHTML(value['id'].toString()) + "' value='" + escapeHTML(value['index'].toString()) + "'>" + escapeHTML(value['text'].toString()) + "</option>";
+							select_html += "<option id='" + escapeHTML(value['id'].toString()) + "' value='" + escapeHTML(value['index'].toString()) + "'>" + escapeHTML(value['text'].toString()) + "</option>";
 						}
 					});
 
-					html += "</select></div></div><div style='clear: left;'></div><div class='background_selector_launch_area'></div></div><div class='background_selector_launch_area'>&nbsp;<div style='float: right;'><span class='btn_grey_white_innerfade btn_small' onclick=\"document.getElementById('es_profile_bg').submit()\"><span>" + localized_strings[language].save + "</span></span></div></div><div class='formRowTitle'>" + localized_strings[language].custom_background + ":<span class='formRowHint' title='" + localized_strings[language].custom_background_help + "'>(?)</span></div></div></div>";
-					html += "</form>";
-
-					$(".group_content_bodytext").before(html);
+					select_html += "</select>";
+					$(".es_loading").remove();
+					$("#es_background_save_btn").removeClass("btn_disabled");
+					$("#es_background_save_btn").click(function(e) {
+						$("#es_profile_bg").submit();
+					});
+					$("#es_profile_background_current_name").html(select_html);
 
 					get_http("http://api.enhancedsteam.com/profile-small/?steam64=" + steam64, function (txt) {
 						$("#es_profile_background_current_image").attr("src", escapeHTML(txt));
