a02c035bd2f515820fa3df45f7009aaa36ac737f
jshackles <jshackles@gmail.com>
2014-06-19 15:40:22 -0700

    Add Early access banners to summer sale homepage

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -4282,6 +4282,7 @@ function process_early_access() {
 									$(".special_tiny_cap").each(function(index, value) { check_early_access($(this), "ea_sm_120.png", 0); });
 									$(".game_capsule").each(function(index, value) { check_early_access($(this), "ea_sm_120.png", 0); });
 									$(".cluster_capsule").each(function(index, value) { check_early_access($(this), "ea_467x181.png", 0); });
+									$(".summersale_dailydeal").each(function(index, value) { check_early_access($(this), "ea_231x87.png", 0); });
 									break;
 							}
 						case "steamcommunity.com":
