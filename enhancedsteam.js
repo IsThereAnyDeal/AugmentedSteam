5eb9449af6945ac0e4ca097eb94a685a61aba047
Jason Shackles <jshackles@gmail.com>
2014-09-11 15:02:08 -0700

    ADD: Basic support for Turkish Lira

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -117,6 +117,9 @@ function formatCurrency(number, type) {
 		case "KRW":
 			places = 2; symbol = "₩"; thousand = ","; decimal = "."; right = false;
 			break;
+		case "TRY":
+			places = 2; symbol = " TL"; thousand = ""; decimal = ","; right = true;
+			break;
 		default:
 			places = 2; symbol = "$"; thousand = ","; decimal = "."; right = false;
 			break;
@@ -160,6 +163,8 @@ function currency_symbol_to_type (currency_symbol) {
 			return "VND";
 		case "₩":
 			return "KRW";
+		case "TL":
+			return "TRY";
 		default:
 			return "USD";
 	}
@@ -167,8 +172,8 @@ function currency_symbol_to_type (currency_symbol) {
 
 function currency_symbol_from_string (string_with_symbol) {
 	var return_string = "";
-	if (string_with_symbol.match(/(?:R\$|S\$|\$|RM|kr|Rp|€|¥|£|฿|pуб|P|₫|₩)/)) {
-		return_string = string_with_symbol.match(/(?:R\$|S\$|\$|RM|kr|Rp|€|¥|£|฿|pуб|P|₫|₩)/)[0];
+	if (string_with_symbol.match(/(?:R\$|S\$|\$|RM|kr|Rp|€|¥|£|฿|pуб|P|₫|₩|TL)/)) {
+		return_string = string_with_symbol.match(/(?:R\$|S\$|\$|RM|kr|Rp|€|¥|£|฿|pуб|P|₫|₩|TL)/)[0];
 	}
 	return return_string;
 }
