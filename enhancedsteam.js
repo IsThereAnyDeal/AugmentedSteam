ba71118019651db12feaa4b14e874c1eb46378bb
jshackles <jshackles@gmail.com>
2015-01-17 14:59:39 -0800

    FIX: system requirements drm lookup
    For example, this fixes the DRM warning on GTA V.

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -1987,7 +1987,7 @@ function drm_warnings(type) {
 			var drm;
 
 			var text = $("#game_area_description").html();
-			text += $("#game_area_sys_req").html();
+			text += $(".game_area_sys_req").html();
 			text += $("#game_area_legal").html();
 			text += $(".game_details").html();
 			text += $(".DRM_notice").html();
