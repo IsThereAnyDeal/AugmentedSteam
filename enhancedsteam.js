4247e870066c4857ddd655805594ff233b313aa2
Jason Shackles <jshackles@gmail.com>
2015-07-22 07:47:48 -0700

    UPDATE: Survey code spacing and formatting

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -3883,7 +3883,7 @@ function survey_data_from_site(appid) {
 					case "30": html += "<span style='color: #8f0e10;'>fixed at 30fps or lower</span>"; break;
 				 	case "fi": html += "<span style='color: #e1c48a;'>fixed at higher than 30fps</span>"; break;
 				 	case "va": html += "<span style='color: #8BC53F;'>variable, dependant on hardware</span>"; break;
-				}				
+				}
 				html += "<br><b>Resolution</b>: This game appears to support "
 				switch (data["mr"]) {
 					case "less": html += "<span style='color: #8f0e10;'>less than 1920x1080 resolution</span>"; break;
@@ -3900,7 +3900,6 @@ function survey_data_from_site(appid) {
 				if (data["nvidia"] || data["amd"] || data["intel"] || data["other"]) {
 					html += "<p><b>Performance Satisfaction by GPU</b>:";
 					html += "<div class='performance-graph'>";
-					data["nvidia"]=91;
 					if (data["nvidia"]) {
 						if (data["nvidia"] > 90) {
 							html += "<div class='row'><div class='left-bar nvidia' style='width: " + parseInt(data["nvidia"]).toString() + "%;'><span>Nvidia " + data["nvidia"] + "%</span></div><div class='right-bar' style='width: " + parseInt(100-data["nvidia"]) + "%;'></div></div>";
@@ -3914,13 +3913,13 @@ function survey_data_from_site(appid) {
 						} else {
 							html += "<div class='row'><div class='left-bar amd' style='width: " + parseInt(data["amd"]).toString() + "%;'><span>AMD</span></div><div class='right-bar' style='width: " + parseInt(100-data["amd"]) + "%'><span>" + data["amd"] + "%</span></div></div>";
 						}
-					}					
+					}
 					if (data["intel"]) {
 						if (data["intel"] > 90) {
 							html += "<div class='row'><div class='left-bar intel' style='width: " + parseInt(data["intel"]).toString() + "%;'><span>Intel " + data["intel"] + "%</span></div><div class='right-bar' style='width: " + parseInt(100-data["intel"]) + "%'></div></div>";
 						} else {
 							html += "<div class='row'><div class='left-bar intel' style='width: " + parseInt(data["intel"]).toString() + "%;'><span>Intel</span></div><div class='right-bar' style='width: " + parseInt(100-data["intel"]) + "%'><span>" + data["intel"] + "%</span></div></div>";
-						}	
+						}
 					}
 					if (data["other"]) {
 						if (data["other"] > 90) {
