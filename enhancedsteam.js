9553abccd3e15b31d3da493e20ef38942222aaea
Dan C <spo0kye@yahoo.com>
2017-01-23 22:30:02 +0200

    Small optimization

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -7289,10 +7289,12 @@ function customize_home_page() {
 		function addToggleHandler(name, element) {
 			var obj = {};
 			obj[name] = settings[name];
-			$("body").toggleClass("es_" + name.replace("show_", "") + "_hidden", !obj[name]);
+			$("body").toggleClass(name.replace("es_", "") + "_hidden", !obj[name]);
 			
 			$("#" + name).click(function() {
 				element.removeClass("es_hide").removeClass("es_show");
+				$("body").toggleClass(name.replace("es_", "") + "_hidden", !obj[name]);
+				
 				if (obj[name]) {
 					obj[name] = false;
 					element.stop().slideUp();
@@ -7305,7 +7307,7 @@ function customize_home_page() {
 					element.stop().slideDown();
 					$(this).find(".home_viewsettings_checkbox").addClass("checked");
 				}
-				$("body").toggleClass("es_" + name.replace("show_", "") + "_hidden", !obj[name]);
+				
 				storage.set(obj);
 			});
 		}
