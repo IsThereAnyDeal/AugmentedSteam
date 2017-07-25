948b87dab936e2d001dcb491515331c1bcee00f8
luchaos <lcs@luchaos.at>
2017-07-25 21:56:43 +0200

    astats explicit http link - no https available yet

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -2849,7 +2849,7 @@ function add_community_profile_links() {
 				"name": "Backpack.tf",
 			},
 			"astatsnl": {
-				"link": `//astats.astats.nl/astats/User_Info.php?steamID64=${ steamID }`,
+				"link": `http://astats.astats.nl/astats/User_Info.php?steamID64=${ steamID }`,
 				"name": "AStats.nl",
 			}
 		};
