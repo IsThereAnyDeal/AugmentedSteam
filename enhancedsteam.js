3dfa4c7c15bd78e5720674477cb20d8e7711ea42
Jason Shackles <jshackles@gmail.com>
2017-08-29 10:24:53 -0700

    FIX: disappearing sell buttons
    Previously the code would perform a .replaceWith() over the existing
    sell button, and this caused some users under certain circumstances to
    have this entire section get overwrote with "".  The new code will
    simply stylize the existing button while assigning a new DOM id.

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -5191,8 +5191,10 @@ function inventory_market_helper(response) {
 				if (settings.quickinv_diff === undefined) { settings.quickinv_diff = -0.01; storage.set({'quickinv_diff': settings.quickinv_diff}); }
 				if (settings.quickinv) {
 					if (marketable && contextID == 6 && global_id == 753) {
-						// Replace the "Sell" button
-						$sideMarketActs.find("a.item_market_action_button").replaceWith("<a class='btn_small btn_green_white_innerfade es_market_btn' id='es_sell' href='javascript:SellCurrentSelection()'><span>" + $sideMarketActs.find(".item_market_action_button_contents").text() + "</span></a>");
+						// Restyle the existing "Sell" button
+						$sideMarketActs.find("a.item_market_action_button").removeClass().addClass("btn_small btn_green_white_innerfade es_market_btn").attr("id", "es_sell");
+						$("#es_sell").find("span.item_market_action_button_left, span.item_market_action_button_right, span.item_market_action_button_preload").hide();
+						$("#es_sell").find("span.item_market_action_button_contents").removeClass();
 
 						if (!$(thisItem).hasClass("es-loading")) {
 							var url = $sideMarketActs.find("a")[0].href;
