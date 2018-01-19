55f1d4ebe2e73d1101b83850dea7d3675c2de54d
luchaos <lcs@luchaos.at>
2018-01-19 16:55:35 +0100

    astats schema agnostic link - https available!

--- enhancedsteam.js
+++ enhancedsteam.js
@@ -2901,7 +2901,7 @@ function add_community_profile_links() {
 				"name": "Backpack.tf",
 			},
 			"astatsnl": {
-				"link": `http://astats.astats.nl/astats/User_Info.php?steamID64=${ steamID }`,
+				"link": `//astats.astats.nl/astats/User_Info.php?steamID64=${ steamID }`,
 				"name": "AStats.nl",
 			}
 		};
