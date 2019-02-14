8e318dcbb8a30048399c80b5cfb6514fafb845b3
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2019-02-14 16:16:29 +0100

    Fixed crash with "Hide 'About' link"
    The extension crashed if the "About" menu item, which seems to only appear on certain clients, wasn't found

--- js/common.js
+++ js/common.js
@@ -1333,7 +1333,12 @@ let EnhancedSteam = (function() {
 
     self.removeAboutMenu = function(){
         if (!SyncedStorage.get("hideaboutmenu")) { return; }
-        document.querySelector(".menuitem[href='https://store.steampowered.com/about/']").remove();
+		
+		let aboutMenu = document.querySelector(".menuitem[href='https://store.steampowered.com/about/']");
+		
+		if (aboutMenu == null) { return; }
+		
+        aboutMenu.remove();
     };
 
     self.addHeaderLinks = function(){
