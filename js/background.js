33e6ec657e39609efcd425479631d262654022f1
Karl Castle <karl.castle@gmail.com>
2019-02-15 23:26:44 +1030

    Created Background actions for dynamicstore

--- js/background.js
+++ js/background.js
@@ -44,17 +44,65 @@ const LocalStorage = (function(){
 const Steam = (function() {
     let self = {};
 
+    /**
+     * Requires user to be signed in, can we validate this from background?
+     */
+    async function _dynamicstore() {
+        // Is data already in scope because of previous request?
+        if (_dynamicstore.data && !_dynamicstore.isExpired()) { return _dynamicstore.data; }
+
+        // Is a request in progress?
+        if (_dynamicstore.promise) { return _dynamicstore.promise; }
+        
+        // Get data from localStorage
+        { 'data': _dynamicstore.data, 'timestamp': _dynamicstore.timestamp, } = LocalStorage.get('dynamicstore');
+        if (_dynamicstore.data && !_dynamicstore.isExpired()) { return _dynamicstore.data; }
+
+        // Cache expired, need to fetch
+        let url = "https://store.steampowered.com/dynamicstore/userdata/";
+        let p = {
+            'method': 'GET',
+            'credentials': 'include',
+        };
+        _dynamicstore.promise = fetch(url, p)
+            .then((response => { 'data': response.json(), 'timestamp': TimeHelper.timestamp(), }))
+            .then(function(dynamicstore) {
+                if (!dynamicstore.data.rgOwnedApps) {
+                    throw "Could not fetch DynamicStore UserData";
+                }
+                LocalStorage.set("dynamicstore", dynamicstore);
+                { 'data': _dynamicstore.data, 'timestamp': _dynamicstore.timestamp, } = dynamicstore;
+                _dynamicstore.promise = null; // no request in progress
+                return dynamicstore.data;
+            })
+            ;
+        return _dynamicstore.promise;
+    }       
+    // _dynamicstore.data keys are:
+    // "rgWishlist", "rgOwnedPackages", "rgOwnedApps", "rgPackagesInCart", "rgAppsInCart"
+    // "rgRecommendedTags", "rgIgnoredApps", "rgIgnoredPackages", "rgCurators", "rgCurations"
+    // "rgCreatorsFollowed", "rgCreatorsIgnored", "preferences", "rgExcludedTags",
+    // "rgExcludedContentDescriptorIDs", "rgAutoGrantApps"
+    _dynamicstore.data = null;
+    _dynamicstore.timestamp = 0;
+    _dynamicstore.EXPIRY = 15 * 60; // dynamicstore userdata expires after 15 minutes
+    _dynamicstore.isExpired = function() { return TimeHelper.isExpired(this.timestamp, this.EXPIRY); };
+    _dynamicstore.promise = null;
+
     self.ignored = async function() {
-      
+        return _dynamicstore().then(userdata => Object.keys(userdata.rgIgnoredApps));
     };
     self.owned = async function() {
-      
+        return _dynamicstore().then(userdata => userdata.rgOwnedApps);       
     };
     self.wishlist = async function() {
-      
+        return _dynamicstore().then(userdata => userdata.rgWishlist);        
     };
     self.clearDynamicStore = async function() {
-
+        LocalStorage.remove('dynamicstore');
+        _dynamicstore.data = null;
+        _dynamicstore.timestamp = 0;
+        _dynamicstore.promise = null;
     };
 
     Object.freeze(self);
@@ -64,8 +112,10 @@ const Steam = (function() {
 
 
 let actionCallbacks = new Map([
+    ['ignored', Steam.ignored],
+    ['owned', Steam.owned],
     ['wishlist', Steam.wishlist],
-    ['wishlist.clear', Steam.clearDynamicStore],
+    ['dynamicstore.clear', Steam.clearDynamicStore],
 
 ]);
 // new Map() for Map.prototype.get() in lieu of:
