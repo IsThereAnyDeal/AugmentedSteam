1b514a6292325fa9c9b8a40b7ab0917114c904fa
Karl Castle <karl.castle@gmail.com>
2019-02-20 20:50:12 +1030

    Simplify initializer of typeToNumberMap

--- js/common.js
+++ js/common.js
@@ -843,7 +843,7 @@ let Currency = (function() {
         "$U": "UYU"
     };
 
-    let typeToNumberMap = {
+    const typeToNumberMap = {
         "RUB": 5,
         "EUR": 3,
         "GBP": 2,
@@ -882,46 +882,13 @@ let Currency = (function() {
         "CRC": 40,
         "UYU": 41
     };
-
-    let numberToTypeMap = {
-        5: "RUB",
-        3: "EUR",
-        2: "GBP",
-        6: "PLN",
-        7: "BRL",
-        8: "JPY",
-        9: "NOK",
-        10: "IDR",
-        11: "MYR",
-        12: "PHP",
-        13: "SGD",
-        14: "THB",
-        15: "VND",
-        16: "KRW",
-        17: "TRY",
-        18: "UAH",
-        19: "MXN",
-        20: "CAD",
-        21: "AUD",
-        22: "NZD",
-        23: "CNY",
-        24: "INR",
-        25: "CLP",
-        26: "PEN",
-        27: "COP",
-        28: "ZAR",
-        29: "HKD",
-        30: "TWD",
-        31: "SAR",
-        32: "AED",
-        34: "ARS",
-        35: "ILS",
-        37: "KZT",
-        38: "KWD",
-        39: "QAR",
-        40: "CRC",
-        41: "UYU"
-    };
+    Object.freeze(typeToNumberMap);
+    
+    const numberToTypeMap = {};
+    for (let [abbr, num] of Object.entries(typeToNumberMap)) {
+        numberToTypeMap[num] = abbr;
+    }
+    Object.freeze(numberToTypeMap);
 
     let _rates = {};
     let _promise = null;
