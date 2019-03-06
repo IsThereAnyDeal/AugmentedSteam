e896b5074dfeae1c35d06cc52ac277c128135247
Karl Castle <karl.castle@gmail.com>
2019-03-06 20:45:11 +1030

    Add SyncedStorage to core.js

--- js/core.js
+++ js/core.js
@@ -89,6 +89,206 @@ class LocalStorage {
 }
 
 
+class SyncedStorage {
+    // static adapter = chrome.storage.sync || chrome.storage.local;
+    // static cache = {};
+
+    /**
+     * chrome.storage.sync limits
+     * QUOTA_BYTES = 102400 // 100KB
+     * QUOTA_BYTES_PER_ITEM = 8192 // 8KB
+     * MAX_ITEMS = 512
+     * MAX_WRITE_OPERATIONS_PER_HOUR = 1800
+     * MAX_WRITE_OPERATIONS_PER_MINUTE = 120
+     */
+    static get(key) {
+        if (typeof this.cache[key] == 'undefined') {
+            return this.defaults[key];
+        }
+        return this.cache[key];
+    }
+
+    static set(key, value) {
+        this.cache[key] = value;
+        this.adapter.set({ [key]: value, });
+    }
+
+    static remove(key) {
+        if (typeof this.cache[key]) {
+            delete this.cache[key];
+        }
+        this.adapter.remove(key);
+    }
+
+    static clear() {
+        this.cache = {};
+        this.adapter.clear();
+    }
+
+    // load whole storage and make local copy
+    static async load() {
+        let that = this;
+        let storage = await new Promise((resolve, reject) => that.adapter.get(null, result => resolve(result)));
+        Object.assign(that.cache, storage);
+        return that.cache;
+    }
+}
+SyncedStorage.adapter = chrome.storage.sync || chrome.storage.local;
+SyncedStorage.cache = {};
+SyncedStorage.defaults = {
+    'language': "english",
+    'highlight_owned_color': "#598400",
+    'highlight_wishlist_color': "#1483ad",
+    'highlight_coupon_color': "#a26426",
+    'highlight_inv_gift_color': "#800040",
+    'highlight_inv_guestpass_color': "#513c73",
+    'highlight_notinterested_color': "#4f4f4f",
+
+    'tag_owned_color': "#5c7836",
+    'tag_wishlist_color': "#0d80bd",
+    'tag_coupon_color': "#c27120",
+    'tag_inv_gift_color': "#b10059",
+    'tag_inv_guestpass_color': "#65449a",
+    'tag_notinterested_color': "#4f4f4f",
+
+    'highlight_owned': true,
+    'highlight_wishlist': true,
+    'highlight_coupon': false,
+    'highlight_inv_gift': false,
+    'highlight_inv_guestpass': false,
+    'highlight_notinterested': false,
+    'highlight_excludef2p': false,
+    'highlight_notdiscounted': false,
+
+    'tag_owned': false,
+    'tag_wishlist': false,
+    'tag_coupon': false,
+    'tag_inv_gift': false,
+    'tag_inv_guestpass': false,
+    'tag_notinterested': true,
+    'tag_short': false,
+
+    'hide_owned': false,
+    'hide_ignored': false,
+    'hide_dlcunownedgames': false,
+    'hide_wishlist': false,
+    'hide_cart': false,
+    'hide_notdiscounted': false,
+    'hide_mixed': false,
+    'hide_negative': false,
+    'hide_priceabove': false,
+    'priceabove_value': "",
+    'hidetmsymbols': false,
+
+    'showlowestprice': true,
+    'showlowestprice_onwishlist': true,
+    'showlowestpricecoupon': true,
+    'showallstores': true,
+    'stores': [],
+    'override_price': "auto",
+    'showregionalprice': "mouse",
+    'regional_countries': ["us", "gb", "eu1", "ru", "br", "au", "jp"],
+
+    'show_featuredrecommended': true,
+    'show_specialoffers': true,
+    'show_trendingamongfriends': true,
+    'show_browsesteam': true,
+    'show_curators': true,
+    'show_morecuratorrecommendations': true,
+    'show_recentlyupdated': true,
+    'show_fromdevelopersandpublishersthatyouknow': true,
+    'show_popularvrgames': true,
+    'show_gamesstreamingnow': true,
+    'show_under': true,
+    'show_updatesandoffers': true,
+    'show_es_discoveryqueue': true,
+    'show_es_homepagetabs': true,
+    'show_es_homepagesidebar': true,
+    'showmarkettotal': false,
+    'showsteamrepapi': true,
+    'showmcus': true,
+    'showoc': true,
+    'showhltb': true,
+    'showpcgw': true,
+    'showclient': true,
+    'showsteamcardexchange': false,
+    'showitadlinks': true,
+    'showsteamdb': true,
+    'showastatslink': true,
+    'showwsgf': true,
+    'exfgls': true,
+    'show_apppage_reviews': true,
+    'show_apppage_about': true,
+    'show_apppage_surveys': true,
+    'show_apppage_sysreq': true,
+    'show_apppage_legal': true,
+    'show_apppage_morelikethis': true,
+    'show_apppage_recommendedbycurators': true,
+    'show_apppage_customerreviews': true,
+    'show_keylol_links': false,
+    'show_package_info': false,
+    'show_sysreqcheck': false,
+    'show_steamchart_info': true,
+    'show_steamspy_info': true,
+    'show_early_access': true,
+    'show_alternative_linux_icon': false,
+    'show_itad_button': false,
+    'skip_got_steam': false,
+
+    'hideinstallsteambutton': false,
+    'hideaboutmenu': false,
+    'keepssachecked': false,
+    'showemptywishlist': true,
+    'wishlist_notes': {},
+    'version_show': true,
+    'replaceaccountname': true,
+    'showfakeccwarning': true,
+    'showlanguagewarning': true,
+    'showlanguagewarninglanguage': "English",
+    'homepage_tab_selection': "remember",
+    'send_age_info': true,
+    'html5video': true,
+    'contscroll': true,
+    'showdrm': true,
+    'regional_hideworld': false,
+    'showinvnav': true,
+    'showesbg': true,
+    'quickinv': true,
+    'quickinv_diff': -0.01,
+    'showallachievements': false,
+    'showachinstore': true,
+    'showcomparelinks': false,
+    'hideactivelistings': false,
+    'hidespamcomments': false,
+    'spamcommentregex': "[\\u2500-\\u25FF]",
+    'wlbuttoncommunityapp': true,
+    'removeguideslanguagefilter': false,
+    'disablelinkfilter': false,
+    'showallfriendsthatown': false,
+    'show1clickgoo': true,
+    'show_profile_link_images': "gray",
+    'profile_steamrepcn': true,
+    'profile_steamgifts': true,
+    'profile_steamtrades': true,
+    'profile_steamrep': true,
+    'profile_steamdbcalc': true,
+    'profile_astats': true,
+    'profile_backpacktf': true,
+    'profile_astatsnl': true,
+    'profile_permalink': true,
+    'profile_custom': false,
+    'profile_custom_name': "Google",
+    'profile_custom_url': "google.com/search?q=[ID]",
+    'profile_custom_icon': "www.google.com/images/branding/product/ico/googleg_lodp.ico",
+    'steamcardexchange': true,
+    'purchase_dates': true,
+    'show_badge_progress': true,
+    'show_wishlist_link': true,
+    'show_wishlist_count': true,
+    'show_progressbar': true,
+};
+
+
 class HTMLParser {
     static clearSpecialSymbols(string) {
         return string.replace(/[\u00AE\u00A9\u2122]/g, "");
