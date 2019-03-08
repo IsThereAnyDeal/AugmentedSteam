6eb81b0d1e676190e1de0ec1777f3ca6f6bf1353
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2019-03-09 00:41:07 +0100

    Set default for show_apppage_surveys

--- js/core.js
+++ js/core.js
@@ -460,6 +460,7 @@ SyncedStorage.defaults = {
     'show_sysreqcheck': false,
     'show_steamchart_info': true,
     'show_steamspy_info': true,
+    'show_apppage_surveys': true,
     'show_early_access': true,
     'show_alternative_linux_icon': false,
     'show_itad_button': false,
