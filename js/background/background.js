c8a15f818897d49681084b8a81be509047a3c273
Karl Castle <karl.castle@gmail.com>
2019-03-09 03:25:19 +1030

    Remove redundant version reporting actions

--- js/background/background.js
+++ js/background/background.js
@@ -42,20 +42,6 @@ class CacheStorage {
 }
 
 
-class Extension {
-    static async version() {
-        return {
-            'cached': LocalStorage.get('version'),
-            'current': Info.version,
-        };
-    }
-
-    static async updateVersion() {
-        LocalStorage.set('version', Info.version);
-    }
-}
-
-
 class Api {
     // FF doesn't support static members
     // static origin; // this *must* be overridden
@@ -651,8 +637,6 @@ let appCacheKey = (params => `app_${params.appid}`);
 let ratesCacheKey = (params => `rates_${params.to}`);
 
 let actionCallbacks = new Map([
-    ['extension.version', Extension.version],
-    ['extension.version.update', Extension.updateVersion],
     ['ignored', Steam.ignored],
     ['owned', Steam.owned],
     ['wishlist', Steam.wishlist],
