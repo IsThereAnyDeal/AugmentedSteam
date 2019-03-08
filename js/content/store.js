2f3b43aae1d314c63a4b51fae7e951ffd0737cba
TcjeenghJaong <wzy960724@gmail.com>
2019-03-08 14:31:02 +0800

    Fix incorrect badge page class selector

--- js/content/store.js
+++ js/content/store.js
@@ -1677,12 +1677,11 @@ let AppPageClass = (function(){
         function loadBadgeContent(targetSelector, result) {
             let dummy = BrowserHelper.htmlToDOM(result);
             
-            // The badges_sheet class only appears when the users badge for this game couldn't get found
-            if (!dummy.querySelector(".badges_sheet")) {
-                let badge = dummy.querySelector(".badge_current");
-                if (badge) {
-                    displayBadgeInfo(targetSelector, badge);
-                }
+            // grap badge and game cards
+            // when there is no badge (e.g. dlc), badge_gamecard_page class won't appear
+            let badge = dummy.querySelector(".badge_gamecard_page");
+            if (badge) {
+                displayBadgeInfo(targetSelector, badge);
             } else {
                 if (document.getElementById("es_badge_progress")) {
                     document.getElementById("es_badge_progress").remove();
@@ -1693,50 +1692,52 @@ let AppPageClass = (function(){
 
         function displayBadgeInfo(targetSelector, badgeNode) {
             let blockSel = document.querySelector(targetSelector);
-            blockSel.append(badgeNode);
 
-            if (!badgeNode.querySelector(".friendPlayerLevelNum")) {
-                let progress;
-                let card_num_owned = badgeNode.querySelectorAll(".badge_detail_tasks .owned").length;
-                let card_num_total = badgeNode.querySelectorAll(".badge_detail_tasks .badge_card_set_card").length;
-                let progress_text_length = (progress = badgeNode.querySelector(".gamecard_badge_progress")) ? progress.textContent.trim().length : 0;
-                let next_level_empty_badge = badgeNode.querySelectorAll(".gamecard_badge_progress .badge_info").length;
-                let badge_completed = (progress_text_length > 0 && next_level_empty_badge == 0);
-                let show_card_num = (card_num_owned > 0 && progress_text_length === 0) || (card_num_owned > 0 && !badge_completed);
-                let is_normal_badge = targetSelector === ".es_normal_badge_progress";
+            // show Steam badge info card
+            let badge = badgeNode.querySelector(".badge_current");
+            blockSel.append(badge);
 
-                if (is_normal_badge || (card_num_owned > 0 || !blockSel.querySelector(".badge_empty_circle"))) {
-                    document.querySelector(".es_badges_progress_block").style.display='block';
-                    blockSel.style.display = "block";
+            // count card
+            let card_num_owned = badgeNode.querySelectorAll(".badge_detail_tasks .owned").length;
+            let card_num_total = badgeNode.querySelectorAll(".badge_detail_tasks .badge_card_set_card").length;
+            // check if badge is completed
+            let progress;
+            let progress_text_length = (progress = badgeNode.querySelector(".gamecard_badge_progress")) ? progress.textContent.trim().length : 0;
+            let next_level_empty_badge = badgeNode.querySelectorAll(".gamecard_badge_progress .badge_info").length;
+            let badge_completed = (progress_text_length > 0 && next_level_empty_badge === 0);
 
-                    let progressBold = badgeNode.querySelector(".progress_info_bold");
+            let show_card_num = (card_num_owned > 0 && !badge_completed);
+            let is_normal_badge = targetSelector === ".es_normal_badge_progress";
+            
+            if (is_normal_badge || (card_num_owned > 0 || !blockSel.querySelector(".badge_empty_circle"))) {
+                document.querySelector(".es_badges_progress_block").style.display = 'block';
+                blockSel.style.display = "block";
 
-                    blockSel.insertAdjacentHTML("beforeend", `
-								<div class="es_cards_numbers">
-									<div class="es_cards_remaining">${progressBold ? progressBold.textContent : ""}</div>
-								</div>
-								<div class="game_area_details_specs">
-									<div class="icon"><img src="//store.steampowered.com/public/images/v6/ico/ico_cards.png" width="24" height="16" border="0" align="top"></div>
-									<a href="//steamcommunity.com/my/gamecards/${ appid + (is_normal_badge ? `/` : `?border=1`) }" class="name">${badge_completed ? Localization.str.view_badge : Localization.str.view_badge_progress}</a>
-								</div>
+                let progressBold = badgeNode.querySelector(".progress_info_bold");
+
+                blockSel.insertAdjacentHTML("beforeend", `
+							<div class="es_cards_numbers">
+								<div class="es_cards_remaining">${progressBold ? progressBold.textContent : ""}</div>
+							</div>
+							<div class="game_area_details_specs">
+								<div class="icon"><img src="//store.steampowered.com/public/images/v6/ico/ico_cards.png" width="24" height="16" border="0" align="top"></div>
+								<a href="//steamcommunity.com/my/gamecards/${ appid + (is_normal_badge ? `/` : `?border=1`) }" class="name">${badge_completed ? Localization.str.view_badge : Localization.str.view_badge_progress}</a>
+							</div>
+						`);
+
+                if (show_card_num) {
+                    blockSel.querySelector(".es_cards_numbers")
+                        .insertAdjacentHTML("beforeend", `
+								<div class="es_cards_owned">${Localization.str.cards_owned.replace("__owned__", card_num_owned).replace("__possible__", card_num_total)}</div>
 							`);
-
-                    if (show_card_num) {
-                        blockSel.querySelector(".es_cards_numbers")
-                            .insertAdjacentHTML("beforeend", `
-									<div class="es_cards_owned">${Localization.str.cards_owned.replace("__owned__", card_num_owned).replace("__possible__", card_num_total)}</div>
-								`);
-                    }
-
-                    let last = blockSel.querySelector(".badge_empty_right div:last-child");
-                    if (last) {
-                        last.classList.add("badge_empty_name");
-                        last.style = "";
-                        last.textContent = Localization.str.badge_not_unlocked;
-                    }
                 }
-            } else {
-                blockSel.remove();
+
+                let last = blockSel.querySelector(".badge_empty_right div:last-child");
+                if (last) {
+                    last.classList.add("badge_empty_name");
+                    last.style = "";
+                    last.textContent = Localization.str.badge_not_unlocked;
+                }
             }
         }
     };
