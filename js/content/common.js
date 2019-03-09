f4d3aa1fb4509637407076bb4624472df5d86607
Tomáš Fedor <tomas.fedor3@gmail.com>
2019-03-09 03:08:46 +0100

    Remove protocol hack for RequestData

--- js/content/common.js
+++ js/content/common.js
@@ -173,11 +173,6 @@ let RequestData = (function(){
                 }
             }
 
-            // TODO (tfedor) this is a hack for firefox, update URLs to use protocol in master and remove this
-            if (url[0] === "/" && url[1] === "/") {
-                url = window.location.protocol + url;
-            }
-
             let request = new XMLHttpRequest();
             request.onreadystatechange = function() { requestHandler(request); };
             request.overrideMimeType("application/json");
@@ -220,11 +215,6 @@ let RequestData = (function(){
                     ProgressBar.failed(null, url, state.status, state.statusText);
                     reject(state.status);
                 }
-            }
-
-            // TODO (tfedor) this is a hack for firefox, update URLs to use protocol in master and remove this
-            if (url[0] === "/" && url[1] === "/") {
-                url = window.location.protocol + url;
             }
 
             let request = new XMLHttpRequest();
