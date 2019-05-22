406adb655cdd85e6b555f7da6e6064a356647885
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2019-05-22 14:14:10 +0200

    Add a button to directly jump to the next discovery queue

--- js/content/store.js
+++ js/content/store.js
@@ -509,6 +509,8 @@ class AppPageClass extends StorePageClass {
         this.initHdPlayer();
         this.addWishlistRemove();
         this.addUserNote();
+        this.addNewQueueButton();
+
         this.addCoupon();
         this.addPrices();
         this.addDlcInfo();
@@ -822,6 +824,32 @@ class AppPageClass extends StorePageClass {
 
         document.querySelector(".js-user-note-button").addEventListener("click", handler);
         document.querySelector("#esi-store-user-note").addEventListener("click", handler);
+    }
+
+    addNewQueueButton() {
+        if (!document.querySelector(".finish_queue_text")) { return; }
+
+        HTML.afterEnd(".btn_next_in_queue",
+            `<div id="es_new_queue" class="btn_next_in_queue btn_next_in_queue_trigger" data-tooltip-text="${Localization.str.queue.new_tooltip}">
+                <div class="next_in_queue_content">
+                    <span class="finish_queue_text">${Localization.str.queue.new}</span>
+                </div>
+            </div>`);
+
+        ExtensionLayer.runInPageContext(() => {
+
+            $J("#es_new_queue").v_tooltip({"tooltipClass": "store_tooltip", "dataName": 'tooltipText', "defaultType": "text", "replaceExisting": false });
+
+            $J("#es_new_queue").on("click", () => {
+                $J.ajax({
+                    url: "https://store.steampowered.com/explore/next/" + g_eDiscoveryQueueType + '/',
+                    type: "POST",
+                    data: $J("#next_in_queue_form").serialize(),
+                    success: () => window.location.href = "https://store.steampowered.com/explore/startnew/" + g_eDiscoveryQueueType + '/'
+                    // TODO error handling, waiting on #231 and #275 to merge
+                });
+            });
+        });
     }
 
     getFirstSubid() {
