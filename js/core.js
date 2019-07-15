1e663c9d9324dd4e429e9dd413be40eb35c61440
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2019-07-15 10:26:55 +0200

    Allow links to be opened in a new tab

--- js/core.js
+++ js/core.js
@@ -622,8 +622,24 @@ class ExtensionResources {
      * We took the original Regex and aded chrome-extension://, moz-extension:// and steam://
      * First two are needed for linking local resources from extension,
      * steam:// protocol is used by Steam store to open their own client (e.g. when you want to launch a game).
+     * 
+     * The addition of the "target" attribute to the allowed attributes is done in order to be able to open links in a new tab,
+     * while considering security concerns (see hook and https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/)
      */
-    DOMPurify.setConfig({ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp|chrome-extension|moz-extension|steam):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i});
+    DOMPurify.setConfig({
+        ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp|chrome-extension|moz-extension|steam):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
+        ADD_ATTR: ["target"]
+    });
+
+    DOMPurify.addHook("uponSanitizeAttribute", (node, data) => {
+        if (data.attrName === "target") {
+            if (data.attrValue === "_blank") {
+                node.setAttribute("rel", "noreferrer noopener");
+            } else {
+                data.keepAttr = false;
+            }
+        }
+    });
 })();
 
 class HTML {
