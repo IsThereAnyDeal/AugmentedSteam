dd3550c9db158f953b4fdc58c79a4b8fb770ea3d
Revadike <revadike@outlook.com>
2019-09-06 17:06:39 +0200

    Removed debug info
    Co-Authored-By: MxtOUT <14999077+MxtOUT@users.noreply.github.com>

--- js/content/community.js
+++ js/content/community.js
@@ -3763,7 +3763,6 @@ let EditGuidePageClass = (function(){
                 let Modal = ShowConfirmDialog("${Localization.str.custom_tags}", \`<div class="commentthread_entry_quotebox"><textarea placeholder="${Localization.str.enter_tag}" class="commentthread_textarea es_tag" rows="1"></textarea></div>\`);
                 
                 let elem = $J(".es_tag");
-                console.log({elem, target: e.target})
                 let tag = elem.val();
 
                 function done() {
