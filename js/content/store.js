43eeb5317d0ee8cd303a203bf4b87a79e892bda2
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2019-10-08 21:15:47 +0200

    Fix wishlist export copy to clipboard for FF
    See https://github.com/tfedor/AugmentedSteam/issues/556#issuecomment-538735999
    Also simplified export

--- js/content/store.js
+++ js/content/store.js
@@ -3422,88 +3422,69 @@ let WishlistPageClass = (function(){
             return result.join("\n");
         }
     }
+    WishlistExporter.method = Object.freeze({"download": Symbol("Download"), "copyToClipboard": Symbol("Copy to clipboard")});
 
-    WishlistPageClass.prototype.showExportModalDialog = function() {
+    /**
+     * Using Valve's CModal API here is very hard, since, when trying to copy data to the clipboard, it has to originate from
+     * a short-lived event handler for a user action.
+     * Since we'd use our Messenger class to pass information in between these two contexts, we would "outrange" this specific event
+     * handler, resulting in a denial of access to the clipboard function.
+     * This could be circumvented by adding the appropriate permissions, but doing so would prompt users to explicitly accept the changed
+     * permissions on an update.
+     * 
+     * If we don't use the Messenger, we'd have to move the whole handler part (including WishlistExporter) to
+     * the page context side.
+     * 
+     * Final solution is to query the action buttons of the dialog and adding some extra click handlers on the content script side.
+     * These handlers are using a capture, so that the dialog elements will still be existent at the time of the invocation.
+     */
+    WishlistPageClass.prototype.showExportModalDialog = function(appInfo) {
 
         let exportStr = Localization.str.export;
 
         ExtensionLayer.runInPageContext(`function() {
-            let options = {};
-            window.AS_WishlistExportModal = ShowConfirmDialog(
+            ShowConfirmDialog(
                 "${exportStr.wishlist}",
-                "<div id='es_export_form'></div>",
-                "${Localization.str.save}",
-                "${Localization.str.cancel}"
+                \`<div id='es_export_form'>
+                    <div class="es-wexport">
+                    <h2>${exportStr.type}</h2>
+                    <div>
+                        <label class="es-wexport__label"><input type="radio" name="es_wexport_type" value="text" checked> ${exportStr.text}</label>
+                        <label class="es-wexport__label"><input type="radio" name="es_wexport_type" value="json"> JSON</label>
+                    </div>
+                    </div>
+                
+                    <div class="es-wexport es-wexport__format">
+                        <h2>${exportStr.format}</h2>
+                        <div>
+                            <input type="text" id="es-wexport-format" class="es-wexport__input" value="%title%"><br>
+                            <div class="es-wexport__symbols">%title%, %id%, %appid%, %url%, %release_date%, %type%, %note%</div>
+                        </div>
+                    </div>
+                </div>\`,
+                "${exportStr.download}",
+                null, // use default "Cancel"
+                "${exportStr.copy_clipboard}"
             );
         }`);
 
-        let formNode = document.querySelector("#es_export_form");
+        let [ dlBtn, copyBtn ] = document.querySelectorAll(".newmodal_buttons > .btn_medium");
 
-        HTML.inner(
-            formNode,
-            `<div class="es-wexport">
-                <h2>${exportStr.type}</h2>
-                <div>
-                    <label class="es-wexport__label"><input type="radio" name="es_wexport_type" value="text" checked> ${exportStr.text}</label>
-                    <label class="es-wexport__label"><input type="radio" name="es_wexport_type" value="json"> JSON</label>
-                </div>
-            </div>
-        
-            <div class="es-wexport es-wexport__format">
-                <h2>${exportStr.format}</h2>
-                <div>
-                    <input type="text" id="es-wexport-format" class="es-wexport__input" value="%title%"><br>
-                    <div class="es-wexport__symbols">%title%, %id%, %appid%, %url%, %release_date%, %type%, %note%</div>
-                </div>
-            </div>`);
+        dlBtn.classList.remove("btn_green_white_innerfade");
+        dlBtn.classList.add("btn_darkblue_white_innerfade");
+
+        dlBtn.addEventListener("click", () => exportWishlist(WishlistExporter.method.download), true);
+        copyBtn.addEventListener("click", () => exportWishlist(WishlistExporter.method.copyToClipboard), true);
 
         let format = document.querySelector(".es-wexport__format");
         for (let el of document.getElementsByName("es_wexport_type")) {
             el.addEventListener("click", e => format.style.display = e.target.value === "json" ? "none" : '');
         }
 
-        let buttonsNode = formNode.closest(".newmodal_content").querySelector(".newmodal_buttons");
-
-        HTML.inner(buttonsNode,
-            `<div id="as_export_download" class="btn_green_white_innerfade btn_medium"><span>${exportStr.download}</span></div>
-             <div id="as_export_copy" class="btn_green_white_innerfade btn_medium"><span>${exportStr.copy_clipboard}</span></div>
-             <div id="as_export_cancel" class="btn_grey_white_innerfade btn_medium"><span>${Localization.str.cancel}</span></div>`);
-
-        // events
-
-        function dismissModal() {
-            ExtensionLayer.runInPageContext(`function() { 
-                window.AS_WishlistExportModal.Dismiss();
-                window.AS_WishlistExportModal = null;
-            }`);
-        }
-
-        function handleExport(method) {
+        function exportWishlist(method) {
             let type = document.querySelector("input[name='es_wexport_type']:checked").value;
-            let format = encodeURIComponent(document.querySelector("#es-wexport-format").value);
-            ExtensionLayer.runInPageContext(`function() { 
-                Messenger.postMessage("exportWishlist", {format: "${format}", method: "${method}", type: "${type}", appInfo: g_rgAppInfo});
-            }`);
-            dismissModal();
-        }
+            let format = document.querySelector("#es-wexport-format").value;
 
-        document.querySelector("#as_export_download").addEventListener("click", function() { handleExport("download"); });
-        document.querySelector("#as_export_copy").addEventListener("click", function() { handleExport("clipboard"); });
-        document.querySelector("#as_export_cancel").addEventListener("click", dismissModal);
-
-        // handle messages
-
-        Messenger.onMessage("exportWishlist").then(data => {
-            let appInfo = data.appInfo;
-            if (!appInfo) { return; }
-            let type = data.type;
-            let method = data.method;
-            let format = decodeURIComponent(data.format);
-
-            exportWishlist(appInfo, type, method, format);
-        });
-
-        function exportWishlist(appInfo, type, method, format) {
             let wishlist = new WishlistExporter(appInfo);
 
             let result = "";
@@ -3519,10 +3500,10 @@ let WishlistPageClass = (function(){
                 filetype = "text/plain";
             }
 
-            if (method === "clipboard") {
+            if (method === WishlistExporter.method.copyToClipboard) {
                 Clipboard.set(result);
-            } else if (method === "download") {
-                Downloader.download(new Blob([result], { type : filetype+";charset=UTF-8" }), filename);
+            } else if (method === WishlistExporter.method.download) {
+                Downloader.download(new Blob([result], { type: `${filetype};charset=UTF-8` }), filename);
             }
         }
     };
@@ -3530,9 +3511,9 @@ let WishlistPageClass = (function(){
     WishlistPageClass.prototype.addExportWishlistButton = function() {
         HTML.afterBegin("#cart_status_data", "<div class='es-wbtn' id='es_export_wishlist'><div>" + Localization.str.export.wishlist + "</div></div>");
 
-        let that = this;
-        document.querySelector("#es_export_wishlist").addEventListener("click", function() {
-            that.showExportModalDialog();
+        document.querySelector("#es_export_wishlist").addEventListener("click", () => {
+            Messenger.onMessage("appInfo").then(appInfo => this.showExportModalDialog(appInfo));
+            ExtensionLayer.runInPageContext(() => Messenger.postMessage("appInfo", g_rgAppInfo));
         });
     };
 
