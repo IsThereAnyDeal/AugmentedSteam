89c3dd3b1bf1402c25d2a30958d3a3fef890cb8e
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2019-10-26 17:14:06 +0200

    Fix not highlighting games with a card tag on activity page (again)
    See #470

--- js/content/community.js
+++ js/content/community.js
@@ -400,7 +400,10 @@ let ProfileActivityPageClass = (function(){
 
         let aNodes = Array.from(blotterBlocks).reduce((acc, cur) => {
             acc.push(...Array.from(cur.querySelectorAll("a:not(.blotter_gamepurchase_logo)")).filter(link =>
-                GameId.getAppid(link) && !link.childElementCount
+                (GameId.getAppid(link) && link.childElementCount <= 1)
+                &&
+                // https://github.com/tfedor/AugmentedSteam/pull/470#pullrequestreview-284928257
+                (link.childElementCount !== 1 || !link.closest(".vote_header"))
             ));
             return acc;
         }, []);
