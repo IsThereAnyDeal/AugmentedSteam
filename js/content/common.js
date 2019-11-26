9a80fd3cbd46fdf3fc391ce39927ebf96ced8fdd
Revadike <revadike@outlook.com>
2019-11-26 22:54:34 +0100

    Changed back to top button position to corner

--- js/content/common.js
+++ js/content/common.js
@@ -952,15 +952,7 @@ let EnhancedSteam = (function() {
 
         node.addEventListener("click", gotop);
         window.addEventListener("scroll", scrolling);
-        window.addEventListener("resize", resizing);
-        window.addEventListener("load", onLoad);
-
-        onLoad();
-        
-        function onLoad() {
-            scrolling();
-            resizing();
-        }
+        scrolling();
 
         function gotop() {
             if (timer) { return; }
@@ -987,12 +979,6 @@ let EnhancedSteam = (function() {
             prevScrollHeight = scrollHeight;
             node.style.opacity = Math.min(Math.max((scrollHeight - 200) / 800, 0), 1);
             node.style.display = node.style.opacity >= 0.1 ? "block" : "none";
-        }
-
-        function resizing() {
-            let winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-            let headerWidth = document.querySelector("div#global_header .content").offsetWidth;
-            node.style.right = `${headerWidth > 0 ? (winWidth / 2) - (headerWidth / 2) - node.offsetWidth - 20 : 20}px`;
         }
     };
 
