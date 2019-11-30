821d7a2ecee4bb4193c63589deb3b757c809f11a
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2019-11-30 22:33:37 +0100

    Reformat showRegionalPricing

--- js/content/store.js
+++ js/content/store.js
@@ -364,7 +364,7 @@ class StorePageClass {
         }
     }
 
-    showRegionalPricing(type) {
+    async showRegionalPricing(type) {
         let showRegionalPrice = SyncedStorage.get("showregionalprice");
         if (showRegionalPrice === "off") { return; }
 
@@ -374,112 +374,110 @@ class StorePageClass {
         let localCountry = User.getCountry().toLowerCase();
         if (!countries.includes(localCountry)) countries.push(localCountry);
 
-        let subids = this.getAllSubids();
-        subids.forEach(subid => {
-            if (!subid) { return; }
+        for (let subid of this.getAllSubids()) {
+            if (!subid) return;
+            
             let promises = [];
-
             let prices = {};
 
-            countries.forEach(country => {
+            for (let country of countries) {
+                promises.push(
+                    RequestData.getJson(`https://store.steampowered.com/api/packagedetails/?packageids=${subid}&cc=${country}`).then(result => {
+                        if (!result || !result[subid] || !result[subid].success) { return; }
+                        prices[country] = result[subid].data.price;
+                    })
+                );
+            }
 
-                let promise = RequestData.getJson("https://store.steampowered.com/api/packagedetails/?packageids="+subid+"&cc="+country).then(result => {
-                    if (!result || !result[subid] || !result[subid].success) { return; }
-                    prices[country] = result[subid].data.price;
-                });
-                promises.push(promise);
-            });
+            await Promise.all(promises);
+            
+            let node = document.querySelector(`input[name=subid][value="${subid}"]`)
+                .closest(".game_area_purchase_game_wrapper,#game_area_purchase,.sale_page_purchase_item")
+                .querySelector(".game_purchase_action");
 
-            Promise.all(promises).then(result => {
+            let apiPrice = prices[User.getCountry().toLowerCase()];
+            let priceLocal;
+            try {
+                priceLocal = new Price(apiPrice.final / 100, apiPrice.currency).inCurrency(Currency.customCurrency);
+            } catch(err) {
+                console.group("Regional pricing");
+                console.error(err);
+                console.warn("Can't show relative price differences to any other currencies");
+                console.groupEnd();
+            }
 
-                let node = document.querySelector("input[name=subid][value='"+subid+"']")
-                    .closest(".game_area_purchase_game_wrapper,#game_area_purchase,.sale_page_purchase_item")
-                    .querySelector(".game_purchase_action");
+            let pricingDiv = document.createElement("div");
+            pricingDiv.classList.add("es_regional_container");
+            pricingDiv.classList.add(`es_regional_${type || "app"}`);
 
-                let apiPrice = prices[User.getCountry().toLowerCase()];
-                let priceLocal;
-                try {
-                    priceLocal = new Price(apiPrice.final / 100, apiPrice.currency).inCurrency(Currency.customCurrency);
-                } catch(err) {
-                    console.group("Regional pricing");
-                    console.error(err);
-                    console.warn("Can't show relative price differences to any other currencies");
-                    console.groupEnd();
-                }
+            if (showRegionalPrice === "mouse") {
+                HTML.inner(pricingDiv, pricingDiv.innerHTML + '<div class="miniprofile_arrow right" style="position: absolute; top: 12px; right: -8px;"></div>');
+            }
 
-                let pricingDiv = document.createElement("div");
-                pricingDiv.classList.add("es_regional_container");
-                pricingDiv.classList.add("es_regional_" + (type || "app"));
+            for (let country of countries) {
+                let apiPrice = prices[country];
+                let html = "";
 
-                if (showRegionalPrice === "mouse") {
-                    HTML.inner(pricingDiv, pricingDiv.innerHTML + '<div class="miniprofile_arrow right" style="position: absolute; top: 12px; right: -8px;"></div>');
-                }
-
-                countries.forEach(country => {
-                    let apiPrice = prices[country];
-                    let html = "";
-
-                    if (apiPrice) {
-                        let priceRegion = new Price(apiPrice.final / 100, apiPrice.currency);
-                        let priceUser;
-                        try {
-                            priceUser = priceRegion.inCurrency(Currency.customCurrency);
-                        } catch(err) {
-                            console.group("Regional pricing");
-                            console.error(err);
-                            console.warn(`Not able to show converted price and relative price differences for country code "%s"`, country.toUpperCase());
-                            console.groupEnd();
-                        }
-
-                        html =
-                            `<div class="es_regional_price es_flag es_flag_${country}">
-                                ${priceRegion}
-                            `;
-
-                        if (priceLocal && priceUser) {
-                            let percentageIndicator = "equal";
-                            let percentage = (((priceUser.value / priceLocal.value) * 100) - 100).toFixed(2);
-
-                            if (percentage < 0) {
-                                percentage = Math.abs(percentage);
-                                percentageIndicator = "lower";
-                            } else if (percentage > 0) {
-                                percentageIndicator = "higher";
-                            }
-
-                            html +=
-                                `<span class="es_regional_converted">(${priceUser})</span>
-                                <span class="es_percentage es_percentage_${percentageIndicator}">${percentage}%</span>`
-                        }
-                        
-                        html += "</div>";
-                    } else {
-                        html =
-                            `<div class="es_regional_price es_flag es_flag_${country}">
-                                <span class="es_regional_unavailable">${Localization.str.region_unavailable}</span>
-                            </div>`;
+                if (apiPrice) {
+                    let priceRegion = new Price(apiPrice.final / 100, apiPrice.currency);
+                    let priceUser;
+                    try {
+                        priceUser = priceRegion.inCurrency(Currency.customCurrency);
+                    } catch(err) {
+                        console.group("Regional pricing");
+                        console.error(err);
+                        console.warn(`Not able to show converted price and relative price differences for country code "%s"`, country.toUpperCase());
+                        console.groupEnd();
                     }
 
-                    HTML.inner(pricingDiv, pricingDiv.innerHTML + html);
-                });
+                    html =
+                        `<div class="es_regional_price es_flag es_flag_${country}">
+                            ${priceRegion}
+                        `;
 
-                let purchaseArea = node.closest(".game_area_purchase_game,.sale_page_purchase_item");
-                purchaseArea.classList.add("es_regional_prices");
+                    if (priceLocal && priceUser) {
+                        let percentageIndicator = "equal";
+                        let percentage = (((priceUser.value / priceLocal.value) * 100) - 100).toFixed(2);
 
-                if (showRegionalPrice === "always") {
-                    node.insertAdjacentElement("beforebegin", pricingDiv);
-                    purchaseArea.classList.add("es_regional_always");
+                        if (percentage < 0) {
+                            percentage = Math.abs(percentage);
+                            percentageIndicator = "lower";
+                        } else if (percentage > 0) {
+                            percentageIndicator = "higher";
+                        }
+
+                        html +=
+                            `<span class="es_regional_converted">(${priceUser})</span>
+                            <span class="es_percentage es_percentage_${percentageIndicator}">${percentage}%</span>`
+                    }
+                    
+                    html += "</div>";
                 } else {
-                    let priceNode = node.querySelector(".price,.discount_prices");
-                    priceNode.insertAdjacentElement("beforeend", pricingDiv);
-                    priceNode.classList.add("es_regional_onmouse");
-
-                    if (!SyncedStorage.get("regional_hideworld")) {
-                        priceNode.classList.add("es_regional_icon");
-                    }
+                    html =
+                        `<div class="es_regional_price es_flag es_flag_${country}">
+                            <span class="es_regional_unavailable">${Localization.str.region_unavailable}</span>
+                        </div>`;
                 }
-            })
-        });
+
+                HTML.inner(pricingDiv, pricingDiv.innerHTML + html);
+            }
+
+            let purchaseArea = node.closest(".game_area_purchase_game,.sale_page_purchase_item");
+            purchaseArea.classList.add("es_regional_prices");
+
+            if (showRegionalPrice === "always") {
+                node.insertAdjacentElement("beforebegin", pricingDiv);
+                purchaseArea.classList.add("es_regional_always");
+            } else {
+                let priceNode = node.querySelector(".price,.discount_prices");
+                priceNode.insertAdjacentElement("beforeend", pricingDiv);
+                priceNode.classList.add("es_regional_onmouse");
+
+                if (!SyncedStorage.get("regional_hideworld")) {
+                    priceNode.classList.add("es_regional_icon");
+                }
+            }
+        }  
     }
 
     forceVideoMP4() {
