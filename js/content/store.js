afeb837be2b2f0b107ea61eeb33a5f5309d286e6
candela97 <54083835+candela97@users.noreply.github.com>
2019-12-16 00:54:44 +0800

    Improve fix for franchise row search links

--- js/content/store.js
+++ js/content/store.js
@@ -1634,7 +1634,7 @@ class AppPageClass extends StorePageClass {
     replaceDevPubLinks() {
         if (!this.isAppPage()) { return; }
 
-        document.querySelectorAll("#game_highlights .dev_row a,.details_block .dev_row:not(:last-of-type) a").forEach((linkNode, i) => {
+        document.querySelectorAll("#game_highlights .dev_row a,.details_block .dev_row:not(:nth-of-type(3)) a").forEach((linkNode, i) => {
             let homepageLink = new URL(linkNode.href);
             if (homepageLink.pathname === "/search/") return;
 
