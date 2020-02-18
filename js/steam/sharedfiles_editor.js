44224b8ef61dbdc7cedc6de6da3c2aafc03ac5ef
Revadike <revadike@outlook.com>
2020-02-19 00:08:15 +0100

    moved BBCode functions to sharedfiles_editor.js

--- js/steam/sharedfiles_editor.js
+++ js/steam/sharedfiles_editor.js
@@ -9,7 +9,6 @@ function InitSectionDescriptionTextArea( textArea, OnContentChanged )
 	}
 }
 
-
 function BBCode_BoldSelection()
 {
 	g_textarea.wrapSelection('[b]','[/b]');
@@ -46,6 +45,98 @@ function BBCode_MakeListFromSelection()
 function BBCode_H1Selection()
 {
 	g_textarea.wrapSelection('[h1]','[/h1]');
+}
+
+function BBCode_SpoilerSelection()
+{
+	g_textarea.wrapSelection("[spoiler]", "[/spoiler]");
+}
+
+function BBCode_NoParseSelection()
+{
+	g_textarea.wrapSelection("[noparse]", "[/noparse]");
+}
+
+function BBCode_QuoteSelection()
+{
+	g_textarea.wrapSelection("[quote]", "[/quote]");
+}
+
+function BBCode_CodeSelection()
+{
+	g_textarea.wrapSelection("[code]", "[/code]");
+}
+
+function BBCode_MakeOListFromSelection()
+{
+	g_textarea.collectFromEachSelectedLine(function(line) {
+		return (line.match(/\*+\s/) ? "[*]" : "[*] ") + line;
+	}, "[olist]\n", "\n[/olist]");
+}
+
+function BBCode_MakeTableFromSelection()
+{
+	let w = 1;
+	let h = 1;
+
+	let Modal = ShowConfirmDialog("",
+		`W:<input type="number" min="1" style="width: 50px;" value="${w}" id="es_width">
+		&nbsp; X &nbsp;
+		H:<input type="number" min="1" style="width: 50px;" value="${h}" id="es_height">
+		<br><br>`
+	);
+	
+	$J("#es_width").on("keydown paste input change", () => w = parseInt($J("#es_width").val()));
+	$J("#es_height").on("keydown paste input change", () => h = parseInt($J("#es_height").val()));
+
+	Modal.done(function() {
+		let n = 0;
+		let tab = "    ";
+
+		let c = "";
+		while (w > 1) {
+			c += `${tab}${tab}[td][/td]\n`;
+			w--;
+		}
+
+		g_textarea.collectFromEachSelectedLine(function(line) {
+			n++;
+			return `${tab}[tr]\n${tab}${tab}[td]${line}[/td]\n${c}${tab}[/tr]`;
+		}, ``, ``);
+
+		let r = ``;
+		while (h > n) {
+			r += `\n${tab}[tr]\n${tab}${tab}[td][/td]\n${c}${tab}[/tr]`;
+			h--;
+		}
+
+		g_textarea.wrapSelection("", r);
+		g_textarea.wrapSelection("[table]\n", "\n[/table]");
+	});
+};
+
+function BBCode_HyperlinkSelection()
+{
+	let text = g_textarea.getSelection();
+	let Modal = ShowConfirmDialog("URL",
+		`<div class="commentthread_entry_quotebox">
+			<textarea class="commentthread_textarea" id="es_url" rows="1"></textarea>
+		</div>`
+	);
+	
+	let url = "";
+	$J("#es_url").on("keydown paste input", function(e) {
+		let code = e.keyCode || e.which;
+		if (code == 13) {
+			Modal.Dismiss();
+			BBCode_MakeURLFromSelection(url, text);
+			return;
+		}
+
+		url = $J("#es_url").val();
+	});
+
+	Modal.done(() => BBCode_MakeURLFromSelection(url, text));
 }
 
 function ShowURLDialog()
