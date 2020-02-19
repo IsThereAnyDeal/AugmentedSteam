6e8fb3d126ee87925afd62e8c333c7e0bd655b2a
Revadike <revadike@outlook.com>
2020-02-19 21:11:22 +0100

    Prevent crash on nginx error

--- js/content/store.js
+++ js/content/store.js
@@ -4207,6 +4207,9 @@ let TabAreaObserver = (function(){
 })();
 
 (async function(){
+    let nginxError = document.querySelector("body > center:nth-child(3)");
+    if (nginxError && nginxError.innerText === "nginx") { return; }
+
     let path = window.location.pathname.replace(/\/+/g, "/");
 
     await SyncedStorage.init().catch(err => console.error(err));
