fbfc188a5425caaae05dd053be0baacf71e21c6d
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2020-02-26 19:31:30 +0100

    Handle /curator/ links for devs and pubs

--- js/content/store.js
+++ js/content/store.js
@@ -1722,14 +1722,21 @@ class AppPageClass extends StorePageClass {
 
     replaceDevPubLinks() {
 
-        for (let node of document.querySelectorAll(`.dev_row a[href^="https://store.steampowered.com/"]`)) {
+        let devs = Array.from(document.querySelectorAll("#developers_list > a, .details_block > .dev_row:first-of-type > a"));
+        let pubs = Array.from(document.querySelectorAll(".user_reviews > .dev_row:last-of-type a, .details_block > .dev_row:nth-of-type(2) > a"));
+        let franchise = document.querySelector(".details_block > .dev_row:nth-of-type(3) > a");
+
+        for (let node of [...devs, ...pubs, franchise]) {
             let homepageLink = new URL(node.href);
+            if (homepageLink.pathname.startsWith("/search/")) { continue; }
 
             let type;
-            if (homepageLink.pathname.startsWith("/developer/")) {
+            if (devs.includes(node)) {
                 type = "developer";
-            } else if (homepageLink.pathname.startsWith("/publisher/")) {
+            } else if (pubs.includes(node)) {
                 type = "publisher";
+            } else if (franchise === node) {
+                type = "franchise";
             }
             if (!type) { continue; }
 
