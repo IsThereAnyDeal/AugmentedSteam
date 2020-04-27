7a7a31f5ce3664b726bc8997a3ecb30ce564cc27
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2020-04-27 16:39:30 +0200

    Fix premature return

--- js/content/store/FForceMP4.js
+++ js/content/store/FForceMP4.js
@@ -7,13 +7,13 @@ class FForceMP4 extends ASFeature {
         for (let node of document.querySelectorAll("[data-webm-source]")) {
             let mp4 = node.dataset.mp4Source;
             let mp4hd = node.dataset.mp4HdSource;
-            if (!mp4 || !mp4hd) { return; }
+            if (!mp4 || !mp4hd) { continue; }
 
             node.dataset.webmSource = mp4;
             node.dataset.webmHdSource = mp4hd;
 
             let video = node.querySelector("video");
-            if (!video) { return; }
+            if (!video) { continue; }
 
             video.dataset.sdSrc = mp4;
             video.dataset.hdSrc = mp4hd;
