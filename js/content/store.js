d8d69b56a3fce725934555eb205a362484d52649
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2020-04-28 10:18:54 +0200

    Add FFullscreenScreenshotView

--- js/content/store.js
+++ js/content/store.js
@@ -587,8 +587,6 @@ class AppPageClass extends StorePageClass {
         this.data = this.storePageDataPromise().catch(err => console.error(err));
         this.appName = document.querySelector(".apphub_AppName").textContent;
 
-        this.addFullscreenScreenshotView();
-
         this.addCoupon();
         this.addPrices();
         this.addDlcInfo();
@@ -640,50 +638,6 @@ class AppPageClass extends StorePageClass {
 
     async _removeFromWaitlist() {
         return Background.action("itad.removefromwaitlist", this.appid);
-    }
-
-    addFullscreenScreenshotView() {
-        function toggleFullScreen(ev) {
-            if (!document.fullscreenElement) {
-                ev.target.closest(".screenshot_popup_modal_content").requestFullscreen();
-            } else {
-                document.exitFullscreen();
-            }
-        }
-
-        function initFSVButtons() {
-            let modalFooter = document.querySelector(".screenshot_popup_modal_footer");
-            let nextButton = modalFooter.querySelector(".next");
-            let nextButtonOffsetWidth = nextButton.offsetWidth;
-            if (nextButton.style.display === "none") {
-                nextButton.style.display = "";
-                nextButtonOffsetWidth = nextButton.offsetWidth;
-                nextButton.style.display = "none";
-            }
-            HTML.beforeEnd(modalFooter,
-                `<div class="btnv6_blue_hoverfade btn_medium es_screenshot_fullscreen_toggle" style="right: calc(${nextButtonOffsetWidth}px + 0.5em)"><i></i></div>`);
-            let fsvButton = modalFooter.querySelector(".es_screenshot_fullscreen_toggle");
-            fsvButton.addEventListener("click", toggleFullScreen);
-
-            let modalTitleLink = modalFooter.parentElement.querySelector(".screenshot_popup_modal_title > a");
-            HTML.beforeEnd(modalFooter,
-                `<div class="btnv6_blue_hoverfade btn_medium es_screenshot_open_btn" style="right: calc(${nextButtonOffsetWidth + fsvButton.offsetWidth}px + 1em)"><i></i></div>`);
-            let openButton = modalFooter.querySelector(".es_screenshot_open_btn");
-            openButton.addEventListener("click", () => {
-                window.open(modalTitleLink.href, "_blank");
-            });
-        }
-
-        let observer = new MutationObserver(mutations => {
-            for (let mutation of mutations) {
-                for (let node of mutation.addedNodes) {
-                    if (node.classList.contains("screenshot_popup_modal")) {
-                        initFSVButtons();
-                    }
-                }
-            }
-        });
-        observer.observe(document.body, { childList: true });
     }
 
     getFirstSubid() {
@@ -3619,7 +3573,7 @@ let TabAreaObserver = (function(){
             break;
 
         case /^\/app\/.*/.test(path):
-            new CAppPage([ FReplaceDevPubLinks, FRemoveFromWishlist, FForceMP4, FHDPlayer, FUserNotes, FWaitlistDropdown, FNewQueue ], window.location.host + path);
+            new CAppPage([ FReplaceDevPubLinks, FRemoveFromWishlist, FForceMP4, FHDPlayer, FUserNotes, FWaitlistDropdown, FNewQueue, FFullscreenScreenshotView ], window.location.host + path);
             break;
 
         case /^\/sub\/.*/.test(path):
---
+++ js/content/store/FFullscreenScreenshotView.js
@@ -0,0 +1,45 @@
+class FFullscreenScreenshotView extends ASFeature {
+    apply() {
+        function toggleFullScreen(ev) {
+            if (!document.fullscreenElement) {
+                ev.target.closest(".screenshot_popup_modal_content").requestFullscreen();
+            } else {
+                document.exitFullscreen();
+            }
+        }
+
+        function initFSVButtons() {
+            let modalFooter = document.querySelector(".screenshot_popup_modal_footer");
+            let nextButton = modalFooter.querySelector(".next");
+            let nextButtonOffsetWidth = nextButton.offsetWidth;
+            if (nextButton.style.display === "none") {
+                nextButton.style.display = "";
+                nextButtonOffsetWidth = nextButton.offsetWidth;
+                nextButton.style.display = "none";
+            }
+            HTML.beforeEnd(modalFooter,
+                `<div class="btnv6_blue_hoverfade btn_medium es_screenshot_fullscreen_toggle" style="right: calc(${nextButtonOffsetWidth}px + 0.5em)"><i></i></div>`);
+            let fsvButton = modalFooter.querySelector(".es_screenshot_fullscreen_toggle");
+            fsvButton.addEventListener("click", toggleFullScreen);
+
+            let modalTitleLink = modalFooter.parentElement.querySelector(".screenshot_popup_modal_title > a");
+            HTML.beforeEnd(modalFooter,
+                `<div class="btnv6_blue_hoverfade btn_medium es_screenshot_open_btn" style="right: calc(${nextButtonOffsetWidth + fsvButton.offsetWidth}px + 1em)"><i></i></div>`);
+            let openButton = modalFooter.querySelector(".es_screenshot_open_btn");
+            openButton.addEventListener("click", () => {
+                window.open(modalTitleLink.href, "_blank");
+            });
+        }
+
+        let observer = new MutationObserver(mutations => {
+            for (let mutation of mutations) {
+                for (let node of mutation.addedNodes) {
+                    if (node.classList.contains("screenshot_popup_modal")) {
+                        initFSVButtons();
+                    }
+                }
+            }
+        });
+        observer.observe(document.body, { childList: true });
+    }
+}
