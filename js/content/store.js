9860f0ed46165b4d7dea996a83d0bee5c44988f0
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2020-04-28 10:07:53 +0200

    Add FWaitlistDropdown

--- js/content/store.js
+++ js/content/store.js
@@ -587,7 +587,6 @@ class AppPageClass extends StorePageClass {
         this.data = this.storePageDataPromise().catch(err => console.error(err));
         this.appName = document.querySelector(".apphub_AppName").textContent;
 
-        this.addWaitlistDropdown();
         this.addNewQueueButton();
         this.addFullscreenScreenshotView();
 
@@ -642,143 +641,6 @@ class AppPageClass extends StorePageClass {
 
     async _removeFromWaitlist() {
         return Background.action("itad.removefromwaitlist", this.appid);
-    }
-
-    async addWaitlistDropdown() {
-        if (!document.querySelector("#add_to_wishlist_area") || !SyncedStorage.get("add_to_waitlist") || !await Background.action("itad.isconnected")) return;
-
-        // This node will be hidden behind the dropdown menu. Also, it's not really desirable when using dropdown menus to have a permanent div floating nearby
-        let notice = document.querySelector(".wishlist_added_temp_notice");
-        if (notice) notice.remove();
-
-        let wishlistDivs = document.querySelectorAll("#add_to_wishlist_area,#add_to_wishlist_area_success");
-        let [wishlistArea, wishlistSuccessArea] = wishlistDivs;
-
-        HTML.afterEnd(".queue_actions_ctn :first-child",
-            `<div style="position: relative; display: inline-block;">
-                <div class="queue_control_button queue_btn_wishlist"></div>
-            </div>`);
-
-        // Creating a common parent for #add_to_wishlist_area and #add_to_wishlist_area_success makes it easier to apply the dropdown menu
-        let wrapper = document.querySelector(".queue_btn_wishlist");
-
-        // Move the wrapper such that there can't be any other elements in between the dropdown and other buttons (see #690)
-        document.querySelector(".queue_actions_ctn").insertBefore(wrapper.parentNode, wishlistArea);
-
-        wishlistDivs.forEach(div => {
-            wrapper.appendChild(div);
-            let button = div.querySelector(".btnv6_blue_hoverfade");
-            button.style.borderTopRightRadius = 0;
-            button.style.borderBottomRightRadius = 0;
-        });
-
-        HTML.afterEnd(wrapper,
-            `<div class="queue_control_button queue_btn_ignore_menu" style="display: inline;">
-                <div class="queue_ignore_menu_arrow btn_medium">
-                    <span><img src="https://steamstore-a.akamaihd.net/public/images/v6/btn_arrow_down_padded.png"></span>
-                </div>
-                <div class="queue_ignore_menu_flyout">
-                    <div class="queue_ignore_menu_flyout_content">
-                        <div class="queue_ignore_menu_option" id="queue_ignore_menu_option_not_interested">
-                            <div>
-                                <img class="queue_ignore_menu_option_image selected" src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_selected_bright.png">
-                                <img class="queue_ignore_menu_option_image unselected" src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_unselected_bright.png">
-                            </div>
-                            <div class="queue_ignore_menu_option_label">
-                                <div class="option_title">${Localization.str.wishlist} (${Localization.str.theworddefault})</div>
-                                <div class="option_subtitle">${Localization.str.add_to_wishlist}</div>
-                            </div>
-                        </div>
-                        <div class="queue_ignore_menu_option" id="queue_ignore_menu_option_owned_elsewhere">
-                            <div>
-                                <img class="queue_ignore_menu_option_image selected" src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_selected_bright.png">
-                                <img class="queue_ignore_menu_option_image unselected" src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_unselected_bright.png">
-                            </div>
-                            <div class="queue_ignore_menu_option_label">
-                                <div class="option_title">Waitlist</div>
-                                <div class="option_subtitle">${Localization.str.add_to_waitlist}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>`);
-
-        let wishlisted = document.querySelector("#add_to_wishlist_area").style.display === "none";
-        let waitlisted = await Background.action("itad.inwaitlist", this.storeid);
-
-        let menuArrow = document.querySelector(".queue_ignore_menu_arrow");
-        let menu = document.querySelector(".queue_btn_ignore_menu");
-        let wishlistOption = document.querySelector("#queue_ignore_menu_option_not_interested");
-        let waitlistOption = document.querySelector("#queue_ignore_menu_option_owned_elsewhere");
-
-        updateDiv();
-
-        function updateDiv() {
-            let oneActive = Boolean(wishlisted) || Boolean(waitlisted);
-
-            menuArrow.classList.toggle("queue_btn_active", oneActive);
-            menuArrow.classList.toggle("queue_btn_inactive", !oneActive);
-
-            menu.classList.toggle("not_interested", wishlisted);
-            menu.classList.toggle("owned_elsewhere", waitlisted);
-
-            wishlistArea.style.display = oneActive ? "none" : '';
-            wishlistSuccessArea.style.display = oneActive ? '' : "none";
-
-            let text;
-            if (wishlisted && !waitlisted) {
-                text = Localization.str.on_wishlist;
-            } else if (!wishlisted && waitlisted) {
-                text = Localization.str.on_waitlist;
-            } else if (wishlisted && waitlisted) {
-                text = `${Localization.str.on_wishlist} & ${Localization.str.on_waitlist}`;
-            } else {
-                document.querySelector("#add_to_wishlist_area span").textContent = ` ${Localization.str.add_to_wishlist}`;
-                return;
-            }
-
-            document.querySelector("#add_to_wishlist_area_success span").lastChild.textContent = ` ${text}`;
-        }
-
-        wishlistArea.querySelector("a").addEventListener("click", async () => {
-
-            await ExtensionLayer.runInPageContext(() => new Promise(resolve => {
-                $J(document).ajaxComplete(function handler(e, xhr, { url }) {
-                    if (url === "https://store.steampowered.com/api/addtowishlist") {
-                        resolve();
-                        $J(document).unbind("ajaxComplete", handler);
-                    }
-                });
-            }), null, "wishlistAdded");
-
-            wishlisted = !wishlisted;
-            updateDiv();
-        });
-
-        this.onWishAndWaitlistRemove = () => {
-            wishlisted = waitlisted = false;
-            updateDiv();
-        };
-
-        wishlistOption.addEventListener("click", async () => {
-            if (wishlisted) {
-                await this._removeFromWishlist();
-                wishlisted = !wishlisted;
-                updateDiv();
-            } else {
-                wishlistArea.querySelector("a").click();
-            }
-        });
-
-        waitlistOption.addEventListener("click", async () => {
-            if (waitlisted) {
-                await Background.action("itad.removefromwaitlist", this.appid);
-            } else {
-                await Background.action("itad.addtowaitlist", this.appid);
-            }
-            waitlisted = !waitlisted;
-            updateDiv();
-        });
     }
 
     addNewQueueButton() {
@@ -3784,7 +3646,7 @@ let TabAreaObserver = (function(){
             break;
 
         case /^\/app\/.*/.test(path):
-            new CAppPage([ FReplaceDevPubLinks, FRemoveFromWishlist, FForceMP4, FHDPlayer, FUserNotes ], window.location.host + path);
+            new CAppPage([ FReplaceDevPubLinks, FRemoveFromWishlist, FForceMP4, FHDPlayer, FUserNotes, FWaitlistDropdown ], window.location.host + path);
             break;
 
         case /^\/sub\/.*/.test(path):
