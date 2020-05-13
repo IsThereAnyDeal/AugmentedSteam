35c8590db17fe3f7596394f0b3461dccad16dfcc
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2020-05-13 11:56:33 +0200

    Add highlights to search page

--- js/content/store/CSearchPage.js
+++ js/content/store/CSearchPage.js
@@ -3,6 +3,7 @@ class CSearchPage extends ASContext {
     constructor() {
         super([
             FSearchFilters,
+            FHighlightsTags,
         ]);
 
         this.infiniScrollEnabled = document.querySelector(".search_pagination").style.display === "none";
