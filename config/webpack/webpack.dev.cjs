2f06befc08b2027f38d36ed7600892768deac558
Tomáš Fedor <tomas.fedor3@gmail.com>
2020-10-29 02:32:54 +0100

    Change devtool source map for development

--- config/webpack/webpack.dev.cjs
+++ config/webpack/webpack.dev.cjs
@@ -3,5 +3,5 @@ const common = require("./webpack.common.cjs");
 
 module.exports = merge(common, {
     "mode": "development",
-    "devtool": "inline-source-map"
+    "devtool": "eval-source-map"
 });
