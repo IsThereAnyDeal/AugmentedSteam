ba9eb1ea850fa49ac12d7abf8d32c2a1b9899183
candela97 <54083835+candela97@users.noreply.github.com>
2020-12-18 21:05:10 +0800

    Simplify re-export syntax in aggregated modules

--- src/js/modulesCore.js
+++ src/js/modulesCore.js
@@ -1,36 +1,20 @@
-import {ErrorParser} from "./Core/Errors/ErrorParser";
-import {Errors} from "./Core/Errors/Errors";
-import {HTML} from "./Core/Html/Html";
-import {HTMLParser} from "./Core/Html/HtmlParser";
-import {Language} from "./Core/Localization/Language";
-import {Localization} from "./Core/Localization/Localization";
-import {CookieStorage} from "./Core/Storage/CookieStorage";
-import {LocalStorage} from "./Core/Storage/LocalStorage";
-import {SyncedStorage} from "./Core/Storage/SyncedStorage";
-import {BackgroundSimple} from "./Core/BackgroundSimple";
-import {StringUtils} from "./Core/Utils/StringUtils";
-import {TimeUtils} from "./Core/Utils/TimeUtils";
-import {Debug} from "./Core/Debug";
-import {Downloader} from "./Core/Downloader";
-import {Environment} from "./Core/Environment";
-import {ExtensionResources} from "./Core/ExtensionResources";
-import {GameId} from "./Core/GameId";
-import {Info} from "./Core/Info";
-import {PermissionOptions, Permissions} from "./Core/Permissions";
-import {Version} from "./Core/Version";
-
-export {
-    ErrorParser, Errors,
-    HTML, HTMLParser,
-    Language, Localization,
-    CookieStorage, LocalStorage, SyncedStorage,
-    BackgroundSimple, StringUtils, TimeUtils,
-    Debug,
-    Downloader,
-    Environment,
-    ExtensionResources,
-    GameId,
-    Info,
-    Permissions, PermissionOptions,
-    Version
-};
+export {ErrorParser} from "./Core/Errors/ErrorParser";
+export {Errors} from "./Core/Errors/Errors";
+export {HTML} from "./Core/Html/Html";
+export {HTMLParser} from "./Core/Html/HtmlParser";
+export {Language} from "./Core/Localization/Language";
+export {Localization} from "./Core/Localization/Localization";
+export {CookieStorage} from "./Core/Storage/CookieStorage";
+export {LocalStorage} from "./Core/Storage/LocalStorage";
+export {SyncedStorage} from "./Core/Storage/SyncedStorage";
+export {BackgroundSimple} from "./Core/BackgroundSimple";
+export {StringUtils} from "./Core/Utils/StringUtils";
+export {TimeUtils} from "./Core/Utils/TimeUtils";
+export {Debug} from "./Core/Debug";
+export {Downloader} from "./Core/Downloader";
+export {Environment} from "./Core/Environment";
+export {ExtensionResources} from "./Core/ExtensionResources";
+export {GameId} from "./Core/GameId";
+export {Info} from "./Core/Info";
+export {PermissionOptions, Permissions} from "./Core/Permissions";
+export {Version} from "./Core/Version";
