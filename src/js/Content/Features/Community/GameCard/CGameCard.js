726b9c5c8d621c148595c9a80924ad96b81d3a7d
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2021-01-19 15:38:56 +0100

    Add source of CGameCard.prototype.saleAppids

--- src/js/Content/Features/Community/GameCard/CGameCard.js
+++ src/js/Content/Features/Community/GameCard/CGameCard.js
@@ -18,7 +18,11 @@ export class CGameCard extends CCommunityBase {
         this.appid = GameId.getAppidFromGameCard(window.location.pathname);
         this.isFoil = window.location.search.includes("?border=1");
 
-        // Steam sale events that have cards but no store page or trading forum
+        /*
+         * Steam sale events that have cards but no store page or trading forum
+         * https://github.com/JustArchiNET/ArchiSteamFarm/blob/8a9f25edcbf1482efc4d5a1fdb3a09637f2eb12b/ArchiSteamFarm/CardsFarmer.cs#L49
+         */
+        // eslint-disable-next-line max-len
         this.saleAppids = [267420, 303700, 335590, 368020, 425280, 480730, 566020, 639900, 762800, 876740, 991980, 1195670, 1343890, 1465680];
     }
 }
