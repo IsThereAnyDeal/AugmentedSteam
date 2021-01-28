ce15ecf9c205b64bb6529f09b9468b4dd0a869cc
candela97 <54083835+candela97@users.noreply.github.com>
2021-01-29 01:59:55 +0800

    Simplify FSkipAgecheck and cleanup CApp

--- src/js/Content/Features/Community/App/CApp.js
+++ src/js/Content/Features/Community/App/CApp.js
@@ -1,32 +1,23 @@
 import ContextType from "../../../Modules/Context/ContextType";
 import {GameId} from "../../../../Core/GameId";
+import {CCommunityBase} from "../CCommunityBase";
 import FHighlightsTags from "../../Common/FHighlightsTags";
 import FSkipAgecheck from "../../Common/FSkipAgecheck";
-import {CCommunityBase} from "../CCommunityBase";
 import FCommunityAppPageLinks from "./FCommunityAppPageLinks";
 import FCommunityAppPageWishlist from "./FCommunityAppPageWishlist";
 
 export class CApp extends CCommunityBase {
 
-    constructor(type = ContextType.COMMUNITY_APP, features = []) {
+    constructor() {
 
-        features.push(
+        super(ContextType.COMMUNITY_APP, [
             FCommunityAppPageLinks,
             FCommunityAppPageWishlist,
             FSkipAgecheck,
-        );
-
-        super(type, features);
+        ]);
 
         this.appid = GameId.getAppid(window.location.href);
 
         FHighlightsTags.highlightTitle(this.appid);
-
-        const node = document.querySelector(".apphub_background");
-        if (node) {
-            new MutationObserver(() => {
-                this.triggerCallbacks();
-            }).observe(node, {"attributes": true}); // display changes to none if age gate is shown
-        }
     }
 }
