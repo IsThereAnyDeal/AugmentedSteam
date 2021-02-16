434c677819cd33b7d185b2f0badafe6589f00516
candela97 <54083835+candela97@users.noreply.github.com>
2021-02-16 18:10:21 +0800

    Simplify FRemoveBroadcasts

--- src/js/Content/Features/Store/App/FRemoveBroadcasts.js
+++ src/js/Content/Features/Store/App/FRemoveBroadcasts.js
@@ -5,17 +5,13 @@ import {Page} from "../../Page";
 export default class FRemoveBroadcasts extends Feature {
 
     checkPrerequisites() {
-        if (!SyncedStorage.get("removebroadcasts")) { return false; }
-
-        const el = document.querySelector('#game_highlights > [data-featuretarget="broadcast-embed"]');
-        if (el === null) { return false; }
-
-        this._el = el;
-        return true;
+        return SyncedStorage.get("removebroadcasts")
+            && document.querySelector("[data-featuretarget=broadcast-embed]") !== null;
     }
 
     apply() {
-        this._el.remove();
+
+        document.querySelector("[data-featuretarget=broadcast-embed]").remove();
 
         Page.runInPageContext(() => {
             const bcStore = window.uiBroadcastWatchStore;
