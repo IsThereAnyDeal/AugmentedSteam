f7901babe9eaef8a0d41f083358bb7004e0a9238
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2021-03-01 19:13:45 +0100

    Update wishlist stats when changing filers
    Resolves #1095

--- src/js/Content/Features/Store/Wishlist/FWishlistStats.js
+++ src/js/Content/Features/Store/Wishlist/FWishlistStats.js
@@ -1,5 +1,5 @@
 import {HTML, Localization, SyncedStorage} from "../../../../modulesCore";
-import {Feature, Price} from "../../../modulesContent";
+import {Feature, Messenger, Price} from "../../../modulesContent";
 import {Page} from "../../Page";
 
 export default class FWishlistStats extends Feature {
@@ -10,12 +10,34 @@ export default class FWishlistStats extends Feature {
 
     async apply() {
 
-        const appInfo = await Page.runInPageContext(() => window.SteamFacade.global("g_rgAppInfo"), null, true);
+        this._appInfo = await Page.runInPageContext(() => window.SteamFacade.global("g_rgAppInfo"), null, true);
+
+        HTML.beforeBegin("#wishlist_ctn", '<div id="esi-wishlist-chart-content"></div>');
+
+        Messenger.addMessageListener("wlUpdate", () => { this._updateStats(); });
+
+        Page.runInPageContext(() => {
+            const updateOld = window.CWishlistController.prototype.Update;
+
+            window.CWishlistController.prototype.Update = function(...args) {
+                updateOld.call(this, args);
+
+                window.Messenger.postMessage("wlUpdate");
+            };
+        });
+
+        this._updateStats();
+    }
+
+    async _updateStats() {
 
         let totalPrice = 0;
         let totalCount = 0;
         let totalOnSale = 0;
         let totalNoPrice = 0;
+
+        const visibleApps = await Page.runInPageContext(() => window.SteamFacade.global("g_Wishlist").rgVisibleApps, null, true);
+        const appInfo = visibleApps.map(appid => this._appInfo[appid]);
 
         for (const data of Object.values(appInfo)) {
             if (data.subs.length > 0) {
@@ -31,12 +53,10 @@ export default class FWishlistStats extends Feature {
         }
         totalPrice = new Price(totalPrice / 100);
 
-        HTML.beforeBegin("#wishlist_ctn",
-            `<div id="esi-wishlist-chart-content">
-                <div class="esi-wishlist-stat"><span class="num">${totalPrice}</span>${Localization.str.wl.total_price}</div>
-                <div class="esi-wishlist-stat"><span class="num">${totalCount}</span>${Localization.str.wl.in_wishlist}</div>
-                <div class="esi-wishlist-stat"><span class="num">${totalOnSale}</span>${Localization.str.wl.on_sale}</div>
-                <div class="esi-wishlist-stat"><span class="num">${totalNoPrice}</span>${Localization.str.wl.no_price}</div>
-            </div>`);
+        HTML.inner("#esi-wishlist-chart-content",
+            `<div class="esi-wishlist-stat"><span class="num">${totalPrice}</span>${Localization.str.wl.total_price}</div>
+            <div class="esi-wishlist-stat"><span class="num">${totalCount}</span>${Localization.str.wl.in_wishlist}</div>
+            <div class="esi-wishlist-stat"><span class="num">${totalOnSale}</span>${Localization.str.wl.on_sale}</div>
+            <div class="esi-wishlist-stat"><span class="num">${totalNoPrice}</span>${Localization.str.wl.no_price}</div>`);
     }
 }
