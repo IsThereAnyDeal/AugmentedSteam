9bb18550fd752baf8f9adbeafd09de79304f142f
candela97 <54083835+candela97@users.noreply.github.com>
2021-03-08 06:18:34 +0800

    Refactor CProfileActivity, only apply highlighting on new nodes

--- src/js/Content/Features/Community/ProfileActivity/CProfileActivity.js
+++ src/js/Content/Features/Community/ProfileActivity/CProfileActivity.js
@@ -1,23 +1,60 @@
+import {GameId} from "../../../../modulesCore";
 import {ContextType, EarlyAccess} from "../../../modulesContent";
 import {CCommunityBase} from "../CCommunityBase";
 import {CommentHandler} from "../../../Modules/Community/CommentHandler";
-import FHighlightFriendsActivity from "./FHighlightFriendsActivity";
+import FHighlightsTags from "../../Common/FHighlightsTags";
+import FAchievementComparisonLink from "./FAchievementComparisonLink";
 
 export class CProfileActivity extends CCommunityBase {
 
     constructor() {
 
         super(ContextType.PROFILE_ACTIVITY, [
-            FHighlightFriendsActivity,
+            FAchievementComparisonLink,
         ]);
+    }
 
-        new MutationObserver(() => {
+    async applyFeatures() {
 
-            // TODO Only apply on new nodes
-            this.triggerCallbacks();
+        await super.applyFeatures();
+
+        const nodes = this._getNodesToHighlight();
+        await FHighlightsTags.highlightAndTag(nodes, false);
+
+        this.triggerCallbacks(nodes); // FAchievementComparisonLink needs to wait on highlighting to check for applicable links
+        this._registerObserver();
+    }
+
+    _getNodesToHighlight(parent = document) {
+
+        const blotterBlocks = parent.querySelectorAll(".blotter_block");
+
+        return Array.from(blotterBlocks).reduce((acc, cur) => {
+            acc.push(
+                ...Array.from(cur.querySelectorAll("a:not(.blotter_gamepurchase_logo)"))
+                    .filter(link => (GameId.getAppid(link) && link.childElementCount <= 1)
+
+                // https://github.com/tfedor/AugmentedSteam/pull/470#pullrequestreview-284928257
+                && (link.childElementCount !== 1 || !link.closest(".vote_header")))
+            );
+            return acc;
+        }, []);
+    }
+
+    _registerObserver() {
+
+        new MutationObserver(async mutations => {
+
             EarlyAccess.showEarlyAccess();
             CommentHandler.hideSpamComments();
 
-        }).observe(document.querySelector("#blotter_content"), {"subtree": true, "childList": true});
+            for (const mutation of mutations) {
+
+                const nodes = this._getNodesToHighlight(mutation.addedNodes[0]);
+                await FHighlightsTags.highlightAndTag(nodes, false);
+
+                this.triggerCallbacks(nodes);
+            }
+        }).observe(document.querySelector("#blotter_content"), {"childList": true});
     }
 }
