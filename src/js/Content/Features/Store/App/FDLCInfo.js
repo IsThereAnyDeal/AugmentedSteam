fb0e246e0fe7404d8cb9b44990852158b4755277
candela97 <54083835+candela97@users.noreply.github.com>
2021-06-13 02:32:01 +0800

    Temporarily remove suggestion link, return early if there's no data

--- src/js/Content/Features/Store/App/FDLCInfo.js
+++ src/js/Content/Features/Store/App/FDLCInfo.js
@@ -1,6 +1,6 @@
+import {HTML, Localization} from "../../../../modulesCore";
 import {Background, Feature} from "../../../modulesContent";
 import Config from "../../../../config";
-import {HTML, Localization} from "../../../../modulesCore";
 
 export default class FDLCInfo extends Feature {
 
@@ -9,30 +9,39 @@ export default class FDLCInfo extends Feature {
     }
 
     async apply() {
+
+        let response;
+
+        try {
+            response = await Background.action("dlcinfo", {"appid": this.context.appid, "appname": this.context.appName});
+            // TODO remove when suggestion link is fixed
+            if (!response || !response.length) { return; }
+        } catch (err) {
+            console.error(err);
+            return;
+        }
+
+        // const suggestUrl = `${Config.PublicHost}/gamedata/dlc_category_suggest.php?appid=${this.context.appid}&appname=${encodeURIComponent(this.context.appName)}`;
+
         let html = `<div class="block responsive_apppage_details_right heading">${Localization.str.dlc_details}</div>
                    <div class="block es_dlc_info">
                    <div class="block_content"><div class="block_content_inner"><div class="details_block">`;
 
-        try {
-            const response = await Background.action("dlcinfo", {"appid": this.context.appid, "appname": this.context.appName});
+        for (const item of response) {
+            const iconUrl = `${Config.PublicHost}/gamedata/icons/${encodeURIComponent(item.icon)}`;
+            const title = HTML.escape(item.desc);
+            const name = HTML.escape(item.name);
 
-            for (const item of response) {
-                const iconUrl = `${Config.PublicHost}/gamedata/icons/${encodeURIComponent(item.icon)}`;
-                const title = HTML.escape(item.desc);
-                const name = HTML.escape(item.name);
-
-                html += `<div class="game_area_details_specs">
-                            <div class="icon"><img src="${iconUrl}"></div>
-                            <a class="name" title="${title}">${name}</a>
-                        </div>`;
-            }
-        } finally {
-            const suggestUrl = `${Config.PublicHost}/gamedata/dlc_category_suggest.php?appid=${this.context.appid}&appname=${encodeURIComponent(this.context.appName)}`;
-            html += `</div>
-                    <br><a class="linkbar" href="${suggestUrl}" target="_blank">${Localization.str.dlc_suggest} <img src="//store.steampowered.com/public/images/v5/ico_external_link.gif"></a>
-                    </div></div></div>`;
-
-            HTML.beforeBegin(document.querySelector("#category_block"), html);
+            html += `<div class="game_area_details_specs">
+                        <div class="icon"><img src="${iconUrl}"></div>
+                        <a class="name" title="${title}">${name}</a>
+                    </div>`;
         }
+
+        html += "</div>";
+        // html += `<br><a class="linkbar" href="${suggestUrl}" target="_blank">${Localization.str.dlc_suggest} <img src="//store.steampowered.com/public/images/v5/ico_external_link.gif"></a>`;
+        html += "</div></div></div>";
+
+        HTML.beforeBegin(document.querySelector("#category_block"), html);
     }
 }
