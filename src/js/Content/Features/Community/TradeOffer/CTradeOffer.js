d718484f1c9124f71a6e315bd82fab3b67e0c99d
Revadike <revadike@outlook.com>
2021-06-20 17:42:59 +0200

    Added my tradeoffers link

--- src/js/Content/Features/Community/TradeOffer/CTradeOffer.js
+++ src/js/Content/Features/Community/TradeOffer/CTradeOffer.js
@@ -1,11 +1,13 @@
 import {Context, ContextType} from "../../../modulesContent";
 import FCountTradeItems from "./FCountTradeItems";
+import FMyTradeOffersLink from "./FMyTradeOffersLink";
 
 export class CTradeOffer extends Context {
 
     constructor() {
         super(ContextType.TRADE_OFFER, [
             FCountTradeItems,
+            FMyTradeOffersLink,
         ]);
     }
 }
