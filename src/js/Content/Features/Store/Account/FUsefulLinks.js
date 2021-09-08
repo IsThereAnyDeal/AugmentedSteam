29150171676263f775422d9ebf7b12d81676ad11
candela97 <54083835+candela97@users.noreply.github.com>
2021-09-08 08:17:47 +0800

    Add two useful links

--- src/js/Content/Features/Store/Account/FUsefulLinks.js
+++ src/js/Content/Features/Store/Account/FUsefulLinks.js
@@ -5,11 +5,29 @@ export default class FUsefulLinks extends Feature {
 
     apply() {
 
+        const blocks = document.querySelectorAll(".account_setting_block");
+
         // View total spent
         HTML.beforeEnd(
-            document.querySelector(".account_setting_block .account_setting_sub_block:nth-child(2)"),
+            blocks[0].querySelector(".account_setting_sub_block:nth-child(2)"),
             `<div>
                 <a class="account_manage_link" href="https://help.steampowered.com/accountdata/AccountSpend">${Localization.str.external_funds}</a>
+            </div>`
+        );
+
+        // View shopping cart history
+        HTML.afterEnd(
+            blocks[0].querySelector(".account_setting_sub_block:nth-child(2) > div"),
+            `<div>
+                <a class="account_manage_link" href="https://help.steampowered.com/accountdata/ShoppingCartHistory">${Localization.str.shopping_cart_history}</a>
+            </div>`
+        );
+
+        // Manage Web API key
+        HTML.beforeEnd(
+            blocks[2].querySelector(".account_setting_sub_block:nth-child(2)"),
+            `<div>
+                <a class="account_manage_link" href="https://steamcommunity.com/dev/apikey">${Localization.str.manage_webapi_key}</a>
             </div>`
         );
     }
