4914b51ad46faaa7cb0f11c24481a428b6472177
candela97 <54083835+candela97@users.noreply.github.com>
2021-09-08 08:24:31 +0800

    Add Steam Cloud section

--- src/js/Content/Features/Store/Account/FUsefulLinks.js
+++ src/js/Content/Features/Store/Account/FUsefulLinks.js
@@ -30,5 +30,21 @@ export default class FUsefulLinks extends Feature {
                 <a class="account_manage_link" href="https://steamcommunity.com/dev/apikey">${Localization.str.manage_webapi_key}</a>
             </div>`
         );
+
+        // Steam Cloud section
+        HTML.afterEnd(
+            blocks[3],
+            `<div class="account_header_line noicon">
+                <div>${Localization.str.steam_cloud_header}</div>
+            </div>
+            <div class="account_setting_block_short">
+                <div class="account_setting_sub_block">
+                    <div>
+                        <a class="account_manage_link" href="https://store.steampowered.com/account/remotestorage" style="margin-top: 8px;">${Localization.str.steam_cloud_saves}</a>
+                    </div>
+                </div>
+                <div style="clear: both;"></div>
+            </div>`
+        );
     }
 }
