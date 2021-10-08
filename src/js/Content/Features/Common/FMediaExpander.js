62a2d5cad640c416a3bdef2858a71e3fa63e36f0
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2021-10-08 16:25:26 +0200

    Handle media expander on workshop pages

--- src/js/Content/Features/Common/FMediaExpander.js
+++ src/js/Content/Features/Common/FMediaExpander.js
@@ -1,4 +1,4 @@
-import {HTML, LocalStorage, Localization, TimeUtils} from "../../../modulesCore";
+import {HTML, LocalStorage, Localization} from "../../../modulesCore";
 import {ContextType, Feature} from "../../modulesContent";
 import {Page} from "../Page";
 
@@ -19,28 +19,10 @@ export default class FMediaExpander extends Feature {
         this._details.classList.add("as-side-details");
 
         this._sliderToggle = document.querySelector(".es_slider_toggle");
-        this._sliderToggle.addEventListener("click", e => {
-            e.preventDefault();
-            e.stopPropagation();
+        this._sliderToggle.addEventListener("click", () => { this._toggleView(); });
 
+        if (LocalStorage.get("expand_slider", false)) {
             this._toggleView();
-        });
-
-        const expandSlider = LocalStorage.get("expand_slider", false);
-        if (expandSlider) {
-            this._toggleView();
-
-            /*for (const node of document.querySelectorAll(".es_side_details_wrap, .es_side_details")) {
-
-                // shrunk => expanded
-                node.style.display = null;
-                node.style.opacity = 1;
-            }
-
-            // Triggers the adjustment of the slider scroll bar
-            TimeUtils.timer(250).then(() => {
-                window.dispatchEvent(new Event("resize"));
-            });*/
         }
 
         /*
@@ -57,44 +39,6 @@ export default class FMediaExpander extends Feature {
             container.addEventListener("mouseleave", () => {
                 this._sliderToggle.style.top = null;
             });
-        }
-    }
-
-    _buildSideDetails() {
-        if (this._detailsBuilt) { return; }
-        this._detailsBuilt = true;
-
-        const details = this._details;
-
-        if (details.matches(".rightcol")) {
-
-            // Clone details on a store page
-            let detailsClone = details.querySelector(".glance_ctn");
-            if (!detailsClone) { return; }
-            detailsClone = detailsClone.cloneNode(true);
-            detailsClone.classList.add("es_side_details", "block", "responsive_apppage_details_left");
-
-            const detailsWrap = HTML.wrap('<div class="es_side_details_wrap"></div>', detailsClone);
-            const target = document.querySelector("div.rightcol.game_meta_data");
-            if (target) {
-                target.insertAdjacentElement("afterbegin", detailsWrap);
-            }
-        } else {
-
-            // Clone details in the workshop
-            const detailsClone = details.cloneNode(true);
-            detailsClone.classList.add("panel", "es_side_details");
-            HTML.afterBegin(detailsClone, `<div class="title">${Localization.str.details}</div><div class="hr padded"></div>`);
-            let target = document.querySelector(".sidebar");
-            if (target) {
-                target.insertAdjacentElement("afterbegin", detailsClone);
-            }
-
-            // Sometimes for a split second the slider pushes the details down, this fixes it
-            target = document.querySelector(".highlight_ctn");
-            if (target) {
-                HTML.wrap('<div class="leftcol"></div>', target);
-            }
         }
     }
 
@@ -118,12 +62,6 @@ export default class FMediaExpander extends Feature {
                 this._handleWorkshop(expand);
             }
 
-            /*
-             * Triggers the adjustment of the slider scroll bar.
-             * https://github.com/SteamDatabase/SteamTracking/blob/ad4e85261f2322eae0b0125e46d7d753bf755730/store.steampowered.com/public/javascript/gamehighlightplayer.js#L101
-             */
-            Page.runInPageContext(() => { window.SteamFacade.jqTrigger(window, "resize.GameHighlightPlayer"); });
-
             this._details.style.opacity = null;
         }, {"once": true});
 
@@ -142,8 +80,25 @@ export default class FMediaExpander extends Feature {
             document.getElementById("game_highlights").insertAdjacentElement("afterbegin", this._details);
         }
 
-        Page.runInPageContext(() => { window.SteamFacade.adjustVisibleAppTags(".popular_tags"); });
+        Page.runInPageContext(() => {
+            window.SteamFacade.adjustVisibleAppTags(".popular_tags");
+
+            /*
+             * Triggers the adjustment of the slider scroll bar.
+             * https://github.com/SteamDatabase/SteamTracking/blob/ad4e85261f2322eae0b0125e46d7d753bf755730/store.steampowered.com/public/javascript/gamehighlightplayer.js#L101
+             */
+            window.SteamFacade.jqTrigger(window, "resize.GameHighlightPlayer");
+        });
     }
 
-    _handleWorkshop(expand) {}
+    _handleWorkshop(expand) {
+
+        this._details.classList.toggle("panel", expand);
+
+        if (expand) {
+            document.querySelector(".sidebar").insertAdjacentElement("afterbegin", this._details);
+        } else {
+            document.querySelector(".highlight_ctn").insertAdjacentElement("afterend", this._details);
+        }
+    }
 }
