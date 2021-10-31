3b05060d9317e823ec6de18b534c3682c5d41bef
Tomáš Fedor <tomas.fedor3@gmail.com>
2021-10-31 03:29:21 +0100

    Fix update locales linting issues, package config

--- config/scripts/locales.cjs
+++ config/scripts/locales.cjs
@@ -3,10 +3,10 @@ const axios = require("axios");
 const FormData = require("form-data");
 
 const Config = {
-    exportUrl: "https://api.poeditor.com/v2/projects/export",
-    apiToken: "8991d8f3eb8072b32b0e664d3a196384",
-    projectId: 319277
-}
+    "exportUrl": "https://api.poeditor.com/v2/projects/export",
+    "apiToken": "8991d8f3eb8072b32b0e664d3a196384",
+    "projectId": 319277
+};
 
 // code in POE: our file name
 const LanguageMap = {
@@ -37,30 +37,30 @@ const LanguageMap = {
     "vi": "vi",
     "zh-Hans": "zh-CN",
     "zh-TW": "zh-TW",
-}
+};
 
 async function loadLanguage(languageCode) {
     const data = new FormData();
-    data.append('api_token', Config.apiToken);
-    data.append('id', Config.projectId);
-    data.append('language', languageCode);
-    data.append('type', "key_value_json");
+    data.append("api_token", Config.apiToken);
+    data.append("id", Config.projectId);
+    data.append("language", languageCode);
+    data.append("type", "key_value_json");
 
-    let result = await axios.post(Config.exportUrl, data, { headers: data.getHeaders() });
+    let result = await axios.post(Config.exportUrl, data, {"headers": data.getHeaders()});
     const response = result.data;
 
-    if (response.response.code != 200) {
+    if (response.response.code !== "200") {
         throw Error;
     }
 
     const url = response.result.url;
-    const filepath = `${__dirname}/../../src/localization/${LanguageMap[languageCode]}.json`
-    result = await axios.get(url, {responseType: "stream"});
+    const filepath = `${__dirname}/../../src/localization/${LanguageMap[languageCode]}.json`;
+    result = await axios.get(url, {"responseType": "stream"});
     result.data.pipe(fs.createWriteStream(filepath));
     return filepath;
 }
 
-for (let languageCode of Object.keys(LanguageMap)) {
+for (const languageCode of Object.keys(LanguageMap)) {
     loadLanguage(languageCode)
         .then(path => console.log(`OK ${languageCode}: ${path}`))
         .catch(e => {
