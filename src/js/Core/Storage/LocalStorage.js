6756cbea2018c189d89616e001debc52e820a51e
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2021-11-11 16:34:07 +0100

    Migrate localStorage contents

--- src/js/Core/Storage/LocalStorage.js
+++ src/js/Core/Storage/LocalStorage.js
@@ -1,15 +1,57 @@
 
 import {Storage} from "./Storage";
 import {SyncedStorage} from "./SyncedStorage";
+import {Environment} from "../Environment";
 
 class LocalStorage extends Storage {
 
-    static init() {
+    static async init() {
         if (SyncedStorage._adapter === this._adapter) {
-            return Promise.resolve(this.cache = SyncedStorage.cache);
+            this.cache = SyncedStorage.cache;
+        } else {
+            await super.init();
         }
-        this.cache = {};
-        return super.init();
+
+        // TODO Remove after some versions
+        return this._migrateLocalStorage();
+    }
+
+    static async _migrateLocalStorage() {
+
+        const migrationDone = this.get("local_storage_migration");
+
+        let type;
+
+        if (location.hostname === "store.steampowered.com") {
+            type = "store";
+        } else if (location.hostname === "steamcommunity.com") {
+            type = "community";
+        } else if (Environment.isContentScript()) {
+            return null;
+        } else {
+            type = "extension";
+        }
+
+        if (migrationDone[type]) { return null; }
+
+        await Promise.all(Object.keys(LocalStorage.defaults).map(async key => {
+
+            let value = localStorage.getItem(key);
+            if (value === null) { return; }
+
+            try {
+                value = JSON.parse(value);
+            } catch (err) {
+                console.error("Can't parse value", value);
+                throw err;
+            }
+
+            await LocalStorage.set(key, value);
+            localStorage.removeItem(key);
+        }));
+
+        migrationDone[type] = true;
+        return LocalStorage.set("local_storage_migration", migrationDone);
     }
 }
 
@@ -31,6 +73,9 @@ LocalStorage.defaults = Object.freeze({
     "support_info": null,
     "hide_login_warn_store": false,
     "hide_login_warn_community": false,
+    "local_storage_migration": {"store": false, "community": false, "extension": false},
 });
+
+LocalStorage.cache = {}
 
 export {LocalStorage};
