IsThereAnyDeal/AugmentedSteam

commit 97b83dad184f6cc5ffe36a2e3422073eae9962a3

Remove objPath parameter from endpointFactory

src/js/Background/Modules/Api.js
@@ -38,22 +38,8 @@ class Api {
         return this.responseHandler(responseFn, response);
     }
 
-    static endpointFactory(endpoint, objPath) {
-        return async params => {
-            let result = await this.getEndpoint(endpoint, params);
-            if (objPath) {
-                if (Array.isArray(objPath)) {
-                    for (const part of objPath) {
-                        result = result?.[part];
-                    }
-                } else {
-                    result = result?.[objPath];
-                }
-            } else {
-                result = result.data;
-            }
-            return result;
-        };
+    static endpointFactory(endpoint) {
+        return params => this.getEndpoint(endpoint, params);
     }
 
     static endpointFactoryCached(endpoint, storeName, mapFn) {

src/js/Background/Modules/SteamStoreApi.js
@@ -202,10 +202,13 @@ class SteamStoreApi extends Api {
         const params = {"appids": appid};
         if (filter) { params.filters = filter; }
 
-        return SteamStoreApi.endpointFactory("api/appdetails/", appid)(params);
+        return SteamStoreApi.endpointFactory("api/appdetails/")(params).then(result => result[appid]);
     }
 
-    static appUserDetails(appid) { return SteamStoreApi.endpointFactory("api/appuserdetails/", appid)({"appids": appid}); }
+    static appUserDetails(appid) {
+        return SteamStoreApi.endpointFactory("api/appuserdetails/")({"appids": appid})
+            .then(result => result.appid);
+    }
 }
 SteamStoreApi.origin = "https://store.steampowered.com/";
 SteamStoreApi.params = {"credentials": "include"};

commit 6c3fc9ad93a994a16f408de9566407d346681a3f

Return expected JSON property
Bug introduced in 97b83dad184f6cc5ffe36a2e3422073eae9962a3

src/js/Background/Modules/Api.js
@@ -56,9 +56,7 @@ class Api {
             let result = await this.endpointFactory(endpoint, method)(params);
 
             if (mapFn) {
-                result = mapFn(result.data);
-            } else {
-                result = result.data;
+                result = mapFn(result);
             }
 
             return IndexedDB.put(storeName, typeof key === "undefined" ? result : new Map([[key, result]]));

src/js/Background/Modules/AugmentedSteamApi.js
@@ -56,8 +56,7 @@ class AugmentedSteamApi extends Api {
         if (json?.result !== "success") {
             throw new Error("Endpoint not successfully retrieved");
         }
-        delete json.result;
-        return json;
+        return json.data;
     }
 
     static storePageData(appid, metalink, showoc) {

src/js/Background/Modules/ITADApi.js
@@ -71,6 +71,10 @@ class ITADApi extends Api {
         };
     }
 
+    static async getEndpoint(...args) { return (await super.getEndpoint(...args)).data; }
+    static async postEndpoint(...args) { return (await super.postEndpoint(...args)).data; }
+    static async deleteEndpoint(...args) { return (await super.deleteEndpoint(...args)).data; }
+
     static async addToWaitlist(appids) {
         if (!appids || (Array.isArray(appids) && !appids.length)) {
             console.warn("Can't add nothing to ITAD waitlist");