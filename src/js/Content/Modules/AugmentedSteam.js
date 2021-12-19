f7a5b89e0f19c37fbb9d75e300431da5c48ed8d6
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2021-12-19 18:49:55 +0100

    Name argument

--- src/js/Content/Modules/AugmentedSteam.js
+++ src/js/Content/Modules/AugmentedSteam.js
@@ -289,7 +289,7 @@ class AugmentedSteam {
 
         // https://github.com/SteamDatabase/SteamTracking/blob/cdf367ce61926a896fe54d710b3ed25d66d7e333/store.steampowered.com/public/javascript/game.js#L1785
         Page.runInPageContext(() => {
-            window.ShowGotSteamModal = function(...args) { window.location.assign(args[0]); };
+            window.ShowGotSteamModal = function(steamUrl) { window.location.assign(steamUrl); };
         });
     }
 
