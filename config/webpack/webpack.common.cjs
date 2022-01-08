c56ae3904ffff668e032e13ccecc0056f5637393
candela97 <54083835+candela97@users.noreply.github.com>
2022-01-09 07:34:39 +0800

    Remove `SALE` context and selectors for the old sale pages

--- config/webpack/webpack.common.cjs
+++ config/webpack/webpack.common.cjs
@@ -51,7 +51,6 @@ module.exports = {
         "js/store/funds": "./src/js/Content/Features/Store/Funds/PFunds.js",
         "js/store/points_shop": "./src/js/Content/Features/Store/PointsShop/PPointsShop.js",
         "js/store/registerkey": "./src/js/Content/Features/Store/RegisterKey/PRegisterKey.js",
-        "js/store/sale": "./src/js/Content/Features/Store/Sale/PSale.js",
         "js/store/search": "./src/js/Content/Features/Store/Search/PSearch.js",
         "js/store/stats": "./src/js/Content/Features/Store/Stats/PStats.js",
         "js/store/sub": "./src/js/Content/Features/Store/Sub/PSub.js",
