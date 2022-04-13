edbc05b9b6a317ee0c439ebb4254a62e6afa380a
candela97 <54083835+candela97@users.noreply.github.com>
2022-04-14 01:11:04 +0800

    Fix points shop background preview link due to Steam changes

--- src/js/Content/Features/Store/PointsShop/FBackgroundPreviewLink.js
+++ src/js/Content/Features/Store/PointsShop/FBackgroundPreviewLink.js
@@ -16,7 +16,7 @@ export default class FBackgroundPreviewLink extends Feature {
                 const previewEl = await new Promise(resolve => {
                     new MutationObserver((mutations, observer) => {
                         observer.disconnect();
-                        resolve(mutations[0].addedNodes[0].querySelector("[class^=redeempointsmodal_BackgroundPreviewContainer] > [class^=redeempointsmodal_PreviewBackgroundContainer]")?.lastElementChild);
+                        resolve(mutations[0].addedNodes[0].querySelector("[class*=redeempointsmodal_BackgroundPreviewContainer] > [class*=redeempointsmodal_PreviewBackgroundContainer]")?.lastElementChild);
                     }).observe(addedNodes[0], {"childList": true, "subtree": true});
                 });
 
@@ -29,7 +29,7 @@ export default class FBackgroundPreviewLink extends Feature {
                 bgLink = bgLink.match(/images\/items\/(\d+)\/([a-z0-9.]+)/i);
 
                 if (bgLink) {
-                    HTML.beforeBegin(addedNodes[0].querySelector("[class^=redeempointsmodal_BackgroundPreviewContainer]"),
+                    HTML.beforeBegin(addedNodes[0].querySelector("[class*=redeempointsmodal_BackgroundPreviewContainer]"),
                         `<div class="as_preview_background_ctn">
                             <a class="as_preview_background" target="_blank" href="${User.profileUrl}#previewBackground/${bgLink[1]}/${bgLink[2]}">
                                 ${Localization.str.preview_background}
@@ -37,6 +37,6 @@ export default class FBackgroundPreviewLink extends Feature {
                         </div>`);
                 }
             }
-        }).observe(document.querySelector("[class^=app_App]"), {"childList": true});
+        }).observe(document.body, {"childList": true});
     }
 }
