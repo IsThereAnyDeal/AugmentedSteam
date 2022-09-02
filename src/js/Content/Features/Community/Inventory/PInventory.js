cc1e08d52db5496558eaf070089739a474e12323
candela97 <54083835+candela97@users.noreply.github.com>
2022-09-02 19:42:55 +0800

    Fix and improve match patterns for content scripts

--- src/js/Content/Features/Community/Inventory/PInventory.js
+++ src/js/Content/Features/Community/Inventory/PInventory.js
@@ -1,12 +1,4 @@
 import {CommunityPage} from "../../CommunityPage";
 import {CInventory} from "./CInventory";
-import {CCommunityBase} from "../CCommunityBase";
 
-const page = new CommunityPage();
-
-// This regex can't be translated to a match pattern / glob combination in the manifest
-if (/^\/(?:id|profiles)\/[^/]+\/inventory\/?$/.test(window.location.pathname)) {
-    page.run(CInventory);
-} else {
-    page.run(CCommunityBase);
-}
+(new CommunityPage()).run(CInventory);
