a18e46a65dadd721d521af444fdd782e618c4600
candela97 <54083835+candela97@users.noreply.github.com>
2022-09-10 23:32:30 +0800

    FMarketLowestPrice: better error handling, minor refactoring

--- src/js/Content/Features/Community/MarketHome/FMarketLowestPrice.js
+++ src/js/Content/Features/Community/MarketHome/FMarketLowestPrice.js
@@ -16,38 +16,20 @@ export default class FMarketLowestPrice extends Feature {
             .observe(document.getElementById("tabContentsMyActiveMarketListingsRows"), {"childList": true});
 
         // update tables' headers
-        for (const node of this._parentNode.querySelectorAll("#my_market_listingsonhold_number,#my_market_selllistings_number")) {
+        for (const node of this._parentNode.querySelectorAll("#my_market_listingsonhold_number, #my_market_selllistings_number")) {
 
             const listingNode = node.closest(".my_listing_section");
             if (listingNode.classList.contains("es_selling")) { continue; }
             listingNode.classList.add("es_selling");
 
-            const headerNode = listingNode.querySelector(".market_listing_table_header span");
-            if (!headerNode) { continue; }
+            const editNode = listingNode.querySelector(".market_listing_edit_buttons");
+            if (!editNode) { continue; }
 
-            headerNode.style.width = "200px"; // TODO do we still need to change width?
-            HTML.afterEnd(headerNode,
-                `<span class='market_listing_right_cell market_listing_my_price'><span class='es_market_lowest_button'>${Localization.str.lowest}</span></span>`);
+            editNode.style.width = "200px";
+            HTML.afterEnd(editNode, `<span class="market_listing_right_cell market_listing_my_price">${Localization.str.lowest}</span>`);
         }
 
         this._insertPrices();
-    }
-
-    _insertPrice(node, data) {
-
-        node.classList.add("es_priced");
-
-        const lowestNode = node.querySelector(".market_listing_es_lowest");
-        lowestNode.textContent = data.lowest_price;
-
-        const myPrice = Price.parseFromString(node.querySelector(".market_listing_price span span").textContent);
-        const lowPrice = Price.parseFromString(data.lowest_price);
-
-        if (myPrice.value <= lowPrice.value) {
-            lowestNode.classList.add("es_percentage_lower"); // Ours matches the lowest price
-        } else {
-            lowestNode.classList.add("es_percentage_higher"); // Our price is higher than the lowest price
-        }
     }
 
     async _insertPrices() {
@@ -56,14 +38,14 @@ export default class FMarketLowestPrice extends Feature {
         const rows = [];
 
         for (const node of this._parentNode.querySelectorAll(".es_selling .market_listing_row")) {
-            if (node.querySelector(".market_listing_es_lowest")) { continue; }
-            const button = node.querySelector(".market_listing_edit_buttons");
-            button.style.width = "200px"; // TODO do we still need to change width?
+            if (node.querySelector(".es_market_listing_lowest") !== null) { continue; }
 
-            HTML.afterEnd(node.querySelector(".market_listing_edit_buttons"),
-                "<div class='market_listing_right_cell market_listing_my_price market_listing_es_lowest'>&nbsp;</div>");
+            const button = node.querySelector(".market_listing_edit_buttons.placeholder");
+            button.style.width = "200px";
 
-            // we do this because of changed width, right?
+            HTML.afterEnd(button, `<div class="market_listing_right_cell market_listing_my_price es_market_listing_lowest">${Localization.str.loading}</div>`);
+
+            // Move the actual button due to changed width
             const actualButton = node.querySelector(".market_listing_edit_buttons.actual_content");
             actualButton.style.width = "inherit";
             button.append(actualButton);
@@ -75,61 +57,73 @@ export default class FMarketLowestPrice extends Feature {
             const linkNode = node.querySelector(".market_listing_item_name_link");
             if (!linkNode) { continue; }
 
-            const m = linkNode.pathname.match(/\/(\d+)\/(.+)$/);
-            if (!m) { continue; }
-            const marketHashName = m[2];
+            const [, appid, marketHashName] = linkNode.pathname.match(/^\/market\/listings\/(\d+)\/([^/]+)/) || [];
+            if (!appid || !marketHashName) { continue; }
 
-            let priceData;
+            const lowestNode = node.querySelector(".es_market_listing_lowest");
+            const data = this._loadedMarketPrices[marketHashName] || await this._getPriceOverview(node, Number(appid), marketHashName);
 
-            if (this._loadedMarketPrices[marketHashName]) {
-                priceData = this._loadedMarketPrices[marketHashName];
-            } else {
-                priceData = await this._getPriceOverview(node, parseInt(m[1]), marketHashName);
+            if (!data) {
+                lowestNode.textContent = Localization.str.theworderror;
+                continue;
             }
 
-            if (priceData !== null) {
-                this._insertPrice(node, priceData);
+            if (!("lowest_price" in data)) {
+                lowestNode.textContent = Localization.str.no_data;
+                continue;
+            }
+
+            lowestNode.textContent = data.lowest_price;
+
+            const myPrice = Price.parseFromString(node.querySelector(".market_listing_price span span").textContent);
+            const lowPrice = Price.parseFromString(data.lowest_price);
+
+            if (myPrice.value <= lowPrice.value) {
+                lowestNode.classList.add("es_priced_lower"); // Our price matches the lowest price
+            } else {
+                lowestNode.classList.add("es_priced_higher"); // Our price is higher than the lowest price
             }
         }
     }
 
     async _getPriceOverview(node, appid, marketHashName) {
 
-        let done = false;
-        let priceData = null;
+        const country = User.storeCountry;
+        const currencyNumber = CurrencyManager.currencyTypeToNumber(CurrencyManager.storeCurrency);
+
+        let data;
+        let attempt = 0;
 
         do {
             try {
-                const data = await RequestData.getJson(
-                    `https://steamcommunity.com/market/priceoverview/?country=${User.storeCountry}`
-                    + `&currency=${CurrencyManager.currencyTypeToNumber(CurrencyManager.storeCurrency)}`
-                    + `&appid=${appid}`
-                    + `&market_hash_name=${marketHashName}`
+                data = await RequestData.getJson(
+                    `https://steamcommunity.com/market/priceoverview/?country=${country}&currency=${currencyNumber}&appid=${appid}&market_hash_name=${marketHashName}`
                 );
 
-                await TimeUtils.timer(1000);
+                if (!data || !data.success) {
+                    throw new Error();
+                }
 
-                done = true;
                 this._loadedMarketPrices[marketHashName] = data;
-                priceData = data;
+                await TimeUtils.timer(1000);
+                break;
             } catch (err) {
 
                 // Too Many Requests
                 if (err instanceof Errors.HTTPError && err.code === 429) {
                     await TimeUtils.timer(30000);
                     if (node) { // If the node still exists after this timeout
-                        done = false;
+                        attempt++;
                     } else {
-                        return null;
+                        break;
                     }
                 } else {
                     console.error("Failed to retrieve price overview for item %s!", marketHashName);
                     break;
                 }
-
             }
-        } while (!done);
+        } while (attempt < 2); // Give up after 2 tries
 
-        return priceData;
+        return data;
     }
 }
