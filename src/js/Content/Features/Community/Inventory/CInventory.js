94abf6259e1b326f22aff27c773b27d9588fa2d9
candela97 <54083835+candela97@users.noreply.github.com>
2022-11-10 02:12:22 +0800

    Support equipping more profile items from inventory

--- src/js/Content/Features/Community/Inventory/CInventory.js
+++ src/js/Content/Features/Community/Inventory/CInventory.js
@@ -1,7 +1,7 @@
 import {CommunityUtils, ContextType, Messenger} from "../../../modulesContent";
 import {CCommunityBase} from "../CCommunityBase";
 import FAddPriceToGifts from "./FAddPriceToGifts";
-import FSetBackgroundOption from "./FSetBackgroundOption";
+import FEquipProfileItems from "./FEquipProfileItems";
 import FBadgeProgressLink from "./FBadgeProgressLink";
 import FOneClickGemsOption from "./FOneClickGemsOption";
 import FQuickSellOptions from "./FQuickSellOptions";
@@ -22,7 +22,7 @@ export class CInventory extends CCommunityBase {
 
         super(ContextType.INVENTORY, [
             FAddPriceToGifts,
-            FSetBackgroundOption,
+            FEquipProfileItems,
             FBadgeProgressLink,
             FOneClickGemsOption,
             FQuickSellOptions,
@@ -66,13 +66,61 @@ export class CInventory extends CCommunityBase {
                 const contextId = Number(item.contextid);
                 const globalId = Number(inv.appid);
 
-                let appid = 0;
-                let isBooster = false;
-
-                // Only parse hashname if the item is a Steam item
+                // Only parse these if the item is a Steam item
+                let appid, itemType;
                 if (contextId === 6 && globalId === 753) {
-                    appid = parseInt(hashName);
-                    isBooster = /Booster Pack/i.test(hashName);
+                    appid = parseInt(hashName); // Should start with "real" appid
+                    
+                    itemType = item.description.tags.find(tag => tag.category === "item_class")?.internal_name;
+
+                    // https://github.com/JustArchiNET/ArchiSteamFarm/blob/f55f58a8ef61ba830ed1bee88e5e895b9e4f479d/ArchiSteamFarm/Steam/Data/InventoryResponse.cs#L150
+                    switch (itemType) {
+                        case "item_class_2":
+                            itemType = "card";
+                            break;
+                        case "item_class_3":
+                            itemType = "profilebackground";
+                            break;
+                        case "item_class_4":
+                            itemType = "emoticon";
+                            break;
+                        case "item_class_5":
+                            itemType = "booster";
+                            break;
+                        case "item_class_6":
+                            itemType = "consumable";
+                            break;
+                        case "item_class_7":
+                            itemType = "gems";
+                            break;
+                        case "item_class_8":
+                            itemType = "profilemodifier";
+                            break;
+                        case "item_class_10":
+                            itemType = "saleitem";
+                            break;
+                        case "item_class_11":
+                            itemType = "sticker";
+                            break;
+                        case "item_class_12":
+                            itemType = "chateffect";
+                            break;
+                        case "item_class_13":
+                            itemType = "miniprofilebackground";
+                            break;
+                        case "item_class_14":
+                            itemType = "avatarframe";
+                            break;
+                        case "item_class_15":
+                            itemType = "animatedavatar";
+                            break;
+                        case "item_class_16":
+                            itemType = "keyboardskin";
+                            break;
+                        default:
+                            itemType = "unknown";
+                            break;
+                    }
                 }
 
                 window.Messenger.postMessage("marketInfo", {
@@ -90,7 +138,7 @@ export class CInventory extends CCommunityBase {
                     lowestListingPrice,
                     restriction,
                     appid,
-                    isBooster,
+                    itemType,
                 });
             });
         });
---
+++ src/js/Content/Features/Community/Inventory/FEquipProfileItems.js
@@ -0,0 +1,81 @@
+import {HTML, Localization} from "../../../../modulesCore";
+import {CallbackFeature, RequestData, User} from "../../../modulesContent";
+
+export default class FEquipProfileItems extends CallbackFeature {
+
+    checkPrerequisites() {
+        return this.context.myInventory;
+    }
+
+    callback({view, assetId, appid, itemType}) {
+
+        // Make sure the selected item is equippable (those returned by the /IPlayerService/GetProfileItemsEquipped/ endpoint)
+        let apiPath;
+        switch (itemType) {
+            case "profilebackground":
+                apiPath = "/IPlayerService/SetProfileBackground/v1/";
+                break;
+            case "profilemodifier":
+                apiPath = "/IQuestService/ActivateProfileModifierItem/v1/";
+                break;
+            case "miniprofilebackground":
+                apiPath = "/IPlayerService/SetMiniProfileBackground/v1/";
+                break;
+            case "avatarframe":
+                apiPath = "/IPlayerService/SetAvatarFrame/v1/";
+                break;
+            case "animatedavatar":
+                apiPath = "/IPlayerService/SetAnimatedAvatar/v1/";
+                break;
+            case "keyboardskin":
+                apiPath = "/IPlayerService/SetSteamDeckKeyboardSkin/v1/";
+                break;
+            default:
+                return;
+        }
+
+        const btn = HTML.toElement(
+            `<a class="btn_small btn_darkblue_white_innerfade">
+                <span>${Localization.str.equip_on_profile}</span>
+            </a>`
+        );
+
+        if (itemType === "profilebackground") {
+            document.getElementById(`iteminfo${view}_item_actions`).append(btn);
+        } else {
+            document.getElementById(`iteminfo${view}_item_owner_actions`).append(btn);
+        }
+
+        // TODO Add checkbox so users can set equip options for the background through /IPlayerService/SetEquippedProfileItemFlags/
+        btn.addEventListener("click", async e => {
+            e.preventDefault();
+
+            if (btn.classList.contains("es_equip_loading") || btn.classList.contains("btn_disabled")) { return; }
+            btn.classList.add("es_equip_loading");
+
+            const formData = new FormData();
+
+            /*
+             * Note: For duplicate items, assetId won't be the same, and the /IPlayerService/GetProfileItemsOwned/ endpoint
+             * will only return one of them (the first one obtained maybe?), but any of them will work for equipping.
+             */
+            formData.append("communityitemid", assetId);
+
+            if (itemType === "profilemodifier") {
+                formData.append("appid", appid);
+                formData.append("activate", true);
+            }
+
+            try {
+                const token = await User.accessToken;
+                await RequestData.post(`https://api.steampowered.com${apiPath}?access_token=${token}`, formData, {"credentials": "omit"});
+
+                btn.classList.add("btn_disabled");
+            } catch (err) {
+                console.error("Failed to equip selected item", err);
+            } finally {
+                btn.classList.remove("es_equip_loading");
+            }
+        });
+    }
+}
