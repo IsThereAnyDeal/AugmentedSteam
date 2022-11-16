aaef7dfa03ecce83dabb9026875402f8ee1defd2
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2022-11-16 13:35:06 +0100

    Remove check if key exists in English localization

--- src/js/Core/Localization/Localization.js
+++ src/js/Core/Localization/Localization.js
@@ -26,10 +26,6 @@ class Localization {
 
             // Object.assign() but deep-assigning objects recursively
             for (const [key, val] of Object.entries(source)) {
-                if (typeof target[key] === "undefined") {
-                    console.warn("The key %s doesn't exist in the English localization file", key);
-                    continue;
-                }
                 if (typeof val === "object") {
                     deepAssign(target[key], val);
                 } else if (val !== "") {
