a2516b12d07327c7d6c8d40c8062f0f21b66b4a5
candela97 <54083835+candela97@users.noreply.github.com>
2022-12-19 16:56:34 +0800

    Load additional base features on CRegisterKey

--- src/js/Content/Features/Store/RegisterKey/CRegisterKey.js
+++ src/js/Content/Features/Store/RegisterKey/CRegisterKey.js
@@ -1,10 +1,17 @@
 import {Context, ContextType} from "../../../modulesContent";
+import FBackToTop from "../../Common/FBackToTop";
+import FKeepSSACheckboxState from "../../Common/FKeepSSACheckboxState";
+import FFocusSearch from "../../Common/FFocusSearch";
 import FMultiProductKeys from "./FMultiProductKeys";
 
 export class CRegisterKey extends Context {
 
     constructor() {
+
         super(ContextType.REGISTER_KEY, [
+            FBackToTop,
+            FKeepSSACheckboxState,
+            FFocusSearch,
             FMultiProductKeys,
         ]);
     }
