6de88d4dd22f202e8de768b24d4a96c43f60e973
candela97 <54083835+candela97@users.noreply.github.com>
2022-12-19 16:56:23 +0800

    Load additional base features on CAgecheck

--- src/js/Content/Features/Store/AgeCheck/CAgecheck.js
+++ src/js/Content/Features/Store/AgeCheck/CAgecheck.js
@@ -1,11 +1,16 @@
 import {Context, ContextType} from "../../../modulesContent";
 import FSkipAgecheck from "../../Common/FSkipAgecheck";
+import FBackToTop from "../../Common/FBackToTop";
+import FFocusSearch from "../../Common/FFocusSearch";
 
 export class CAgeCheck extends Context {
 
     constructor() {
+
         super(ContextType.AGECHECK, [
             FSkipAgecheck,
+            FBackToTop,
+            FFocusSearch,
         ]);
     }
 }
