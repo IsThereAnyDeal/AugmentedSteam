da774a26f7f3d63cdf73311f020798df08bc4f86
candela97 <54083835+candela97@users.noreply.github.com>
2022-12-30 04:28:37 +0800

    CEditGuide: don't apply base features

--- src/js/Content/Features/Community/Guides/CGuides.js
+++ src/js/Content/Features/Community/Guides/CGuides.js
@@ -1,6 +1,5 @@
 import ContextType from "../../../Modules/Context/ContextType";
 import {CApp} from "../App/CApp";
-
 import FRemoveGuidesLangFilter from "./FRemoveGuidesLangFilter";
 
 export class CGuides extends CApp {
