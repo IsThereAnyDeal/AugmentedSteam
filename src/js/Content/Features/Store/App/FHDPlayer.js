8c53776a6c6d62cf9263208fd0a9bfaf3b93613d
candela97 <54083835+candela97@users.noreply.github.com>
2023-05-08 16:22:01 +0800

    Refactor, autohide the new titlebar while playing video

--- src/js/Content/Features/Store/App/FHDPlayer.js
+++ src/js/Content/Features/Store/App/FHDPlayer.js
@@ -9,84 +9,82 @@ export default class FHDPlayer extends Feature {
 
     apply() {
 
-        const context = this.context;
-
-        function clickHDControl() {
-            const playInHD = LocalStorage.get("playback_hd");
-
-            // When the "HD" button is clicked change the definition for all videos accordingly
-            for (const node of document.querySelectorAll("video.highlight_movie")) {
-                context.toggleVideoDefinition(node, !playInHD);
-            }
-
-            LocalStorage.set("playback_hd", !playInHD);
-        }
-
-        function addHDControl(videoEl) {
-            const container = videoEl.parentNode;
-
-            const btn = document.createElement("div");
-            btn.classList.add("es_hd_toggle");
-            btn.textContent = "HD";
-            btn.addEventListener("click", clickHDControl);
-            container.querySelector(".time").insertAdjacentElement("afterend", btn);
-
-            // Toggle fullscreen on video double click
-            videoEl.addEventListener("dblclick", () => {
-                container.querySelector(".fullscreen_button").click();
-            });
-
-            context.toggleVideoDefinition(videoEl, LocalStorage.get("playback_hd"));
-        }
-
-        function addMouseMoveHandler(videoEl) {
-            const overlay = videoEl.nextElementSibling;
-            let timer;
-
-            videoEl.addEventListener("mousemove", () => {
-                if (timer) {
-                    timer.reset();
-                    overlay.style.bottom = "0px";
-                    videoEl.style.cursor = "";
-                } else {
-                    timer = TimeUtils.resettableTimer(() => {
-                        overlay.style.bottom = "-35px";
-                        videoEl.style.cursor = "none";
-                    }, 2000);
-                }
-            });
-
-            videoEl.addEventListener("mouseleave", () => {
-                timer?.stop(); // Avoid hiding the overlay when moving the cursor from video to overlay
-            });
-        }
-
-        // Add HD Control to each video as it's added to the DOM
         for (const container of document.querySelectorAll("div.highlight_movie")) {
 
-            // Check if video has already loaded
-            const videoEl = container.querySelector("video");
-            if (videoEl !== null) {
-                addHDControl(videoEl);
-                addMouseMoveHandler(videoEl);
+            // Check if the video has already loaded
+            if (container.children[0] instanceof HTMLVideoElement) {
+                this._addHDControl(container);
+                this._addMouseMoveHandler(container);
                 continue;
             }
 
-            new MutationObserver((mutations, observer) => {
-
-                // Steam empties the container if the video or poster failed to load, but that should rarely happen so disconnect immediately
+            // Apply features to each video as it's added to the DOM
+            new MutationObserver((_, observer) => {
+                // The video may fail to load, but just disconnect for now
                 observer.disconnect();
 
-                for (const {addedNodes} of mutations) {
-                    for (const node of addedNodes) {
-                        if (node instanceof HTMLVideoElement) {
-                            addHDControl(node);
-                            addMouseMoveHandler(node);
-                            break;
-                        }
-                    }
+                if (container.children[0] instanceof HTMLVideoElement) {
+                    this._addHDControl(container);
+                    this._addMouseMoveHandler(container);
                 }
             }).observe(container, {"childList": true});
         }
+    }
+
+    _clickHDControl() {
+        const playInHD = LocalStorage.get("playback_hd");
+
+        // When the "HD" button is clicked change the definition for all videos accordingly
+        for (const node of document.querySelectorAll("video.highlight_movie")) {
+            this.context.toggleVideoDefinition(node, !playInHD);
+        }
+
+        LocalStorage.set("playback_hd", !playInHD);
+    }
+
+    _addHDControl(container) {
+        const video = container.children[0];
+
+        const btn = document.createElement("div");
+        btn.classList.add("es_hd_toggle");
+        btn.textContent = "HD";
+        btn.addEventListener("click", () => { this._clickHDControl(); });
+        container.querySelector(".time").after(btn);
+
+        // Toggle fullscreen on double click
+        video.addEventListener("dblclick", () => {
+            container.querySelector(".fullscreen_button").click();
+        });
+
+        this.context.toggleVideoDefinition(video, LocalStorage.get("playback_hd"));
+    }
+
+    _addMouseMoveHandler(container) {
+        const [video, overlay, titlebar = null] = container.children;
+        let timer;
+
+        video.addEventListener("mousemove", () => {
+            if (timer) {
+                timer.reset();
+                video.style.cursor = "";
+                overlay.style.bottom = "0px";
+                if (titlebar) {
+                    titlebar.style.top = "0px";
+                }
+            } else {
+                timer = TimeUtils.resettableTimer(() => {
+                    video.style.cursor = "none";
+                    overlay.style.bottom = "-35px";
+                    if (titlebar) {
+                        titlebar.style.top = "-35px";
+                    }
+                }, 2000);
+            }
+        });
+
+        video.addEventListener("mouseleave", () => {
+            // Avoid hiding the overlay when moving the cursor from video to overlay
+            timer?.stop();
+        });
     }
 }
