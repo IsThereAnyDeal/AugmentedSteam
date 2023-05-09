02c8f7da9614e57451fe94f05a5f2470c9ddff9e
candela97 <54083835+candela97@users.noreply.github.com>
2023-05-09 23:05:46 +0800

    Check if active element is within a shadow root

--- src/js/Content/Features/Common/FFocusSearch.js
+++ src/js/Content/Features/Common/FFocusSearch.js
@@ -9,18 +9,25 @@ export default class FFocusSearch extends Feature {
 
     apply() {
 
-        // Tags that should NOT trigger this feature when focused
-        const focusableTags = new Set(["input", "textarea"]);
+        // Tags that receive text input and should NOT trigger this feature
+        const editableTags = new Set(["input", "textarea"]);
+
+        function isContentEditable(el) {
+            if (!el) { return false; }
+            return editableTags.has(el.tagName.toLowerCase()) || el.isContentEditable;
+        }
 
         document.addEventListener("keydown", e => {
-            if (
-                e.key === "s"
-                && !e.repeat
-                && !focusableTags.has(document.activeElement?.tagName.toLowerCase())
-            ) {
-                e.preventDefault();
-                this._node.focus();
-            }
+            if (e.key !== "s" || e.repeat) { return; }
+
+            let el = document.activeElement;
+            if (isContentEditable(el)) { return; }
+            // Check if active element is within a shadow root, see #1623
+            el = el.shadowRoot?.activeElement;
+            if (isContentEditable(el)) { return; }
+
+            e.preventDefault();
+            this._node.focus();
         });
     }
 }
