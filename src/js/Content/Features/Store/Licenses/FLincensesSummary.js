949511d1162ec336de059f364f0fbe64fcce2eb3
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2023-09-12 10:52:27 +0200

    Adjust coding style to the rest of the repo

--- src/js/Content/Features/Store/Licenses/FLincensesSummary.js
+++ src/js/Content/Features/Store/Licenses/FLincensesSummary.js
@@ -4,31 +4,47 @@ import {Feature} from "../../../modulesContent";
 export default class FLincensesSummary extends Feature {
 
     apply() {
-
-        const all = document.querySelectorAll(".account_table tbody tr[data-panel]");
+        const all = document.querySelectorAll(".account_table tr[data-panel]");
         const list = {};
         const types = {};
+
         for (const item of all) {
             const dateStr = item.querySelector(".license_date_col").innerHTML;
-            const year
-                   = /\d{4}$/.exec(dateStr)?.[0] // "24 May, 2022" -> "2022"
-                || /^\d{4}/.exec(dateStr)?.[0] // "2023. aug. 2." -> "2023"
-                || "";
+            const year = /\d{4}/.exec(dateStr)?.[0] ?? "";
             const typ = item.querySelector(".license_acquisition_col").innerHTML.trim();
-            (list[year] ||= {})[typ] = (list[year][typ] || 0) + 1;
-            types[typ] = (types[typ] || 0) + 1;
+
+            (list[year] ??= {})[typ] = (list[year][typ] ?? 0) + 1;
+            types[typ] = (types[typ] ?? 0) + 1;
         }
 
         const listEntries = Object.entries(list).reverse();
-        const totals = [];
-        listEntries.forEach(([, row], i, arr) => {
-            totals[i] = Object.entries(row)
+        const totals = listEntries.map(
+            ([, row]) => Object.entries(row)
                 .map(([, count]) => count)
-                .reduce((a, b) => a + b, 0);
-        });
-        const totalGlobal = totals.reduce((a, b) => a + b, 0);
+                .reduce((a, b) => a + b, 0)
+        );
 
+        const totalGlobal = totals.reduce((a, b) => a + b, 0);
         const typesEntries = Object.entries(types);
+
+        const tableHeader = typesEntries
+            .map(([name]) => `<th>${name}</th>`)
+            .join("");
+
+        const tableFooter = typesEntries
+            .map(([, value]) => `<td>${value || "0"}</td>`)
+            .join("");
+
+        const rows = listEntries
+            .map(([year, row], i) => `<tr>
+                <td>${year}</td>
+                ${typesEntries
+        .map(([name]) => `<td>${row[name] || "0"}</td>`)
+        .join("")
+}
+                <td>${totals[i] || "0"}</td>
+            </tr>`)
+            .join("");
 
         HTML.beforeBegin(
             document.querySelector(".youraccount_page > .block"),
@@ -38,28 +54,13 @@ export default class FLincensesSummary extends Feature {
                         <tbody>
                             <tr>
                                 <th>${Localization.str.year}</th>
-                                ${typesEntries
-        .map(([name]) => `<th>${name}</th>`)
-        .join("")
-}
+                                ${tableHeader}
                                 <th>${Localization.str.all}</th>
                             </tr>
-                            ${listEntries
-        .map(([year, row], i) => `<tr>
-                                <td>${year}</td>
-                                ${typesEntries
-        .map(([name]) => `<td>${row[name] || "0"}</td>`)
-        .join("")
-}
-                                <td>${totals[i] || "0"}</td>
-                            </tr>`)
-        .join("")}
+                            ${rows}
                             <tr>
                                 <td>${Localization.str.all}</td>
-                                ${typesEntries
-        .map(([, value]) => `<td>${value || "0"}</td>`)
-        .join("")
-}
+                                ${tableFooter}
                                 <td>${totalGlobal}</td>
                             </tr>
                         </tbody>
