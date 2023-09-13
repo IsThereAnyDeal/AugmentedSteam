e1fc845e41efa5fe274a526ce614e35eecf58d06
MxtOUT <14999077+MxtOUT@users.noreply.github.com>
2023-09-13 22:09:30 +0200

    Properly remove option
    https://github.com/IsThereAnyDeal/AugmentedSteam/commit/1905c24e27ed889302ef70c7e0114f6ec9caa31b#commitcomment-127267711

--- src/js/Options/Modules/Data/OptionsSetup.js
+++ src/js/Options/Modules/Data/OptionsSetup.js
@@ -3,7 +3,6 @@ export default {
     // general
     "show_progressbar": "show_progressbar",
     "version_show": "options.version_show",
-    "replaceaccountname": "options.replace_account_name",
     "show_backtotop": "options.show_backtotop",
     "disablelinkfilter": "options.disablelinkfilter",
     "send_age_info": "options.send_age_info",
