e2645bc05f85ba0bdcd6ff8ebf2635571271d243
Tomáš Fedor <tomas.fedor3@gmail.com>
2024-03-14 22:00:37 +0100

    Update make-release script

--- config/scripts/make-release.mjs
+++ config/scripts/make-release.mjs
@@ -1,11 +1,20 @@
-const YAML = require("yaml");
-const fs = require("fs");
-const {execSync} = require("child_process");
-const archiver = require("archiver");
+import archiver from "archiver";
+import {Command} from "commander";
+import {execSync} from "node:child_process";
+import fs from "node:fs";
+import fsAsync from "node:fs/promises";
+import YAML from "yaml";
+
+const __dirname = import.meta.dirname;
 
 const root = `${__dirname}/../..`;
 const changelogPath = `${root}/changelog.yml`;
 const releasesPath = `${root}/../AugmentedSteam_releases/`;
+
+const options = (new Command())
+    .option("--rewrite", "Rewrite existing release", false)
+    .parse()
+    .opts();
 
 const changelog = fs.readFileSync(changelogPath, {
     "encoding": "utf8",
@@ -19,8 +28,12 @@ const releaseDir = `${releasesPath}${latestVersion}`;
 const sourceDir = `${releaseDir}/source`;
 
 if (fs.existsSync(releaseDir)) {
-    console.error(`${latestVersion} dir already exists`);
-    process.exit(1);
+    if (options.rewrite) {
+        await fsAsync.rm(releaseDir, {recursive: true});
+    } else {
+        console.error(`${latestVersion} dir already exists`);
+        process.exit(1);
+    }
 }
 
 function copySource() {
@@ -63,42 +76,45 @@ function run(command) {
     });
 }
 
-async function zipSource() {
-    const output = fs.createWriteStream(`${releaseDir}/source.zip`);
-    const archive = archiver("zip");
+async function zip(dirpath, outname) {
+    const output = fs.createWriteStream(`${releaseDir}/${outname}.zip`);
+    const archive = archiver("zip", {
+        zlib: {
+            level: 6
+        }
+    });
 
     archive.on("error", (err) => {
         throw err;
     });
     archive.pipe(output);
-    archive.directory(sourceDir, false);
+    archive.directory(dirpath, false);
     await archive.finalize();
 }
 
-function cleanup() {
-    fs.rmSync(sourceDir, {
+async function cleanup() {
+    await fsAsync.rm(sourceDir, {
         "recursive": true
     });
 }
 
-(async function() {
-    console.log(`Creating release ${latestVersion}`);
-    console.log("1. copy source");
-    copySource();
 
-    console.log("2. zip source");
-    await zipSource();
+console.log(`Creating release ${latestVersion}`);
+console.log("1. copy source");
+copySource();
 
-    console.log("3. npm install");
-    run("npm install");
+console.log("2. zip source");
+await zip(sourceDir, "source");
 
-    console.log("4. build...");
-    for (const platform of ["firefox", "chrome", "edge"]) {
-        console.log(`...${platform}`);
-        run(`npm run build ${platform} -- --production`);
-        fs.cpSync(`${sourceDir}/dist/${platform}.zip`, `${releaseDir}/${platform}.zip`);
-    }
+console.log("3. npm install");
+run("npm install");
 
-    console.log("5. cleanup");
-    cleanup();
-})();
+console.log("4. build...");
+for (const platform of ["firefox", "chrome", "edge"]) {
+    console.log(`...${platform}`);
+    run(`npm run build ${platform} -- --production`);
+    await zip(`${sourceDir}/dist/prod.${platform}`, platform);
+}
+
+console.log("5. cleanup");
+await cleanup();
