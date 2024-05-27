1cb97aea19dfc0abb5e25922c00f49a2f0aa7309
Tomáš Fedor <tomas.fedor3@gmail.com>
2024-05-27 22:30:55 +0200

    Minor Config refactor

---
+++ src/js/config.ts
@@ -0,0 +1,10 @@
+// This is the basic configuration for the Augmented Steam extension
+
+export default {
+    ApiServerHost: "https://api.augmentedsteam.com",
+    PublicHost: "https://augmentedsteam.com",
+    ITADApiServerHost: "https://api.isthereanydeal.com",
+    ITADServer: "https://isthereanydeal.com",
+    ITADClientId: "5fe78af07889f43a",
+    ITADDiscord: "https://discord.gg/yn57q7f",
+};
