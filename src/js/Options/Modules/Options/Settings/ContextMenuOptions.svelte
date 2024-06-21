e691ece343af3fdb69a8adf083f28e65a0e5671e
Tomáš Fedor <tomas.fedor3@gmail.com>
2024-06-21 23:50:41 +0200

    Fix ContextMenu requiring reload to apply changes

--- src/js/Options/Modules/Options/Settings/ContextMenuOptions.svelte
+++ src/js/Options/Modules/Options/Settings/ContextMenuOptions.svelte
@@ -13,56 +13,66 @@
     import {L} from "@Core/Localization/Localization";
     import Toggle from "../Components/Toggle.svelte";
     import Permissions from "@Core/Permissions";
+    import ContextMenu from "@Background/Modules/ContextMenu/ContextMenu";
+
+    type ContextMenuKeys = keyof SettingsSchema & (
+         "context_steam_store"
+       | "context_steam_market"
+       | "context_itad"
+       | "context_bartervg"
+       | "context_steamdb"
+       | "context_steamdb_instant"
+       | "context_steam_keys"
+    );
 
     export let settings: Writable<SettingsSchema>;
 
-    async function handleChange(value: boolean): Promise<boolean> {
-        if (!value) {
-            return false;
+    async function handleChange(key: ContextMenuKeys, value: boolean): Promise<void> {
+        if (value) {
+            const permissions = ["contextMenus"];
+            const hasPermissions = await Permissions.contains(permissions);
+            if (!hasPermissions) {
+                // @ts-expect-error
+                value = await Permissions.request(permissions);
+            }
         }
 
-        const permissions = ["contextMenus"];
-        const hasPermissions = await Permissions.contains(permissions);
-        if (hasPermissions) {
-            return true;
-        }
-
-        // @ts-ignore
-        return Permissions.request(permissions);
+        $settings[key] = value;
+        ContextMenu.update();
     }
 </script>
 
 <Toggle value={$settings.context_steam_store}
-        on:toggle={async (e) => $settings.context_steam_store = await handleChange(e.detail)}>
+        on:toggle={async (e) => handleChange("context_steam_store", e.detail)}>
     {L(__options_contextSteamStore, {query: "..."})}
 </Toggle>
 
 <Toggle value={$settings.context_steam_market}
-        on:toggle={async (e) => $settings.context_steam_market = await handleChange(e.detail)}>
+        on:toggle={async (e) => handleChange("context_steam_market", e.detail)}>
     {L(__options_contextSteamMarket, {query: "..."})}
 </Toggle>
 
 <Toggle value={$settings.context_itad}
-        on:toggle={async (e) => $settings.context_itad = await handleChange(e.detail)}>
+        on:toggle={async (e) => handleChange("context_itad", e.detail)}>
     {L(__options_contextItad, {query: "..."})}
 </Toggle>
 
 <Toggle value={$settings.context_bartervg}
-        on:toggle={async (e) => $settings.context_bartervg = await handleChange(e.detail)}>
+        on:toggle={async (e) => handleChange("context_bartervg", e.detail)}>
     {L(__options_contextBartervg, {query: "..."})}
 </Toggle>
 
 <Toggle value={$settings.context_steamdb}
-        on:toggle={async (e) => $settings.context_steamdb = await handleChange(e.detail)}>
+        on:toggle={async (e) => handleChange("context_steamdb", e.detail)}>
     {L(__options_contextSteamdb, {query: "..."})}
 </Toggle>
 
 <Toggle value={$settings.context_steamdb_instant}
-        on:toggle={async (e) => $settings.context_steamdb_instant = await handleChange(e.detail)}>
+        on:toggle={async (e) => handleChange("context_steamdb_instant", e.detail)}>
     {L(__options_contextSteamdbInstant, {query: "..."})}
 </Toggle>
 
 <Toggle value={$settings.context_steam_keys}
-        on:toggle={async (e) => $settings.context_steam_keys = await handleChange(e.detail)}>
+        on:toggle={async (e) => handleChange("context_steam_keys", e.detail)}>
     {L(__options_contextSteamKeys)}
 </Toggle>
