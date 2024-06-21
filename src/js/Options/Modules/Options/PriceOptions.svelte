2b260f4498503085b2c42450903feee014801736
candela97 <54083835+candela97@users.noreply.github.com>
2024-06-21 08:20:05 +0800

    Options/StoreListSetting: Fix

--- src/js/Options/Modules/Options/PriceOptions.svelte
+++ src/js/Options/Modules/Options/PriceOptions.svelte
@@ -12,7 +12,7 @@
     import Section from "./Components/Section.svelte";
     import Toggle from "./Components/Toggle.svelte";
     import RegionSelect from "./Settings/RegionSelect.svelte";
-    import StoreList from "./Settings/StoreListSetting.svelte";
+    import StoreListSetting from "./Settings/StoreListSetting.svelte";
     import type {SettingsSchema} from "../../Data/_types";
     import OverridePriceSetting from "./Settings/OverridePriceSetting.svelte";
     import OptionGroup from "./Components/OptionGroup.svelte";
@@ -32,7 +32,7 @@
     </OptionGroup>
 
     <OptionGroup>
-        <StoreList {settings} />
+        <StoreListSetting {settings} />
     </OptionGroup>
 
     <OptionGroup>
