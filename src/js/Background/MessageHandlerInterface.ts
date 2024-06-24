871ce9e42a28435fa065dbde83d342b19b566cd6
Tomáš Fedor <tomas.fedor3@gmail.com>
2024-06-25 00:05:17 +0200

    Make MessangeHandlerInterface return type more strict

--- src/js/Background/MessageHandlerInterface.ts
+++ src/js/Background/MessageHandlerInterface.ts
@@ -1,4 +1,5 @@
+import type {Unrecognized} from "@Background/background";
 
 export default interface MessageHandlerInterface {
-    handle(message: any): symbol|Promise<any>;
+    handle(message: any): typeof Unrecognized|Promise<any>;
 }
