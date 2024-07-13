979833ed6833073bf04790f90873465a357b04c3
candela97 <54083835+candela97@users.noreply.github.com>
2024-07-14 06:45:14 +0800

    Fix market-overview scriptlet

--- src/scriptlets/Community/Inventory/marketOverviewPopulation.js
+++ src/scriptlets/Community/Inventory/marketOverviewPopulation.js
@@ -1,7 +1,7 @@
 (function() {
     function onComplete(response) {
         const params = JSON.parse(document.currentScript.dataset.params);
-        const {hashName} = params.hashName;
+        const {hashName} = params;
 
         // Wait for population of the div https://github.com/SteamDatabase/SteamTracking/blob/6c5145935aa0a9f9134e724d89569dfd1f2af014/steamcommunity.com/public/javascript/economy_v2.js#L3563
         if (!response.url.startsWith("https://steamcommunity.com/market/priceoverview/")
