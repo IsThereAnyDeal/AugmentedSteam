bd72856a25ce98fa35d9b7457167bc6b74d5d8f7
candela97 <54083835+candela97@users.noreply.github.com>
2024-07-15 05:51:15 +0800

    Fix some more scriptlets

--- src/scriptlets/Community/Badges/loadImages.js
+++ src/scriptlets/Community/Badges/loadImages.js
@@ -2,13 +2,13 @@
 (function() {
     const params = JSON.parse(document.currentScript.dataset.params);
 
-    window.g_rgDelayedLoadImages = params.images;
+    g_rgDelayedLoadImages = params.images;
 
     // Clear registered image lazy loader watchers
     CScrollOffsetWatcher.sm_rgWatchers = [];
 
     // Recreate image lazy loader watchers
     for (const node of document.querySelectorAll("div[id^=image_group_scroll_badge_images_]")) {
-        f.loadImageGroupOnScroll(node.id, node.id.slice(19));
+        LoadImageGroupOnScroll(node.id, node.id.slice(19));
     }
 })();
--- src/scriptlets/Community/ProfileActivity/patchBlotter.js
+++ src/scriptlets/Community/ProfileActivity/patchBlotter.js
@@ -3,9 +3,9 @@
     if (typeof window.Blotter_ShowLargeScreenshot !== "function") {
 
         window.Blotter_ShowLargeScreenshot = (galleryid, showComments) => {
-            const gallery = f.global("g_BlotterGalleries")[galleryid];
+            const gallery = g_BlotterGalleries[galleryid];
             const ss = gallery.shots[gallery.m_screenshotActive];
-            f.showModalContent(`${ss.m_modalContentLink}&insideModal=1&showComments=${showComments}`, ss.m_modalContentLinkText, ss.m_modalContentLink, true);
+            ShowModalContent(`${ss.m_modalContentLink}&insideModal=1&showComments=${showComments}`, ss.m_modalContentLinkText, ss.m_modalContentLink, true);
         };
     }
 })();
