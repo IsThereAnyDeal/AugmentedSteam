357278baa6456c06b1ea2021e0058901f9419efb
candela97 <54083835+candela97@users.noreply.github.com>
2024-07-28 09:40:39 +0800

    FFriendsInviteButton: svelte-fy

---
+++ src/js/Content/Features/Community/GroupHome/FFriendsInviteButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {__inviteFriends} from "@Strings/_strings";
+    import {L} from "@Core/Localization/Localization";
+
+    export let groupId: string;
+</script>
+
+
+<div class="grouppage_join_area">
+    <a class="btn_blue_white_innerfade btn_medium" href="https://steamcommunity.com/my/friends/?invitegid={groupId}">
+        <span><img src="//community.cloudflare.steamstatic.com/public/images/groups/icon_invitefriends.png" alt="Invite Icon">&nbsp; {L(__inviteFriends)}</span>
+    </a>
+</div>
--- src/js/Content/Features/Community/GroupHome/FFriendsInviteButton.ts
+++ src/js/Content/Features/Community/GroupHome/FFriendsInviteButton.ts
@@ -1,9 +1,7 @@
-import {__inviteFriends} from "@Strings/_strings";
-import {L} from "@Core/Localization/Localization";
+import self_ from "./FFriendsInviteButton.svelte";
 import Feature from "@Content/Modules/Context/Feature";
 import type CGroupHome from "@Content/Features/Community/GroupHome/CGroupHome";
 import User from "@Content/Modules/User";
-import HTML from "@Core/Html/Html";
 
 export default class FFriendsInviteButton extends Feature<CGroupHome> {
 
@@ -13,12 +11,18 @@ export default class FFriendsInviteButton extends Feature<CGroupHome> {
             && document.querySelector(".grouppage_join_area") === null;
     }
 
-    apply() {
-        HTML.afterEnd("#join_group_form",
-            `<div class="grouppage_join_area">
-                <a class="btn_blue_white_innerfade btn_medium" href="https://steamcommunity.com/my/friends/?invitegid=${this.context.groupId}">
-                    <span><img src="//community.cloudflare.steamstatic.com/public/images/groups/icon_invitefriends.png">&nbsp; ${L(__inviteFriends)}</span>
-                </a>
-            </div>`);
+    override apply(): void {
+
+        const node = document.querySelector("#join_group_form");
+        if (!node) {
+            throw new Error("Node not found");
+        }
+
+        (new self_({
+            target: node.parentElement!,
+            props: {
+                groupId: this.context.groupId!
+            }
+        }));
     }
 }
