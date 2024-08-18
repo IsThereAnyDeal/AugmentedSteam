5d4f07d1e79ffeee99cb564ab76b59a71bee5501
candela97 <54083835+candela97@users.noreply.github.com>
2024-08-19 06:41:23 +0800

    Init settings before localization

--- src/js/Content/Features/Community/TradeOffer/PTradeOffer.ts
+++ src/js/Content/Features/Community/TradeOffer/PTradeOffer.ts
@@ -6,10 +6,12 @@
 import CTradeOffer from "./CTradeOffer";
 import "../../../../../css/community/tradeoffer.css"
 import Localization from "@Core/Localization/Localization";
+import {SettingsStore} from "@Options/Data/Settings";
 
 (async function() {
 
     try {
+        await SettingsStore;
         await Localization;
     } catch (err) {
         console.group("Augmented Steam initialization");
