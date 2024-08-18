5976834c1376db5cfd882782d867c3f440c6df41
candela97 <54083835+candela97@users.noreply.github.com>
2024-08-19 07:10:09 +0800

    Simplify a bit

--- src/js/Content/Features/Community/TradeOffer/FCountTradeItems.ts
+++ src/js/Content/Features/Community/TradeOffer/FCountTradeItems.ts
@@ -6,19 +6,15 @@ import HTML from "@Core/Html/Html";
 
 export default class FCountTradeItems extends Feature<CTradeOffer> {
 
-    override checkPrerequisites(): boolean {
-        return document.querySelector(".error_page_links") === null;
-    }
-
     override apply(): void {
 
         HTML.afterEnd(
-            document.querySelector("#your_slots")!.parentNode as HTMLElement,
-            '<div id="your_slots_count" class="trade_item_box"><span id="your_items_count"></span></div>',
+            document.querySelector("#your_slots")!.parentElement!,
+            '<div id="your_slots_count" class="trade_item_box"><span id="your_items_count"></span></div>'
         );
         HTML.afterEnd(
-            document.querySelector("#their_slots")!.parentNode as HTMLElement,
-            "<div id='their_slots_count' class='trade_item_box'><span id='their_items_count'></span></div>",
+            document.querySelector("#their_slots")!.parentElement!,
+            '<div id="their_slots_count" class="trade_item_box"><span id="their_items_count"></span></div>'
         );
 
         new MutationObserver(mutations => {
@@ -37,9 +33,9 @@ export default class FCountTradeItems extends Feature<CTradeOffer> {
                         yourItemsCountNode.textContent = L(yourItems === 1 ? __tradeoffer_numItem : __tradeoffer_numItems, {
                             "num": yourItems
                         });
-                        document.querySelector<HTMLElement>("#your_slots_count")!.style.display = "block"; // TODO slideDown
+                        document.querySelector<HTMLElement>("#your_slots_count")!.style.display = "block";
                     } else {
-                        document.querySelector<HTMLElement>("#your_slots_count")!.style.display = "none"; // TODO slideUp
+                        document.querySelector<HTMLElement>("#your_slots_count")!.style.display = "none";
                     }
 
                     const theirItems = document.querySelectorAll("#their_slots .has_item").length;
@@ -47,23 +43,20 @@ export default class FCountTradeItems extends Feature<CTradeOffer> {
                         theirItemsCountNode.textContent = L(theirItems === 1 ? __tradeoffer_numItem : __tradeoffer_numItems, {
                             "num": theirItems
                         });
-                        document.querySelector<HTMLElement>("#their_slots_count")!.style.display = "block"; // TODO slideDown
+                        document.querySelector<HTMLElement>("#their_slots_count")!.style.display = "block";
                     } else {
-                        document.querySelector<HTMLElement>("#their_slots_count")!.style.display = "none"; // TODO slideUp
+                        document.querySelector<HTMLElement>("#their_slots_count")!.style.display = "none";
                     }
 
-                    yourItemsCountNode.className = "";
-                    theirItemsCountNode.className = "";
-
                     if (yourItems === theirItems) {
-                        yourItemsCountNode.classList.add("es_same");
-                        theirItemsCountNode.classList.add("es_same");
+                        yourItemsCountNode.className = "es_same";
+                        theirItemsCountNode.className = "es_same";
                     } else if (yourItems > theirItems) {
-                        yourItemsCountNode.classList.add("es_higher");
-                        theirItemsCountNode.classList.add("es_lower");
+                        yourItemsCountNode.className = "es_higher";
+                        theirItemsCountNode.className = "es_lower";
                     } else {
-                        yourItemsCountNode.classList.add("es_lower");
-                        theirItemsCountNode.classList.add("es_higher");
+                        yourItemsCountNode.className = "es_lower";
+                        theirItemsCountNode.className = "es_higher";
                     }
                 }
             }
