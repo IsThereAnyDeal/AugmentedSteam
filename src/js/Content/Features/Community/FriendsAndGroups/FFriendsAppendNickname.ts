467c286174af2d27889b21968ab22c28a26e35c8
Tomáš Fedor <tomas@isthereanydeal.com>
2024-08-20 23:59:56 +0200

    Simplify friends nicknames feature, add styles for better readability

--- src/js/Content/Features/Community/FriendsAndGroups/FFriendsAppendNickname.ts
+++ src/js/Content/Features/Community/FriendsAndGroups/FFriendsAppendNickname.ts
@@ -3,6 +3,7 @@ import type CFriendsAndGroups from "@Content/Features/Community/FriendsAndGroups
 import Settings from "@Options/Data/Settings";
 import RequestData from "@Content/Modules/RequestData";
 import User from "@Content/Modules/User";
+import "./FFriendsAppendNickname.css";
 
 export default class FFriendsAppendNickname extends Feature<CFriendsAndGroups> {
 
@@ -21,46 +22,45 @@ export default class FFriendsAppendNickname extends Feature<CFriendsAndGroups> {
             return;
         }
 
-        const nicknamedFriends = document.querySelectorAll(".player_nickname_hint");
+        const nicknamedFriends: NodeListOf<HTMLElement> = document.querySelectorAll(".player_nickname_hint");
         if (nicknamedFriends.length === 0) { return; }
 
-        const steamidMap = new Map();
+        const steamidMap: Map<string, HTMLElement> = new Map();
         for (const node of nicknamedFriends) {
-            const steamid = node.closest<HTMLElement>("[data-steamid]")!.dataset.steamid;
-            steamidMap.set(node, steamid);
+            const steamid = node.closest<HTMLElement>("[data-steamid]")!.dataset.steamid!;
+            steamidMap.set(steamid, node);
         }
 
-        const steamids = [...steamidMap.values()];
-        const personaMap = new Map();
         const chunkSize = 99; // Max 100
         const token = await User.getWebApiToken();
 
-        for (let i = 0; i < steamids.length; i += chunkSize) {
-            const chunk = steamids.slice(i, i + chunkSize).join(",");
+        const steamids: string[] = [...steamidMap.keys()];
+        while (steamids.length > 0) {
+            const chunk = steamids.splice(0, chunkSize).join(",");
 
-            const data = await RequestData.getJson<{
-                players: {
-                    steamid: string,
-                    personaname: string,
-                    lastlogoff?: number, // If friends
-                }[]
-            }>(`https://api.steampowered.com/ISteamUserOAuth/GetUserSummaries/v1/?steamids=${chunk}&access_token=${token}`, {credentials: "omit"})
-                .catch(err => console.error(err));
+            try {
+                const data = await RequestData.getJson<{
+                    players: {
+                        steamid: string,
+                        personaname: string,
+                        lastlogoff?: number, // If friends
+                    }[]
+                }>(`https://api.steampowered.com/ISteamUserOAuth/GetUserSummaries/v1/?steamids=${chunk}&access_token=${token}`, {credentials: "omit"});
 
-            data?.players?.forEach(player => {
-                personaMap.set(player.steamid, player.personaname);
-            });
-        }
+                for (let player of data.players ?? []) {
+                    const node = steamidMap.get(player.steamid)!;
 
-        if (personaMap.size === 0) { return; }
+                    const persona = player.personaname;
+                    const nickname = node.previousSibling!.textContent!.trim();
 
-        for (const [node, steamid] of steamidMap.entries()) {
-            const persona = personaMap.get(steamid);
-            const nickname = node.previousSibling!.textContent!.trim();
-
-            node.previousSibling!.textContent = persona;
-            node.after(...node.children); // Fix last online text styles
-            node.textContent = ` (${nickname})`;
+                    node.after(...node.children); // Fix last online text styles
+                    node.previousSibling!.textContent = persona;
+                    node.textContent = nickname;
+                    node.closest(".friend_block_content")!.classList.add("has-note");
+                }
+            } catch(e) {
+                console.error(e);
+            }
         }
     }
 }
