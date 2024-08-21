bad401290ff9f9cd92c3fdc7984eff3e0d1ebc78
candela97 <54083835+candela97@users.noreply.github.com>
2024-08-22 00:29:30 +0800

    FInGameStoreLink: simplify and avoid redundant requests

--- src/js/Content/Features/Community/ProfileHome/FInGameStoreLink.ts
+++ src/js/Content/Features/Community/ProfileHome/FInGameStoreLink.ts
@@ -8,43 +8,56 @@ import HTML from "@Core/Html/Html";
 
 export default class FInGameStoreLink extends Feature<CProfileHome> {
 
-    private _avatarNode: HTMLElement|null = null;
+    private gameNameNode: HTMLElement|null = null;
 
     override checkPrerequisites(): boolean {
         if (this.context.isPrivateProfile) {
             return false;
         }
 
-        this._avatarNode = document.querySelector<HTMLElement>(".profile_header_size[data-miniprofile]");
+        this.gameNameNode = document.querySelector<HTMLElement>(".profile_in_game_name");
 
-        return this._avatarNode !== null
-            && this._avatarNode.classList.contains("in-game");
+        return this.gameNameNode !== null
+            // Non-Steam game or private
+            && this.gameNameNode.textContent!.trim() !== "";
     }
 
     override async apply(): Promise<void> {
 
+        let appid: number|null = null;
+
         // Try to get appid from the deprecated "report abuse" form (only appears when logged in)
-        let appid: string|number|null = document.querySelector<HTMLInputElement>("input[name=ingameAppID]")?.value ?? null;
+        const node = document.querySelector<HTMLInputElement>("input[name=ingameAppID]");
+        if (node) {
+            appid = Number(node.value);
+        } else {
+            // Otherwise fetch the profile's miniprofile data
+            const node = document.querySelector<HTMLElement>("[data-miniprofile]");
+            if (!node) {
+                throw new Error("Node with miniprofile data not found");
+            }
 
-        // Otherwise fetch the profile's miniprofile hover content
-        if (!appid) {
-            const html = await RequestData.getText(`https://steamcommunity.com/miniprofile/${this._avatarNode!.dataset.miniprofile}`);
-            const doc = HTML.toDom(html);
-            appid = AppId.fromImageElement(doc.querySelector("img.game_logo"));
-            if (!appid) { return; }
+            const data = await RequestData.getJson<{
+                in_game?: {
+                    name: string, // Might be empty
+                    is_non_steam: boolean,
+                    logo?: string,
+                }
+            }>(`https://steamcommunity.com/miniprofile/${node.dataset.miniprofile}/json/`, {credentials: "omit"});
+
+            if (data.in_game?.logo) {
+                appid = AppId.fromCDNUrl(data.in_game.logo);
+            }
         }
 
-        const node = document.querySelector<HTMLElement>(".profile_in_game_name");
-        if (!node) {
-            return;
-        }
+        if (!appid) { return; }
 
         // Find the text node and replace it to avoid removing the `.private_app_indicator` image
-        for (const n of node.childNodes) {
+        for (const n of this.gameNameNode!.childNodes) {
             if (n.nodeType === Node.TEXT_NODE && n.textContent!.trim() !== "") {
 
-                HTML.beforeEnd(node,
-                    `<a href="//store.steampowered.com/app/${appid}" target="_blank">
+                HTML.beforeEnd(this.gameNameNode!,
+                    `<a href="//store.steampowered.com/app/${appid}/" target="_blank">
                         <span data-tooltip-text="${L(__viewInStore)}">${n.textContent}</span>
                     </a>`);
 
