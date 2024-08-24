be71f27fe62de8c04c207c2fe9902a296aab2596
candela97 <54083835+candela97@users.noreply.github.com>
2024-08-25 03:40:25 +0800

    Split out icons.css

---
+++ src/js/Options/Modules/Options/Components/ProfileLink.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import ProfileLinkIcon from "../../Icons/ProfileLinkIcon.svelte";
+
+    export let id: string;
+</script>
+
+
+<span class="link">
+    <ProfileLinkIcon {id} iconType="color" /><slot></slot>
+</span>
+
+
+<style>
+    .link {
+        display: inline-flex;
+        align-items: center;
+    }
+</style>
