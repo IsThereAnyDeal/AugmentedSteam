0af33aa12908e8faeadf71ab4e373cca9be2c489
candela97 <54083835+candela97@users.noreply.github.com>
2024-09-03 19:52:08 +0800

    BoosterCreator\FShowGemsCost: simplify, add and fix types

--- src/js/Content/Features/Community/BoosterCreator/FShowGemsCost.ts
+++ src/js/Content/Features/Community/BoosterCreator/FShowGemsCost.ts
@@ -1,33 +1,34 @@
 import type CBoosterCreator from "@Content/Features/Community/BoosterCreator/CBoosterCreator";
 import Feature from "@Content/Modules/Context/Feature";
-import DOMHelper from "@Content/Modules/DOMHelper";
+import SteamFacade from "@Content/Modules/Facades/SteamFacade";
+
+type BoosterData = Record<string, {
+    appid: number,
+    name: string,
+    series: number,
+    price: string
+}>;
 
 export default class FShowGemsCost extends Feature<CBoosterCreator> {
 
     override async apply() {
 
-        // TODO add boosterData type
-        const boosterData = await (new Promise(resolve => {
-            // @ts-ignore
-            document.addEventListener("as_boosterData", (e: CustomEvent) => resolve(e.detail), {once: true});
-            DOMHelper.insertScript("scriptlets/Community/BoosterCreator/boosterData.js");
-        }));
+        const boosterData = (await SteamFacade.global<{sm_rgBoosterData: BoosterData}>("CBoosterCreatorPage"))?.sm_rgBoosterData;
+        if (!boosterData) {
+            throw new Error("Booster data not found");
+        }
 
         // Localized string of "(quantifier) + Gems"
-        const gemWord = document.querySelector<HTMLElement>(".booster_creator_goostatus .goo_display")!
-            .textContent!
-            .trim()
-            .replace(/[\d]+,?/g, "");
+        const gemWord = document.querySelector(".booster_creator_goostatus .goovalue")?.nextSibling?.textContent;
+        if (!gemWord) {
+            return;
+        }
 
-        // TODO what is options param?
-
-        // @ts-ignore
-        const options = document.getElementById("booster_game_selector")?.options;
+        const options = document.querySelector<HTMLSelectElement>("#booster_game_selector")?.options ?? [];
         for (const option of options) {
-            const val = option.value;
-            if (val) {
-                // @ts-ignore
-                option.append(` - ${boosterData[val].price} ${gemWord}`);
+            const data = boosterData[option.value];
+            if (data) {
+                option.append(` - ${data.price} ${gemWord}`);
             }
         }
     }
--- src/scriptlets/Community/BoosterCreator/boosterData.js
+++
@@ -1,5 +0,0 @@
-
-(function(){
-    const data = JSON.parse(JSON.stringify(window.CBoosterCreatorPage.sm_rgBoosterData));
-    document.dispatchEvent(new CustomEvent("as_boosterData", {detail: data}));
-})();
