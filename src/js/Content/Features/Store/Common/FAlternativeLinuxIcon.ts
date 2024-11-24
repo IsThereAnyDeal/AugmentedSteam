dd8167bf5283308b712828c59567b609c6d9e8d3
Tomáš Fedor <tomas@isthereanydeal.com>
2024-11-24 02:58:33 +0100

    Update FAlternativeLinuxIcon

--- src/js/Content/Features/Store/Common/FAlternativeLinuxIcon.ts
+++ src/js/Content/Features/Store/Common/FAlternativeLinuxIcon.ts
@@ -14,10 +14,33 @@ export default class FAlternativeLinuxIcon extends Feature<CStoreBase> {
     override apply(): void {
         const url = ExtensionResources.getURL("img/alternative_linux_icon.png");
 
-        let cssText = `span.platform_img.linux { background-image: url(${url}) !important; }`;
+        let cssText = "";
+
+        // react page, alternatively, use react class: .wIs-huKXogw-.EdGS6nZmIig-
+        cssText += `
+            span[title*='Linux'] svg {
+              visibility:hidden
+            }
+            span[title*='Linux']:has(svg) {
+              background-image: url(${url});
+              background-repeat: no-repeat;
+              background-position: center;
+            }
+        `;
+
+        // legacy
+        cssText += `
+            span.platform_img.linux {
+              background-image: url(${url}) !important;
+            }
+        `;
 
         if (this.context.type === ContextType.STORE_FRONT) {
-            cssText += ".tab_item.focus .tab_item_details span.platform_img.linux { filter: brightness(0%); }";
+            cssText += `
+              .tab_item.focus .tab_item_details span.platform_img.linux {
+                filter: brightness(0%);
+              }
+            `;
         }
 
         DOMHelper.insertCSS(cssText);
