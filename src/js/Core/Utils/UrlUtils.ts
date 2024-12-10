e12949065acc46e3b3440be998d7b0d67b28a225
Tomáš Fedor <tomas@isthereanydeal.com>
2024-12-10 01:33:40 +0100

    Fix and simplify URL custom URLs handling

--- src/js/Core/Utils/UrlUtils.ts
+++ src/js/Core/Utils/UrlUtils.ts
@@ -2,20 +2,9 @@
 export default class UrlUtils {
 
     public static escapeUserUrl(url: string, replacements: Record<string, string>): string {
-        const result = new URL(url);
-
-        /*
-         * For some reason
-         *  for (const ... of result.searchParams.entries())
-         *  does not work, and I have no idea why (says it's not iterable)
-         */
-
-        result.searchParams.forEach((value, name, searchParams) => {
-            for (const [pattern, replacement] of Object.entries(replacements)) {
-                value = value.replace(`[${pattern}]`, replacement);
-            }
-            searchParams.set(name, value);
-        });
-        return result.toString();
+        for (const [pattern, replacement] of Object.entries(replacements)) {
+            url = url.replace(`[${pattern}]`, replacement);
+        }
+        return (new URL(url)).toString();
     }
 }
