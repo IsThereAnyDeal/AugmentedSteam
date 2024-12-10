da2daebf48c68a6fd849ea78dc1dd832c7c98931
Tomáš Fedor <tomas@isthereanydeal.com>
2024-12-10 20:27:52 +0100

    Add protocol to custom URLs if it's missing, encode replacement

--- src/js/Core/Utils/UrlUtils.ts
+++ src/js/Core/Utils/UrlUtils.ts
@@ -2,8 +2,12 @@
 export default class UrlUtils {
 
     public static escapeUserUrl(url: string, replacements: Record<string, string>): string {
+        if (!/^[^/]+\/\//.test(url)) {
+            url = `https://${url}`;
+        }
+
         for (const [pattern, replacement] of Object.entries(replacements)) {
-            url = url.replace(`[${pattern}]`, replacement);
+            url = url.replace(`[${pattern}]`, encodeURIComponent(replacement));
         }
         return (new URL(url)).toString();
     }
