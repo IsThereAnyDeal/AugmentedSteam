edfc2a2da1bfd577fa5b565ed7e4b4b79ce75986
Nuckle <nuckle@itscat.org>
2025-03-20 16:17:36 +0300

    Add Give Steam Community Award to profile dropdown options

---
+++ src/js/Content/Features/Community/ProfileHome/FProfileAwardButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { __giveAward } from "@Strings/_strings";
+	import { L } from "@Core/Localization/Localization";
+</script>
+
+<a class="popup_menu_item es_award_button" id="es_award" role="button">
+	<img
+		src="//community.fastly.steamstatic.com/public/shared/images/award_icon.svg"
+		alt="Steam Community Award Icon"
+	/>&nbsp; {L(__giveAward)}
+</a>
+
+<style>
+	.es_award_button img {
+		top: 5px !important;
+	}
+</style>
--- src/js/Content/Features/Community/ProfileHome/FProfileDropdownOptions.ts
+++ src/js/Content/Features/Community/ProfileHome/FProfileDropdownOptions.ts
@@ -1,39 +1,73 @@
-import {__addNickname, __postHistory} from "@Strings/_strings";
-import {L} from "@Core/Localization/Localization";
-import type CProfileHome from "@Content/Features/Community/ProfileHome/CProfileHome";
-import Feature from "@Content/Modules/Context/Feature";
-import HTML from "@Core/Html/Html";
-import DOMHelper from "@Content/Modules/DOMHelper";
+import { __addNickname, __postHistory } from '@Strings/_strings';
+import { L } from '@Core/Localization/Localization';
+import FProfileAwardButton from './FProfileAwardButton.svelte';
+import type CProfileHome from '@Content/Features/Community/ProfileHome/CProfileHome';
+import Feature from '@Content/Modules/Context/Feature';
+import HTML from '@Core/Html/Html';
+import DOMHelper from '@Content/Modules/DOMHelper';
 
 export default class FProfileDropdownOptions extends Feature<CProfileHome> {
+	private _node: HTMLElement | null = null;
+	private _path: string = '';
 
-    private _node: HTMLElement|null = null;
+	override checkPrerequisites(): boolean {
+		this._node = document.querySelector(
+			'#profile_action_dropdown .popup_body .profile_actions_follow',
+		);
+		return this._node !== null;
+	}
 
-    override checkPrerequisites(): boolean {
-        this._node = document.querySelector("#profile_action_dropdown .popup_body .profile_actions_follow");
-        return this._node !== null;
-    }
+	override apply(): void {
+		this._path = window.location.pathname.match(
+			/\/((?:id|profiles)\/[^\/]+)/,
+		)![1]!;
 
-    override apply(): void {
-
-        // add nickname option for non-friends
-        if (this.context.user.isSignedIn) {
-
-            // Selects the "Add Friend" button (ID selector for unblocked, class selector for blocked users)
-            if (document.querySelector("#btn_add_friend, .profile_header_actions > .btn_profile_action_disabled")) {
-                HTML.afterEnd(this._node,
-                    `<a class="popup_menu_item" id="es_nickname">
+		// add nickname option for non-friends
+		if (this.context.user.isSignedIn) {
+			// Selects the "Add Friend" button (ID selector for unblocked, class selector for blocked users)
+			if (
+				document.querySelector(
+					'#btn_add_friend, .profile_header_actions > .btn_profile_action_disabled',
+				)
+			) {
+				HTML.afterEnd(
+					this._node,
+					`<a class="popup_menu_item" id="es_nickname">
                         <img src="//community.cloudflare.steamstatic.com/public/images/skin_1/notification_icon_edit_bright.png">&nbsp; ${L(__addNickname)}
-                    </a>`);
-            }
+                    </a>`,
+				);
+			}
 
-            DOMHelper.insertScript("scriptlets/Community/ProfileHome/profileDropdown.js");
-        }
+			DOMHelper.insertScript(
+				'scriptlets/Community/ProfileHome/profileDropdown.js',
+			);
+		}
 
-        // add post history link
-        HTML.afterEnd(this._node,
-            `<a class="popup_menu_item" href="${window.location.pathname}/posthistory">
+		// add give awards option for hidden profiles
+		if (this.context.user.isSignedIn) {
+			const loginURL = encodeURIComponent(
+				`https://steamcommunity.com/login/home/?goto=${this._path}/?insideModal=0`,
+			);
+
+			// Selects the default "Give Award" button
+			if (!document.querySelector('.reward_btn_icon[data-tooltip-text]') && this._node) {
+				new FProfileAwardButton({
+					target: this._node,
+				});
+			}
+
+			DOMHelper.insertScript('scriptlets/Community/ProfileHome/awardDropdown.js', {
+				loginURL,
+				steamId: this.context.steamId,
+			});
+		}
+
+		// add post history link
+		HTML.afterEnd(
+			this._node,
+			`<a class="popup_menu_item" href="${window.location.pathname}/posthistory">
                 <img src="//community.cloudflare.steamstatic.com/public/images/skin_1/icon_btn_comment.png">&nbsp; ${L(__postHistory)}
-            </a>`);
-    }
+            </a>`,
+		);
+	}
 }
