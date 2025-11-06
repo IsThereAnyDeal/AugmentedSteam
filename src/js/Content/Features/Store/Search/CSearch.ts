d5f2923bdcb52768cefe97986e90e27a1ea51bba
Tomáš Fedor <tomas@isthereanydeal.com>
2025-11-07 00:16:24 +0100

    Fix Steam deck compatibility filters not being collapsable (Steam bug) #2161

--- src/js/Content/Features/Store/Search/CSearch.ts
+++ src/js/Content/Features/Store/Search/CSearch.ts
@@ -2,6 +2,7 @@ import FSearchFilters from "./FSearchFilters";
 import CStoreBase from "@Content/Features/Store/Common/CStoreBase";
 import ContextType from "@Content/Modules/Context/ContextType";
 import type {ContextParams} from "@Content/Modules/Context/Context";
+import DOMHelper from "@Content/Modules/DOMHelper";
 
 export default class CSearch extends CStoreBase {
 
@@ -13,5 +14,17 @@ export default class CSearch extends CStoreBase {
         ]);
 
         this.infiniScrollEnabled = document.querySelector<HTMLElement>(".search_pagination")?.style.display === "none";
+
+        /**
+         * Fixing Steam bug where Steam deck compatibility box wasn't collapsable
+         * */
+        const deckCompatibility = document.querySelector("#deck_compatibility_search_filter");
+        if (deckCompatibility && !deckCompatibility.classList.contains("search_collapse_block")) {
+            deckCompatibility.classList.add("search_collapse_block");
+            DOMHelper.insertScript("scriptlets/Store/Search/addCollapseHandlers.js", {
+                collapseName: "deck_compatibility",
+                shouldCollapse: true
+            });
+        }
     }
 }
