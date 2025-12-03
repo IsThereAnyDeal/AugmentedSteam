21f6766210fc0440665d706d09003e277d0f6f27
Tomáš Fedor <tomas@isthereanydeal.com>
2025-12-03 22:20:38 +0100

    Add expiry to Vaporlens cache

--- src/js/Background/Modules/VaporLens/VaporLensApi.ts
+++ src/js/Background/Modules/VaporLens/VaporLensApi.ts
@@ -6,28 +6,40 @@ import type { TVaporLensResponse } from "@Background/Modules/VaporLens/_types";
 
 export default class VaporLensApi extends Api implements MessageHandlerInterface {
 
-    private readonly cache: Map<number, any>;
+    private readonly cache: Map<number, {timestamp: number, data: TVaporLensResponse|null}>;
 
     constructor() {
         super("https://vaporlens.app/");
         this.cache = new Map();
     }
 
-    private fetchInsights(appid: number): Promise<TVaporLensResponse|null> {
+    private async fetchInsights(appid: number): Promise<TVaporLensResponse|null> {
         const url = this.getUrl(`api/app/${appid}`);
 
         if (this.cache.has(appid)) {
-            // TODO expiry, 12 hrs
-            return this.cache.get(appid);
+            const {timestamp, data} = this.cache.get(appid)!;
+            if (timestamp < Date.now() - 12*86400*1000) {
+                this.cache.delete(appid);
+            } else {
+                return data;
+            }
         }
 
-        const response = this.fetchJson<TVaporLensResponse>(url, {
-            credentials: "omit",
-            headers: {
-                Accept: "application/json",
-            },
+        let response: TVaporLensResponse|null;
+        try {
+            response = await this.fetchJson<TVaporLensResponse>(url, {
+                credentials: "omit",
+                headers: {
+                    Accept: "application/json",
+                },
+            });
+        } catch {
+            response = null;
+        }
+        this.cache.set(appid, {
+            timestamp: Date.now(),
+            data: response
         });
-        this.cache.set(appid, response);
 
         return response;
     }
