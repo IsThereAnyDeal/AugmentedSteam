6510001343ddfc9019350a84c0f6892fd1cbf916
wplacelongkeeper-del <wplacelongkeeper@gmail.com>
2026-05-02 05:27:54 +0000

    Update MostlySearchFilter.ts

--- src/js/Content/Features/Store/Search/Filters/MostlySearchFilter.ts
+++ src/js/Content/Features/Store/Search/Filters/MostlySearchFilter.ts
@@ -1,18 +1,18 @@
 import SimpleSearchFilter from "./SimpleSearchFilter";
-import {__searchFilters_hideNegative} from "@Strings/_strings";
+import {__searchFilters_hideMostly} from "@Strings/_strings";
 import type FSearchFilters from "@Content/Features/Store/Search/FSearchFilters";
 import {L} from "@Core/Localization/Localization";
 
-export default class NegativeSearchFilter extends SimpleSearchFilter {
+export default class MostlySearchFilter extends SimpleSearchFilter {
 
     constructor(feature: FSearchFilters) {
-        super("as-hide", feature, L(__searchFilters_hideNegative), "negative");
+        super("as-hide", feature, L(__searchFilters_hideMostly), "mostly");
     }
 
     override _addRowMetadata(rows: NodeListOf<HTMLElement>): void {
         for (const row of rows) {
-            if (row.querySelector(".search_reviewscore span.search_review_summary.negative")) {
-                row.classList.add("as-hide-negative");
+            if (row.querySelector(".search_reviewscore span.search_review_summary.mostly")) {
+                row.classList.add("as-hide-mostly");
             }
         }
     }
