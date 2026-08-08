1f3142d71235299ee30797a7feae30f9a33f85dc
Tomáš Fedor <tomas@isthereanydeal.com>
2026-08-08 17:00:48 +0200

    Bump strict_min_version in Firefox manifest

--- scripts/tools/manifestBuilder.mjs
+++ scripts/tools/manifestBuilder.mjs
@@ -53,7 +53,7 @@ export default class ManifestBuilder {
             this._manifest.browser_specific_settings = {
                 gecko: {
                     id: "{1be309c5-3e4f-4b99-927d-bb500eb4fa88}",
-                    strict_min_version: "109.0",
+                    strict_min_version: "140.0",
                     data_collection_permissions: {
                         required: ["none"],
                         optional: [
